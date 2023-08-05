94b666fd4659ad77f9fb6b879c6cedd8436a39ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:03 +0000

    Add package tg/0.10.0/tg-0.10.0.tar.gz

---
+++ packages/tg/tg-0.10.0.tar.gz/tg-0.10.0/setup.py
@@ -0,0 +1,26 @@
+import tg
+from setuptools import setup
+
+with open("readme.md", "r") as fh:
+    readme = fh.read()
+
+
+setup(
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    name="tg",
+    version=tg.__version__,
+    description="Terminal client for telegram",
+    url="https://github.com/paul-nameless/tg",
+    author="Paul Nameless",
+    author_email="reacsdas@gmail.com",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=["tg"],
+    entry_points={"console_scripts": ["tg = tg.__main__:main"]},
+    python_requires=">=3.8",
+    install_requires=["python-telegram==0.14.0"],
+)
