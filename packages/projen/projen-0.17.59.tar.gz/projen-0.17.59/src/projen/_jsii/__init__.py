2af271601eeea23e8399bd2ef007cadab34f49dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:42 +0000

    Add package projen/0.17.59/projen-0.17.59.tar.gz

---
+++ packages/projen/projen-0.17.59.tar.gz/projen-0.17.59/src/projen/_jsii/__init__.py
@@ -0,0 +1,19 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "projen", "0.17.59", __name__[0:-6], "projen@0.17.59.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
