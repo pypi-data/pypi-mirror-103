82fb0fbc40ae71b296f11796f5ce9904e5f337cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:00 +0000

    Add package unicms/0.11.4/unicms-0.11.4.tar.gz

---
+++ packages/unicms/unicms-0.11.4.tar.gz/unicms-0.11.4/src/cms/api/views/page_media.py
@@ -0,0 +1,49 @@
+from cms.pages.forms import PageMediaForm
+from cms.pages.models import *
+from cms.pages.serializers import *
+
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .. serializers import UniCMSFormSerializer
+from .. views.page import PageRelatedObject, PageRelatedObjectList
+
+
+class PageMediaList(PageRelatedObjectList):
+    """
+    """
+    description = ""
+    search_fields = ['media__title', 'media__file', 'media__description']
+    ordering_fields = ['id', 'media__title', 'media__file',
+                        'media__description', 'order', 'is_active']
+    serializer_class = PageMediaSerializer
+
+    def get_queryset(self):
+        """
+        """
+        super().get_data()
+        if self.page:
+            return PageMedia.objects.filter(page=self.page)
+        return PageMedia.objects.none() # pragma: no cover
+
+
+class PageMediaView(PageRelatedObject):
+    """
+    """
+    description = ""
+    serializer_class = PageMediaSerializer
+
+    def get_queryset(self):
+        """
+        """
+        super().get_data()
+        items = PageMedia.objects.filter(pk=self.pk, page=self.page)
+        return items
+
+
+class PageMediaFormView(APIView):
+
+    def get(self, *args, **kwargs):
+        form = PageMediaForm(page_id=kwargs.get('page_id'))
+        form_fields = UniCMSFormSerializer.serialize(form)
+        return Response(form_fields)
