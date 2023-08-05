97aca6222149c764e88d661f72ad080736dbfe0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:07:27 +0000

    Add package openapiart/0.0.7/openapiart-0.0.7.tar.gz

---
+++ packages/openapiart/openapiart-0.0.7.tar.gz/openapiart-0.0.7/openapiart/tests/test_bundle.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+def test_config(api):
+    config  = api.config()
+    config.a = 'asdf'
+    config.b = 1.1
+    config.c = 1
+    config.d = ['a', 'b']
+    print(config)
+    
+
+if __name__ == '__main__':
+    pytest.main(['-v', '-s', __file__])
