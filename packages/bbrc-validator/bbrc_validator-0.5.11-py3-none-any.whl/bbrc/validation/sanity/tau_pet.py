d0eedbe4fbb8e9123cbd17b73de4524cb13b9b2c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:49 +0000

    Add package bbrc-validator/0.5.11/bbrc_validator-0.5.11-py3-none-any.whl

---
+++ packages/bbrc-validator/bbrc_validator-0.5.11-py3-none-any.whl/bbrc/validation/sanity/tau_pet.py
@@ -0,0 +1,23 @@
+from ..test import ExperimentTest, ScanTest, Results
+from ..utils import __is_valid_scan__
+from . import pet
+
+
+class IsScannerVersionCorrect(pet.IsScannerVersionCorrect):
+
+    passing = 'BBRCDEV_E02957',
+    failing = 'BBRCDEV_E02948',
+    scanner_info = {'Manufacturer': ['SIEMENS'],
+                    'ManufacturersModelName': ['Biograph64', 'Biograph64_mCT'],
+                    'SoftwareVersion(s)': ['VG62B']}
+    __doc__ = pet.IsScannerVersionCorrect.__doc__
+    __doc__ = __doc__.replace(
+        pet.IsScannerVersionCorrect.scanner_info['SoftwareVersion(s)'][0],
+        scanner_info['SoftwareVersion(s)'][0])
+
+
+class HasUsableT1(pet.HasUsableT1):
+    passing = 'BBRCDEV_E02957',
+    failing = 'BBRCDEV_E02948',
+    included_projects = ['ALFA_PLUS', 'ALFA_PLUS2', 'ALFA_OPCIONAL',
+                         'ALFA_PLUS_V2', 'ALFA_PLUS2_V2', 'EPAD', 'testenv']
