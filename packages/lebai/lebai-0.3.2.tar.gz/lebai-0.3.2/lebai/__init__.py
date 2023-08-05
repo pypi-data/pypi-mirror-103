cdb4056f7b2fbea58f5c068e4b992fecc446af6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:24 +0000

    Add package lebai/0.3.2/lebai-0.3.2.tar.gz

---
+++ packages/lebai/lebai-0.3.2.tar.gz/lebai-0.3.2/lebai/__init__.py
@@ -0,0 +1,10 @@
+# Copyright (C) 2017-2021 Lebai Robotics
+# Author: kingfree
+# Contact: kingfree@toyama.moe
+
+"""Lebai Robot Python SDK"""
+
+from .type import RobotState, TaskStatus, IODeviceType, CartesianPose, JointPose, Error, RequestError
+from .robot_async import LebaiRobotAsync
+from .robot import LebaiRobot
+from .scene import LebaiScene
