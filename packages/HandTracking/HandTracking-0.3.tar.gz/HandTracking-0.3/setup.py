bb5605f1073c9cb90fc3c6cdbb6d6c67aeb26504
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:26 +0000

    Add package HandTracking/0.3/HandTracking-0.3.tar.gz

---
+++ packages/HandTracking/HandTracking-0.3.tar.gz/HandTracking-0.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+setup(name="HandTracking",
+version="0.3",
+description="This is HandTracking package made by abhinav kourav.",
+long_description="This package will track your hand. It can be used in many projects.",
+author="Abhinav Kourav",
+author_email='kouabhinav@gmail.com',
+packages=['HandTracking'],
+install_requires=['opencv-python', 'mediapipe'])
