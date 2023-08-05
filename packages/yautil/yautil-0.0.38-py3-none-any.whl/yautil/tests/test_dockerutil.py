f9c5dde590acd9645365f1c7d82fe0c8e5beff54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:59 +0000

    Add package yautil/0.0.38/yautil-0.0.38-py3-none-any.whl

---
+++ packages/yautil/yautil-0.0.38-py3-none-any.whl/yautil/tests/test_dockerutil.py
@@ -0,0 +1,63 @@
+import os
+from os import path as _p
+from unittest import TestCase
+
+from yautil import docker_sh
+from tempfile import TemporaryDirectory
+
+from yautil.dockerutil import dsh
+
+
+class TestDocker(TestCase):
+    tmpdir: TemporaryDirectory
+
+    def setUp(self):
+        self.tmpdir = TemporaryDirectory()
+
+    def tearDown(self) -> None:
+        self.tmpdir.cleanup()
+
+    # def test_hello(self):
+    #     uid = os.getuid()
+    #     gid = os.getgid()
+    #
+    #     ctx = _p.join(self.tmpdir.name, 'test_hello')
+    #     os.makedirs(ctx, exist_ok=True)
+    #
+    #     dockerfile = _p.join(ctx, 'Dockerfile')
+    #     with open(dockerfile, 'w+') as f:
+    #         f.write('FROM ubuntu')
+    #
+    #     assert str(dsh('echo', 'hello', _build_context=ctx)).strip() == 'hello'
+    #
+    #     assert int(str(dsh('id -u', _build_context=ctx)).strip()) == uid
+    #
+    #     assert int(str(dsh('id -g', _build_context=ctx)).strip()) == gid
+    #
+    #     assert int(str(dsh('id -u', _build_context=ctx, _root=True)).strip()) == '0'
+    #
+    #     assert int(str(dsh('id -g', _build_context=ctx, _root=True)).strip()) == '0'
+
+    def test_docker_sh(self):
+        uid = os.getuid()
+        gid = os.getgid()
+
+        ctx = _p.join(self.tmpdir.name, 'test_v2')
+        os.makedirs(ctx, exist_ok=True)
+
+        dockerfile = _p.join(ctx, 'Dockerfile')
+        with open(dockerfile, 'w+') as f:
+            f.write('FROM ubuntu')
+
+        c = docker_sh(ctx)
+        rc = docker_sh(ctx, root=True)
+
+        assert str(c.echo('-n', 'hello')) == 'hello'
+
+        assert int(str(c.id(u=True)).strip()) == uid
+
+        assert int(str(c.id(g=True)).strip()) == gid
+
+        assert int(str(rc.id(u=True)).strip()) == 0
+
+        assert int(str(rc.id(g=True)).strip()) == 0
