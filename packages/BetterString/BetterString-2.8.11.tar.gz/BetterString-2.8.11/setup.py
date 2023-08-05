9503a376445c302a800170a88ec9adc89af173a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:05 +0000

    Add package BetterString/2.8.11/BetterString-2.8.11.tar.gz

---
+++ packages/BetterString/BetterString-2.8.11.tar.gz/BetterString-2.8.11/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+# Get Long Description
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+setup(
+    name="BetterString",
+    version="2.8.11",
+    # Major version 2
+    # Minor version 8
+    # Maintenance version 11
+
+    author="DerSchinken (aka DrBumm)",
+    description="Like a normal string but with more functionality",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    python_requires=">=3.8",
+    url="https://github.com/DrBumm/BetterString",
+    keyword=[
+        "Better String",
+        "String",
+    ],
+    classifiers=[
+        'Intended Audience :: Developers',
+
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
