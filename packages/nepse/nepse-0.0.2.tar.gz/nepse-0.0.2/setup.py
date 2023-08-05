37f9f763b0e7f35fb84bb1f4f3579e2085e4bf42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:13 +0000

    Add package nepse/0.0.2/nepse-0.0.2.tar.gz

---
+++ packages/nepse/nepse-0.0.2.tar.gz/nepse-0.0.2/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Python Wrapper for Newweb Nepse'
+LONG_DESCRIPTION = 'Nepse.py allows you to get realtime market prices and stats off nepse.'
+
+# Setting up
+setup(
+    name="nepse",
+    version=VERSION,
+    author="FRAPPÉ (FRAPPÉ#4101)",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['requests','matplotlib'],
+    keywords=['python', 'nepse', 'stock', 'nepal stock', 'nepal stock prices', 'nepse pythonb']
+)
