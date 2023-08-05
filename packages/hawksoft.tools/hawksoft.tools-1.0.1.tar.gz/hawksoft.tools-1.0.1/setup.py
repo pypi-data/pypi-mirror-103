a27c5a211209370e94cba6047dc218532eadab16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:20 +0000

    Add package hawksoft.tools/1.0.1/hawksoft.tools-1.0.1.tar.gz

---
+++ packages/hawksoft.tools/hawksoft.tools-1.0.1.tar.gz/hawksoft.tools-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(name='hawksoft.tools',
+      version='1.0.1',
+      packages=find_packages(exclude=['contrib', 'docs', 'tests']),  # 多文件模块写法
+      author="xingyongkang",
+      author_email="xingyongkang@cqu.edu.cn",
+      description="Provides many useful command lines in python",
+      long_description=open('./README.md', encoding='utf-8').read(),
+      long_description_content_type = "text/markdown",
+      #long_description="http://gitee.comg/xingyongkang",
+      license="MIT",
+      url="https://github.com/xingyongkang",
+      include_package_data=True,
+      platforms="any",
+      #install_requires=[],
+      keywords='md2zhihu tree ',
+      entry_points={
+          'console_scripts': [
+              'md2zhihu=hawksoft.md2zhihu:main',
+              'tree=hawksoft.tree:main'
+          ]
+      },
+)
