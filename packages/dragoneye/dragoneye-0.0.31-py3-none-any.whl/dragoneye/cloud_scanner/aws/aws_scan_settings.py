035da422be7c51ab7fb44d3daabbfc691197cc15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:13:32 +0000

    Add package dragoneye/0.0.31/dragoneye-0.0.31-py3-none-any.whl

---
+++ packages/dragoneye/dragoneye-0.0.31-py3-none-any.whl/dragoneye/cloud_scanner/aws/aws_scan_settings.py
@@ -0,0 +1,26 @@
+import os
+from typing import List
+
+from dragoneye.cloud_scanner.aws.aws_utils import AwsUtils
+from dragoneye.cloud_scanner.base_cloud_scanner import CloudScanSettings, CloudProvider
+
+
+class AwsCloudScanSettings(CloudScanSettings):
+    def __init__(self,
+                 commands_path: str,
+                 account_name: str,
+                 default_region: str = AwsUtils.get_default_region(),
+                 regions_filter: List[str] = None,
+                 max_attempts: int = 10,
+                 duration_session_time: int = 3600,
+                 max_pool_connections: int = 50,
+                 command_timeout: int = 600,
+                 output_path: str = os.getcwd(),
+                 should_clean_before_scan: bool = True):
+        super().__init__(CloudProvider.AWS, account_name, should_clean_before_scan, output_path, commands_path)
+        self.regions_filter: str = ','.join(regions_filter) if regions_filter else ''
+        self.default_region: str = default_region
+        self.max_attempts: int = max_attempts
+        self.duration_session_time: int = duration_session_time
+        self.max_pool_connections: int = max_pool_connections
+        self.command_timeout: int = command_timeout
