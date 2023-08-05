715636d931b0a405e507e2a7c3ed8f36dbcb3f6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:21 +0000

    Add package mapsipy/2021.4.22/mapsipy-2021.4.22.tar.gz

---
+++ packages/mapsipy/mapsipy-2021.4.22.tar.gz/mapsipy-2021.4.22/mapsi/template_files/src/router.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+routes = [
+    {
+        "name": "Home",
+        "url": "Home"
+    },
+    {
+        "name": "Counter",
+        "url": "Counter"
+    },
+    {
+        "name": "Hello",
+        "url": "Hello"
+    },
+    {
+        "name": "Tabular",
+        "url": "Tabular"
+    }
+]
