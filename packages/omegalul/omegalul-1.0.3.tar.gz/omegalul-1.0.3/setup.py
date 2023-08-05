c89157bbe84ebd5cc4b53ca4b692a5585bffe50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:22 +0000

    Add package omegalul/1.0.3/omegalul-1.0.3.tar.gz

---
+++ packages/omegalul/omegalul-1.0.3.tar.gz/omegalul-1.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('readme.md') as file:
+    readme = file.read()
+
+setuptools.setup(
+    name='omegalul',
+    version='1.0.3',
+    author='pcranaway',
+    author_email='pcranaway@tuta.io',
+    description='A Python library for building omegle clients',
+    url='https://github.com/pcranaway/omegalul',
+    packages=setuptools.find_packages('src'),
+    package_dir={'': 'src'},
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    install_requires=['requests']
+)
