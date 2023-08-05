6ebdf433a45c7956af29f1482646e1b7ad887ffa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:57 +0000

    Add package Melchizedek/0.0.2/Melchizedek-0.0.2.tar.gz

---
+++ packages/Melchizedek/Melchizedek-0.0.2.tar.gz/Melchizedek-0.0.2/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Melchizedek", # It should be unique name
+    version="0.0.2",
+    author="Hugo Six",
+    author_email="hugo.six.pro@gmail.com",
+    description="Hugo's Personnal package for own code reuse",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+#    url="https://mysite.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+install_requires=[
+        "seaborn"
+        "matplotlib"
+        "numpy==1.19.2"
+        "scipy"
+        "pandas"
+        "scikit_learn"
+        "pytest"
+        "tensorflow"
+    ],
+    python_requires='>=3.8',
+)
