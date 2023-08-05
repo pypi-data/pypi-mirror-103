bf165c6999538cd9385f81afa5c6efad874ac9e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:25 +0000

    Add package test-import-faiss-atran/0.5/test_import_faiss_atran-0.5.tar.gz

---
+++ packages/test-import-faiss-atran/test_import_faiss_atran-0.5.tar.gz/test_import_faiss_atran-0.5/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='test_import_faiss_atran',
+      version='0.5',
+      description='Basic Test',
+      url=' ',
+      author='Faisal',
+      author_email='muhammad.faisal@altran.com',
+      license='',
+      packages=['test_import_faiss_atran'],
+      zip_safe=False)
