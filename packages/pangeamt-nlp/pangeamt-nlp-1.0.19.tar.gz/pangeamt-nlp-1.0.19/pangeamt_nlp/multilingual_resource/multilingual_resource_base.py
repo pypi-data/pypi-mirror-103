e5b06e8f987cd597c706e5f75c1358a5bfa6d2e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:30 +0000

    Add package pangeamt-nlp/1.0.19/pangeamt-nlp-1.0.19.tar.gz

---
+++ packages/pangeamt-nlp/pangeamt-nlp-1.0.19.tar.gz/pangeamt-nlp-1.0.19/pangeamt_nlp/multilingual_resource/multilingual_resource_base.py
@@ -0,0 +1,12 @@
+class MultilingualResourceBase:
+    TYPE_TMX = 'tmx'
+    TYPE_AF = 'af'
+    TYPE_AFTXT = 'aftxt'
+    TYPE_BILINGUAL = 'bilingual'
+
+    def __init__(self, type):
+        self._multilingual_resource_type = type
+
+    def get_multilingual_resource_type(self):
+        return self._multilingual_resource_type
+    multilingual_resource_type = property(get_multilingual_resource_type)
