7272e67c86d3110456484055205ecedebdb0d307
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:02 +0000

    Add package airstore/0.0.0/airstore-0.0.0.tar.gz

---
+++ packages/airstore/airstore-0.0.0.tar.gz/airstore-0.0.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'airstore',
+    version = '0.0.0',
+    description = 'This is only a placeholder package.',
+)
+
+raise RuntimeError("This is a placeholder package that was never meant to be actually installed from public PyPI. Please double check your installation instructions.")
