68e42c13971766e1b1fb6d7ba757bf1d21786f16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:27 +0000

    Add package distributions-jona/0.1/distributions_jona-0.1.tar.gz

---
+++ packages/distributions-jona/distributions_jona-0.1.tar.gz/distributions_jona-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='distributions_jona',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      email='jonatas.iw@gmail.com',
+      author='Jonatas Oliveira',
+      zip_safe=False)
