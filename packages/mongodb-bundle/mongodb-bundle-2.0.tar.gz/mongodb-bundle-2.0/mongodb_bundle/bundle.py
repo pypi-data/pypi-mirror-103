f59249c7b908c40338d79f46c5f01a342b387471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:25 +0000

    Add package mongodb-bundle/2.0/mongodb-bundle-2.0.tar.gz

---
+++ packages/mongodb-bundle/mongodb-bundle-2.0.tar.gz/mongodb-bundle-2.0/mongodb_bundle/bundle.py
@@ -0,0 +1,39 @@
+from applauncher.event import ConfigurationReadyEvent
+from pymongo import MongoClient
+from pydantic import BaseModel
+from dependency_injector import containers, providers
+from applauncher.applauncher import Configuration
+
+
+class MongoDBConfig(BaseModel):
+    uri: str
+    connect: bool = True
+
+
+class MongoDBContainer(containers.DeclarativeContainer):
+    config = providers.Dependency(instance_of=MongoDBConfig)
+    configuration = Configuration()
+    mongo_client = providers.Singleton(
+        MongoClient,
+        host=configuration.provided.mongodb.uri,
+        connect=configuration.provided.mongodb.connect
+    )
+
+
+class MongoDBBundle:
+    def __init__(self):
+        self.config_mapping = {
+            "mongodb": MongoDBConfig
+        }
+
+        self.event_listeners = [
+            (ConfigurationReadyEvent, self.config_ready),
+        ]
+
+        self.injection_bindings = {
+
+        }
+
+    def config_ready(self, event):
+        self.injection_bindings["mongodb"] = MongoDBContainer(config=event.configuration.mongodb)
+
