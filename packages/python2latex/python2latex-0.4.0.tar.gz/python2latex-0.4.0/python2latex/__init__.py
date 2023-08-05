df1f47bf07ffdc00fad4dcaed630b77b6c485835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:13:17 +0000

    Add package python2latex/0.4.0/python2latex-0.4.0.tar.gz

---
+++ packages/python2latex/python2latex-0.4.0.tar.gz/python2latex-0.4.0/python2latex/__init__.py
@@ -0,0 +1,12 @@
+# Basics must be loaded first
+from .tex_base import *
+from .tex_environment import TexEnvironment, Label
+
+# Other features
+from .document import Document, Section, Subsection
+from .color import *
+from .colormap import *
+from .floating_environment import FloatingFigure, FloatingTable, FloatingEnvironmentMixin
+from .plot import Plot, LinePlot, MatrixPlot
+from .template import Template
+from .table import Table, Tabular
