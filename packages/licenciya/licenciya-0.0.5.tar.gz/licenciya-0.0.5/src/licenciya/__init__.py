c5a1ef4eac8501356cec2c96c5de64c42863b962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:33 +0000

    Add package licenciya/0.0.5/licenciya-0.0.5.tar.gz

---
+++ packages/licenciya/licenciya-0.0.5.tar.gz/licenciya-0.0.5/src/licenciya/__init__.py
@@ -0,0 +1,6 @@
+from src.licenciya.main import Licenciya
+from src.licenciya import exceptions
+from src.licenciya.helper import AdminManageLicense
+
+__version__ = "0.0.5"
+__author__ = 'amiwrpremium'
