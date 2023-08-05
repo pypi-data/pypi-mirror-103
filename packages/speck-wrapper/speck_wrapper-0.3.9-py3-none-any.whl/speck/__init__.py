6f26656f4bc5667ea3148d14b0337c68bc2c0d7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:12 +0000

    Add package speck-wrapper/0.3.9/speck_wrapper-0.3.9-py3-none-any.whl

---
+++ packages/speck-wrapper/speck_wrapper-0.3.9-py3-none-any.whl/speck/__init__.py
@@ -0,0 +1,18 @@
+"""
+Speck
+=====
+A minimal weatherapi API wrapper.
+
+Licensed under the MIT license. See LICENSE for more information.
+"""
+
+__title__ = 'speck'
+__authors__ = [
+    'Nevin Jose',
+    'Sachin Cherian'
+]
+__version__ = 'v0.3.9'
+
+from .cache import *
+from .client import *
+from .errors import *
