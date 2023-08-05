138f76052a0643163da03269ecedcddc3974675a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:14 +0000

    Add package vex-cog-utils/1.1.2/vex-cog-utils-1.1.2.tar.gz

---
+++ packages/vex-cog-utils/vex-cog-utils-1.1.2.tar.gz/vex-cog-utils-1.1.2/setup.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from setuptools import setup
+
+exc: dict = {}
+with open(Path(__file__).parent / "vexcogutils" / "version.py") as fp:
+    exec(fp.read(), exc)
+version = exc["__version__"]
+
+
+setup(
+    name="vex-cog-utils",
+    version=version,
+    url="https://github.com/Vexed01/vex-cog-utils",
+    author="Vexed01",
+    author_email="gh.vexed@gmail.com",
+    description="Utility functions for the Vex-Cogs repo.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    packages=["vexcogutils"],
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8.1",
+)
---
+++ packages/vex-cog-utils/vex-cog-utils-1.1.2.tar.gz/vex-cog-utils-1.1.2/vexcogutils/__init__.py
@@ -0,0 +1,2 @@
+from .chat import humanize_bytes, inline_hum_list
+from .meta import format_help, format_info
