caa03a4cb631920788bf6fa00b77ed040539088c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:39 +0000

    Add package hydra-fair-plugins/0.0.1/hydra-fair-plugins-0.0.1.tar.gz

---
+++ packages/hydra-fair-plugins/hydra-fair-plugins-0.0.1.tar.gz/hydra-fair-plugins-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = 'hydra-fair-plugins',
+    version = '0.0.1',
+    description = 'This is only a placeholder package.',
+)
+
+raise RuntimeError("This is a dummy package that was never ment to be actually installed from public PyPI. Please double check your installation instructions.")
