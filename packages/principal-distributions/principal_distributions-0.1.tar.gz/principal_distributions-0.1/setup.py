f26e568eed9622fc777dc6d674a0d28fec4f086e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:42 +0000

    Add package principal-distributions/0.1/principal_distributions-0.1.tar.gz

---
+++ packages/principal-distributions/principal_distributions-0.1.tar.gz/principal_distributions-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='principal_distributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['principal_distributions'],
+      zip_safe=False)
