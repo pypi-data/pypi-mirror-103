11c4c5dd391b5345dcde1d8baf9f58681d7909d8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:40 +0000

    Add package graphmanagerlib/2.3.3/graphmanagerlib-2.3.3.tar.gz

---
+++ packages/graphmanagerlib/graphmanagerlib-2.3.3.tar.gz/graphmanagerlib-2.3.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='graphmanagerlib',
+    packages=find_packages(),
+    version='2.3.3',
+    description='My first Python library',
+    author='Cambier Damien',
+    license='MIT',
+    install_requires=['tensorflow',
+                      'pandas',
+                      'networkx',
+                      'torch==1.8.0',
+                      'torch-geometric',
+                      'bpemb',
+                      'sklearn',
+                      'matplotlib',
+                      'python-Levenshtein',
+                      'opencv-python',
+                      'sentence_transformers',
+                      'stellargraph'],
+)
