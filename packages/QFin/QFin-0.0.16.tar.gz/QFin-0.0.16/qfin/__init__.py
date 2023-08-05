ca1272f78431144393cfbfadb82b1b94bab7d0ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:04 +0000

    Add package QFin/0.0.16/QFin-0.0.16.tar.gz

---
+++ packages/QFin/QFin-0.0.16.tar.gz/QFin-0.0.16/qfin/__init__.py
@@ -0,0 +1,7 @@
+from qfin.options import BlackScholesCall
+from qfin.options import BlackScholesPut
+from qfin.simulations import GeometricBrownianMotion
+from qfin.simulations import MonteCarloBinaryCall
+from qfin.simulations import MonteCarloBinaryPut
+from qfin.simulations import MonteCarloCall
+from qfin.simulations import MonteCarloPut
