c1d233e6d4bceba799b955ff52d734c65b3f3f88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:32 +0000

    Add package quenouille/1.1.0/quenouille-1.1.0.tar.gz

---
+++ packages/quenouille/quenouille-1.1.0.tar.gz/quenouille-1.1.0/quenouille/constants.py
@@ -0,0 +1,12 @@
+# =============================================================================
+# Quenouille Constants
+# =============================================================================
+#
+# Handy constants.
+#
+
+# A sentinel value for the output queue to know when to stop
+THE_END_IS_NIGH = object()
+
+# Default buffer size for imap iterators
+DEFAULT_BUFFER_SIZE = 1024
