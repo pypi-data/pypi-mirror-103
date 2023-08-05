f841a9c8b910996df781c09420184ca68ac5d71c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:33 +0000

    Add package sym-flow-cli/0.1.25/sym-flow-cli-0.1.25.tar.gz

---
+++ packages/sym-flow-cli/sym-flow-cli-0.1.25.tar.gz/sym-flow-cli-0.1.25/sym/flow/cli/commands/users/template.py
@@ -0,0 +1,33 @@
+import csv
+import json
+import os
+import sys
+
+import click
+import xattr
+
+from sym.flow.cli.helpers.global_options import GlobalOptions
+from sym.flow.cli.helpers.sym_api_client import SymAPIClient
+from sym.flow.cli.helpers.sym_api_service import SymAPIService
+
+
+@click.command(name="template", short_help="Create a CSV template for new Users.")
+@click.make_pass_decorator(GlobalOptions, ensure=True)
+@click.argument("path", type=click.Path(writable=True))
+@click.option("-f/--force", default=False)
+def users_template(options: GlobalOptions, path: str, force: bool) -> None:
+    api_service = SymAPIService(api_client=SymAPIClient(url=options.api_url))
+    integrations = api_service.get_user_integrations()
+    fields = ["email"] + [x["name"] for x in integrations]
+
+    if not force and os.path.exists(path):
+        click.secho(f"File already exists: '{path}'", fg="red", bold=True)
+        click.secho("Try running `symflow users template --force PATH`.", fg="cyan")
+        sys.exit(1)
+
+    with open(path, "w") as f:
+        writer = csv.DictWriter(f, fieldnames=fields)
+        writer.writeheader()
+
+    connectors = json.dumps({i["name"]: i["type"] for i in integrations}).encode("utf-8")
+    xattr.xattr(path).update({"user.sym.connectors": connectors})
