41de5e6099defe05ee30bd5e6d7b3b0ba04252a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:27 +0000

    Add package NulledAPI/1.0.5/NulledAPI-1.0.5.tar.gz

---
+++ packages/NulledAPI/NulledAPI-1.0.5.tar.gz/NulledAPI-1.0.5/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+from pypandoc import convert_file
+
+VERSION = '1.0.5' 
+DESCRIPTION = 'NulledAPI - UnOfficial Nulled API'
+with open('readme.md', encoding='utf-8') as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+        name="NulledAPI", 
+        version=VERSION,
+        author="M3GZ",
+        author_email="megzfromnulled@gmail.com",
+        url="https://www.nulled.to/user/4103370-m3gz",
+        description=DESCRIPTION,
+        long_description=LONG_DESCRIPTION,
+        long_description_content_type='text/markdown',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'Programming Language :: Python :: 3.0',
+            'Intended Audience :: Developers',
+            'Topic :: Utilities'
+        ],
+        packages=find_packages(),
+        install_requires=[
+            'requests==2.25.1'
+        ]
+)
