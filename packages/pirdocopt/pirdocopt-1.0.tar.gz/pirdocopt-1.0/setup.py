fc5a5cdc9c58c808ae4419fcd30e0d41211ea348
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:06:45 +0000

    Add package pirdocopt/1.0/pirdocopt-1.0.tar.gz

---
+++ packages/pirdocopt/pirdocopt-1.0.tar.gz/pirdocopt-1.0/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='pirdocopt',
+    version='1.0',
+    packages=['pirdocopt'],
+    license='MIT License',
+    long_description=open('README.rst').read(),
+)
