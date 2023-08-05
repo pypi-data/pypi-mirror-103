5c9c12be4e3207b1724cd3875fea3bfd0dfb76ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:45 +0000

    Add package instareels/0.1.1/instareels-0.1.1.tar.gz

---
+++ packages/instareels/instareels-0.1.1.tar.gz/instareels-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from  setuptools import setup
+setup(name="instareels",
+version="0.1.1",description="This can download instagram reels",author="@tanay_mishra",packages=['download_reels'],
+author_email="tanaymishra2204@gmail.com",install_requires=['requests-HTML','requests'],
+long_description="""This module can download Instagram reels video 
+to download import download function from module and call with parameters 
+like that download('link','videos/video.mp4')..
+If it dosent work then just Visit : "https://github.com/tanaymishra/reels" to get the code..
+Because this Module dose not works sometime.. 
+and visit "https://github.com/tanaymishra/Reels-instruction For instruction"
+"""
+)
