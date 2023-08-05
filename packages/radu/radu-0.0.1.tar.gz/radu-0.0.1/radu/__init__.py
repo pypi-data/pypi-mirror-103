9e74ccd9deacf36e1553f6db662bb50103a2152d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:17 +0000

    Add package radu/0.0.1/radu-0.0.1.tar.gz

---
+++ packages/radu/radu-0.0.1.tar.gz/radu-0.0.1/radu/__init__.py
@@ -0,0 +1,10 @@
+class Radu(object):
+    def __init__(self):
+        self.iq = -1000
+        self.prost = True
+        self.destept = False
+        self.mate = "LOL! RADU NU STIE MATE HAHAHAHAH!"
+        self.bot = True
+
+    def calculeaza_adunare(self, nr1, nr2):
+        return nr1 * nr2
