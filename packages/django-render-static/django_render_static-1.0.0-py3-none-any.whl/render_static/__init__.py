30775e9a6b598b3319131e2cc9cc787665ae1172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:15:15 +0000

    Add package django-render-static/1.0.0/django_render_static-1.0.0-py3-none-any.whl

---
+++ packages/django-render-static/django_render_static-1.0.0-py3-none-any.whl/render_static/__init__.py
@@ -0,0 +1,18 @@
+r"""
+    ____                 __             _____ __        __  _
+   / __ \___  ____  ____/ /__  _____   / ___// /_____ _/ /_(_)____
+  / /_/ / _ \/ __ \/ __  / _ \/ ___/   \__ \/ __/ __ `/ __/ / ___/
+ / _, _/  __/ / / / /_/ /  __/ /      ___/ / /_/ /_/ / /_/ / /__
+/_/ |_|\___/_/ /_/\__,_/\___/_/      /____/\__/\__,_/\__/_/\___/
+
+"""
+from .url_tree import ClassURLWriter, SimpleURLWriter, URLTreeVisitor
+
+__title__ = 'Django Render Static'
+__version__ = '1.0.0'
+__author__ = 'Brian Kohan'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021 Brian Kohan'
+
+
+default_app_config = 'render_static.apps.RenderStaticConfig'  # pylint: disable=C0103
