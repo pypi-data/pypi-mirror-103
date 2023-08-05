2438ae6d0eb24f3182ed492e6d9e09a8c6e2325d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:30 +0000

    Add package yamlflow/0.0.8/yamlflow-0.0.8-py3-none-any.whl

---
+++ packages/yamlflow/yamlflow-0.0.8-py3-none-any.whl/yamlflow/cli/__init__.py
@@ -0,0 +1,17 @@
+import click
+
+from yamlflow.cli.commands import (
+    version,
+    init,
+    build
+)
+
+@click.group()
+def main():
+    """Yet Another ML flow"""
+    pass
+
+
+main.add_command(version)
+main.add_command(init)
+main.add_command(build)
---
+++ packages/yamlflow/yamlflow-0.0.8-py3-none-any.whl/yamlflow/cli/constants.py
@@ -0,0 +1,7 @@
+import os
+
+BASE_DIR = ".yamlflow"
+MODEL_DIR = "model"
+SERVICE_DIR = "service"
+
+MANIFEST_FILE = "yamlflow.yaml"
