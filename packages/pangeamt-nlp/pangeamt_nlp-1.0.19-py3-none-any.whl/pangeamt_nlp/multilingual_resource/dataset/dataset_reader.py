b9de349cf3791cb2de5519e74a63504a1694c391
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:28 +0000

    Add package pangeamt-nlp/1.0.19/pangeamt_nlp-1.0.19-py3-none-any.whl

---
+++ packages/pangeamt-nlp/pangeamt_nlp-1.0.19-py3-none-any.whl/pangeamt_nlp/multilingual_resource/dataset/dataset_reader.py
@@ -0,0 +1,22 @@
+class DatasetReader:
+    def __init__(self, tmx_reader = None, af_reader = None, aftxt_reader = None, bilingual_reader = None):
+        self._tmx_reader = tmx_reader
+        self._af_reader = af_reader
+        self._aftxt_reader = aftxt_reader
+        self._bilingual_reader = bilingual_reader
+
+    def get_tmx_reader(self):
+        return self._tmx_reader
+    tmx_reader = property(get_tmx_reader)
+
+    def get_af_reader(self):
+        return self._af_reader
+    af_reader = property(get_af_reader)
+
+    def get_aftxt_reader(self):
+        return self._aftxt_reader
+    aftxt_reader = property(get_aftxt_reader)
+
+    def get_bilingual_reader(self):
+        return self._bilingual_reader
+    bilingual_reader = property(get_bilingual_reader)
