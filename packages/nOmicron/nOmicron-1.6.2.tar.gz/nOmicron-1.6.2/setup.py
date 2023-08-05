4d1158dbd4aee6c6d85950e888abd88b2ed523de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:53 +0000

    Add package nOmicron/1.6.2/nOmicron-1.6.2.tar.gz

---
+++ packages/nOmicron/nOmicron-1.6.2.tar.gz/nOmicron-1.6.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nOmicron',
+    version='1.6.2',
+    url='https://github.com/OGordon100/nOmicron',
+    license='GPL v3.0',
+    author='Oliver Gordon',
+    author_email='oliver.gordon@nottingham.ac.uk',
+    description="Python Control for Scienta Omicron Matrix",
+    long_description="nOmicron package/API to automatically control Matrix through Python",
+    packages=find_packages(),
+    install_requires=['numpy', 'scipy', 'tqdm', 'matplotlib', 'natsort', 'pefile', 'psutil', 'selenium', 'chromedriver_autoinstaller', 'bs4', 'lxml'],
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: Microsoft :: Windows"]
+)
