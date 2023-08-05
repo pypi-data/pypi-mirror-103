046fd1d94d62bf7468a850a14f0aa2f566fda6ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:19 +0000

    Add package zp3/2.0.0.8/zp3-2.0.0.8.tar.gz

---
+++ packages/zp3/zp3-2.0.0.8.tar.gz/zp3-2.0.0.8/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['zp3']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'zp3',
+    'version': '2.0.0.8',
+    'description': 'Example Package for Zühlke Python Course',
+    'long_description': None,
+    'author': 'Iwan Silvan Bolzern',
+    'author_email': 'iwan.bolzern@bluewin.ch',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
