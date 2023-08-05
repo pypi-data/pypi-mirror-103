ab26d95d8c83e9d01f2799093b20f377530aceec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:49 +0000

    Add package grilled-common/0.2.2/grilled-common-0.2.2.tar.gz

---
+++ packages/grilled-common/grilled-common-0.2.2.tar.gz/grilled-common-0.2.2/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['grilled', 'grilled.common', 'grilled.common.data_structure']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['logging>=0.4.9.6,<0.5.0.0', 'pydantic<=1.7.1']
+
+setup_kwargs = {
+    'name': 'grilled-common',
+    'version': '0.2.2',
+    'description': '',
+    'long_description': None,
+    'author': 'hans',
+    'author_email': 'dxzenghan@163.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
