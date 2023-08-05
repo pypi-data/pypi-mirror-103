e674253c152aefd6ba6d793c0a6c45e424f1de34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:14:13 +0000

    Add package airflow-kubernetes-job-operator-latest/0.0.1/airflow_kubernetes_job_operator_latest-0.0.1.tar.gz

---
+++ packages/airflow-kubernetes-job-operator-latest/airflow_kubernetes_job_operator_latest-0.0.1.tar.gz/airflow_kubernetes_job_operator_latest-0.0.1/setup.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+    setup.py
+    ~~~~~~~~
+
+    An airflow job operator that executes a task as a Kubernetes job on a cluster,
+given a job yaml configuration or an image uri.
+
+    :copyright: (c) 2020 by zav.
+    :license: see LICENSE for more details.
+"""
+
+import os
+from setuptools import setup
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    version_file_path = os.path.join(here, "package_version.txt")
+    if not os.path.isfile(version_file_path):
+        return "0.0.1"
+    version = None
+    with open(version_file_path, "r") as raw:
+        version = raw.read()
+
+    return version
+
+
+setup(
+    name="airflow_kubernetes_job_operator_latest",
+    version=get_version(),
+    description="An airflow job operator that executes a task as a Kubernetes job on a cluster, given a job yaml configuration or an image uri.",
+    long_description="Please see readme.md @ https://github.com/Fahadsaadullahkhan/KubernetesJobOperator",
+    classifiers=[],
+    author="Zav Shotan",
+    author_email="",
+    url="https://github.com/Fahadsaadullahkhan/KubernetesJobOperator",
+    packages=["airflow_kubernetes_job_operator",
+              "airflow_kubernetes_job_operator/kube_api"],
+    platforms="any",
+    license="LICENSE",
+    install_requires=[
+        "PyYAML>=5.0",
+        "kubernetes>=8.0.1",
+        "urllib3>=1.25.0",
+        "zthreading>=0.1.13",
+        "python-dateutil>=2.8.1",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+)
