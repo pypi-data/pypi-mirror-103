4d7aa733ee13a6d92264ed02b3f2f6cfe3334303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:14:32 +0000

    Add package Gaussian-Binomial-dist-SL/1.0/Gaussian_Binomial_dist_SL-1.0.tar.gz

---
+++ packages/Gaussian-Binomial-dist-SL/Gaussian_Binomial_dist_SL-1.0.tar.gz/Gaussian_Binomial_dist_SL-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='Gaussian_Binomial_dist_SL',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['Gaussian_Binomial_dist_SL'],
+      author='Sikang Luan',
+      author_email='micluan41@gmail.com',
+      zip_safe=False)
