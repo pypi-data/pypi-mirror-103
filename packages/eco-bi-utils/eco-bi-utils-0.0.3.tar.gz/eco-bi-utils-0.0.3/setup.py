830049d29a181495bb8515ce034d6c5628a76696
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:44 +0000

    Add package eco-bi-utils/0.0.3/eco-bi-utils-0.0.3.tar.gz

---
+++ packages/eco-bi-utils/eco-bi-utils-0.0.3.tar.gz/eco-bi-utils-0.0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="eco-bi-utils",
+    version="0.0.3",
+    author="Bismark Adomako",
+    author_email="bismark.adomako@bizmaercq.ocm",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/pypa/sampleproject",
+    project_urls={
+        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
---
+++ packages/eco-bi-utils/eco-bi-utils-0.0.3.tar.gz/eco-bi-utils-0.0.3/src/eco_bi_utils/twitter/authentication.py
@@ -0,0 +1,31 @@
+
+class Authentication:
+
+    def __init__(self, **kwargs) -> None:
+        """
+        Authentication credentials to access the twitter api. These should be changed to ecobank twitter app
+        and store in Azure Key vault before going to production.
+
+        :param kwargs:
+                API_KEY: consumer_key	Your consumer key
+                API_SECRET_KEY: consumer_secret	Your consumer secret
+                ACCESS_TOKEN: access_token	Your access token
+                ACCESS_TOKEN_SECRET: token_secret	Your access token secret
+                BEARER_TOKEN: bearer_token	Your bearer token (MANDATORY)
+        """
+
+        params = {
+            'API_KEY': None,
+            'API_SECRET_KEY': None,
+            'BEARER_TOKEN': None,
+            'ACCESS_TOKEN': None,
+            'ACCESS_TOKEN_SECRET': None,
+        }
+
+        for kwarg in kwargs:
+            if kwarg in params:
+                params[kwarg] = kwargs[kwarg]
+
+        self.HEADERS = {
+            "Authorization": f"Bearer {params['BEARER_TOKEN']}"
+        }
