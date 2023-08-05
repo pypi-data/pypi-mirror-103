18aa15c7e74412c13dddfa79a5b8b1fa0752e5ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:14:06 +0000

    Add package hamolicious-vector-class/0.1.7/hamolicious_vector_class-0.1.7-py3-none-any.whl

---
+++ packages/hamolicious-vector-class/hamolicious_vector_class-0.1.7-py3-none-any.whl/vector/test.py
@@ -0,0 +1,62 @@
+
+from vector2d import Vec2d
+from vector3d import Vec3d
+import click
+
+click.clear()
+
+v = Vec2d(0.5, 0.5)
+print(v.x, v.y)
+
+v = Vec2d.random_unit()
+print(v.x, v.y)
+
+v = Vec2d.random_pos()
+print(v.x, v.y)
+
+v = Vec2d.random_pos()
+print(v.get())
+
+v = Vec2d.random_pos()
+print(v.get_int())
+
+v = Vec2d.random_pos()
+print(v.copy())
+
+v = Vec2d.random_pos()
+print(v[0], v[1])
+
+v = Vec2d.random_pos() * [10, 10]
+print(v.get())
+
+#---------------------
+
+v = Vec3d(0.5, 0.5, 0.5)
+print(v.x, v.y, v.z)
+
+v = Vec3d.random_unit()
+print(v.x, v.y, v.z)
+
+v = Vec3d.random_unit()
+print(v.w, v.h, v.d)
+
+v = Vec3d.random_unit()
+print(v.r, v.g, v.b)
+
+v = Vec3d.random_pos()
+print(v.x, v.y, v.z)
+
+v = Vec3d.random_pos()
+print(v.get())
+
+v = Vec3d.random_pos()
+print(v.get_int())
+
+v = Vec3d.random_pos()
+print(v.copy())
+
+v = Vec3d.random_pos()
+print(v[0], v[1], v[2])
+
+v = Vec3d.random_pos() * [10, 10, 10]
+print(v.get())
