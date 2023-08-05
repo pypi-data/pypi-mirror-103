8a3f7c83a66b7fe06db4fd680be3720adc32669a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:00 +0000

    Add package xorencryption/0.1.6/xorencryption-0.1.6.tar.gz

---
+++ packages/xorencryption/xorencryption-0.1.6.tar.gz/xorencryption-0.1.6/setup.py
@@ -0,0 +1,33 @@
+"""
+Module: setup.py
+Created by alvif@usagi 
+on 20/04/21
+"""
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='xorencryption',
+    packages=setuptools.find_packages(where="src"),
+    version='0.1.6',
+    license='MIT',
+    description='little Python library for encrypt and decrypt using XOR operation.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='Alvif Sandana Mahardika',
+    author_email='alvifsandana@gmail.com',
+    url='https://github.com/AlvifSandana/xorencryption',
+    download_url='https://github.com/AlvifSandana/xorencryption/',
+    keywords=['XOR', 'LEARN', 'CRYPTOGRAPHY'],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+    ],
+    package_dir={"": "src"},
+    python_requires=">=3.7",
+)
---
+++ packages/xorencryption/xorencryption-0.1.6.tar.gz/xorencryption-0.1.6/src/xorencryption/__init__.py
@@ -0,0 +1,9 @@
+"""
+Module: __init__.py
+Created by alvif@usagi 
+on 20/04/21
+"""
+
+from .xor import XOREncryption
+
+__all__ = ["XOREncryption"]
