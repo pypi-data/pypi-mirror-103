36cec79336289dfc673c23622b24ab5cdc93f342
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:34 +0000

    Add package ocrd-fork-pylsd/0.0.4/ocrd-fork-pylsd-0.0.4.tar.gz

---
+++ packages/ocrd-fork-pylsd/ocrd-fork-pylsd-0.0.4.tar.gz/ocrd-fork-pylsd-0.0.4/setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Date    : 2019-01-08 09:32:00
+# @Author  : Gefu Tang (tanggefu@gmail.com)
+# @Link    : https://github.com/primetang/pylsd
+# @Version : 0.0.3
+
+from setuptools import setup, Extension
+
+clib = Extension('pylsd.lib',
+                 sources=['source/src/lsd.cpp'],
+                 include_dirs=['source/include'],
+                 depends=['source/include/lsd.h'],
+                 language="c++")
+
+setup(
+    name='ocrd-fork-pylsd',
+    version='0.0.4',
+    description='pylsd is the python bindings for LSD - Line Segment Detector',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Gefu Tang',
+    author_email='tanggefu@gmail.com',
+    maintainer='bertsky,kba',
+    license='BSD',
+    keywords=["LSD", 'line segmentation'],
+    url='https://github.com/kba/pylsd',
+    packages=['pylsd', 'pylsd.bindings'],
+    install_requires=['numpy'],
+    ext_modules=[clib],
+)
