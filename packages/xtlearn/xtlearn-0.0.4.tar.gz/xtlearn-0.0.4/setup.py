142dbbf958e8a342977b633525a51a58a0dc53af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:12:38 +0000

    Add package xtlearn/0.0.4/xtlearn-0.0.4.tar.gz

---
+++ packages/xtlearn/xtlearn-0.0.4.tar.gz/xtlearn-0.0.4/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    readme = fh.read()
+
+setup(
+    name="xtlearn",
+    version="0.0.4",
+    url="https://github.com/emdemor/xtlearn",
+    license="MIT License",
+    author="Eduardo M. de Morais",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email="emdemor415@gmail.com",
+    keywords="covid-19",
+    description=u"This is a package with classes to be used in sklearn pipelines with pandas dataframes",
+    packages=["xtlearn"],
+    install_requires=[
+        "numpy>=1.18.2",
+        "scipy>=1.6.1",
+        "pandas>=1.0.3",
+        "setuptools",
+        "tqdm",
+    ],
+)
