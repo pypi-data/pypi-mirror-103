3f43d725fc72b9e3c2a8090b2fbcbe8d75d5a252
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:01 +0000

    Add package randomimg/0.0.0/randomimg-0.0.0.tar.gz

---
+++ packages/randomimg/randomimg-0.0.0.tar.gz/randomimg-0.0.0/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name='randomimg',
+      description='Random image generator',
+      author='Adam Jenca',
+      url="https://gtsforum.xyz",
+      author_email='jenca.a@gjh.sk',
+)
