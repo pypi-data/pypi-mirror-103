ba95aa3a274dd9833c3ed6faf13244943fcbec4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:10 +0000

    Add package Undo-Redo/1.0/Undo_Redo-1.0.tar.gz

---
+++ packages/Undo-Redo/Undo_Redo-1.0.tar.gz/Undo_Redo-1.0/Undo_Redo/__init__.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+
+class UndoRedo:
+    def __init__(self):
+        self._stack = deque()
+        self._index = -1
+
+    def append(self, num):
+        self.pop()
+        self._stack.append(num)
+        self._index += 1
+
+    def pop(self):
+        l = len(self._stack)
+        for i in reversed(range(self._index + 1, l)):
+            self._stack.pop()
+
+    def undo(self):
+        if self._index > 0:
+            self._index -= 1
+
+    def undoText(self):
+        i=self._index
+        if i> 0:
+            return self._stack[i - 1]
+        else:
+            return self._stack[0]
+
+    def redo(self):
+        indx=len(self._stack)
+        if self._index < (indx - 1):
+            self._index += 1
+
+    def redoText(self):
+        indx = len(self._stack)
+        i=self._index
+        if i < (indx - 1):
+            return self._stack[i + 1]
+        else:
+            return self._stack[indx - 1]
+
+
+
