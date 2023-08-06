5025cd5304f26753b7d80fabe39c2bf4198d1319
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:33:54 +0000

    Add package warna/1.0.2/warna-1.0.2.tar.gz

---
+++ packages/warna/warna-1.0.2.tar.gz/warna-1.0.2/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+	long_description = fh.read()
+
+github = 'https://github.com/mzaini30/warna'
+
+setuptools.setup(
+	name="warna", # Replace with your own username
+	version="1.0.2",
+	author="Zen",
+	author_email="muhzaini30@gmail.com",
+	description="Menambahkan warna di Terminal",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url=github,
+	project_urls={
+		"Bug Tracker": github + "/issues",
+	},
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	package_dir={"": "src"},
+	packages=setuptools.find_packages(where="src"),
+	python_requires=">=3.6",
+)
