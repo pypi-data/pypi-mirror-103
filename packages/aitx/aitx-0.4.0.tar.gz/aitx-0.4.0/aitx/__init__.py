c8c0fa44a82796b360b4d9d688f8776dd85d6795
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:38 +0000

    Add package aitx/0.4.0/aitx-0.4.0.tar.gz

---
+++ packages/aitx/aitx-0.4.0.tar.gz/aitx-0.4.0/aitx/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from aitx.lightbox import *
+from aitx.history import *
+import os
+__author__ = 'Chia-Jung, Yang'
+__email__ = 'jeroyang@gmail.com'
+__version__ = '0.4.0'
