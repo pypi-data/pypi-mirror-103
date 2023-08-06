e4b951416acf0647ae32fa410dcec69158875b1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:57 +0000

    Add package guvva-m/2.0.0/guvva-m-2.0.0.tar.gz

---
+++ packages/guvva-m/guvva-m-2.0.0.tar.gz/guvva-m-2.0.0/README.md
@@ -0,0 +1,23 @@
+Statement_Writeln,Statement_Write = print statements for single line and new line,
+__________ 
+
+If_these_equals_then = if statement,
+
+_______________________________________
+
+WhenREALrun = infinite loop,
+
+__________________________
+
+Code_Rewrite_with = replace,
+
+Reverse_text,
+
+User_Input_Get,
+
+intTOstring,
+
+stringToint,
+
+Init_op_System,
+System_Beep,
---
+++ packages/guvva-m/guvva-m-2.0.0.tar.gz/guvva-m-2.0.0/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="guvva-m",
+    version="2.0.0",
+    author="MasterMind",
+    author_email="mastermindm2rd@gmail.com",
+    description="A tough programing language for mac",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://pypi.org/project/guvva/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
