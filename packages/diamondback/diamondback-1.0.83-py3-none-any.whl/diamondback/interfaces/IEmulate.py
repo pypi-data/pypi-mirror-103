4e0c459948e62d1f7b22a20672e99792a4ddabf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:30 +0000

    Add package diamondback/1.0.83/diamondback-1.0.83-py3-none-any.whl

---
+++ packages/diamondback/diamondback-1.0.83-py3-none-any.whl/diamondback/interfaces/IEmulate.py
@@ -0,0 +1,81 @@
+""" **Description**
+
+        Emulate interface.
+
+    **Example**
+
+        ::
+
+            from diamondback import IEmulate
+
+
+            class Test( IEmulate ) :
+
+                def __init__( self ) -> None :
+
+                    super( ).__init__( )
+
+                    self.emulate = False
+
+            test = Test( )
+
+            test.emulate = True
+
+    **License**
+
+        `BSD-3C.  <https://github.com/larryturner/diamondback/blob/master/license>`_
+
+        © 2018 - 2021 Larry Turner, Schneider Electric Industries SAS. All rights reserved.
+
+    **Author**
+
+        Larry Turner, Schneider Electric, Analytics & AI, 2020-10-15.
+
+    **Definition**
+
+"""
+
+from diamondback.interfaces.IEqual import IEqual
+
+
+class IEmulate( IEqual ) :
+
+    """ Emulate interface.
+    """
+
+    @property
+    def emulate( self ) :
+
+        """ emulate : any.
+        """
+
+        return self._emulate
+
+    @emulate.setter
+    def emulate( self, emulate : any ) :
+
+        self._emulate = emulate
+
+    def __eq__( self, other : any ) -> bool :
+
+        """ Equal.
+
+            Arguments :
+
+                other : any.
+
+            Returns :
+
+                equal : bool.
+        """
+
+        return ( ( super( ).__eq__( other ) ) and ( self.emulate == other.emulate ) )
+
+    def __init__( self ) -> None :
+
+        """ Initialize.
+        """
+
+        super( ).__init__( )
+
+        self._emulate = None
