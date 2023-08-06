36c49cf4dcea82cba5b86414729b196fd0111db5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:20 +0000

    Add package Honu/0.0.1/Honu-0.0.1.tar.gz

---
+++ packages/Honu/Honu-0.0.1.tar.gz/Honu-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="Honu",
+    install_requires=[
+        "graphics.py>=5.0.1.post1",
+    ],
+    extras_require={
+    },
+)
