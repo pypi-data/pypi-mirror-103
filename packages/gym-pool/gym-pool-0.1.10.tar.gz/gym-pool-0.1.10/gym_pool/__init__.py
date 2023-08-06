dae11f2097d4607bd67fd4a0cd112b052246438d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:10 +0000

    Add package gym-pool/0.1.10/gym-pool-0.1.10.tar.gz

---
+++ packages/gym-pool/gym-pool-0.1.10.tar.gz/gym-pool-0.1.10/gym_pool/__init__.py
@@ -0,0 +1,25 @@
+import logging
+from gym.envs.registration import register
+
+logger = logging.getLogger(__name__)
+
+register(
+    id='Pool-v0',
+    entry_point='gym_pool.envs:PoolEnv'
+)
+
+#register(
+#    id='SoccerEmptyGoal-v0',
+#    entry_point='gym_soccer.envs:SoccerEmptyGoalEnv',
+#    timestep_limit=1000,
+#    reward_threshold=10.0,
+#    nondeterministic = True,
+#)
+
+#register(
+#    id='SoccerAgainstKeeper-v0',
+#    entry_point='gym.envs:SoccerAgainstKeeperEnv',
+#    timestep_limit=1000,
+#    reward_threshold=8.0,
+#    nondeterministic = True,
+#)
