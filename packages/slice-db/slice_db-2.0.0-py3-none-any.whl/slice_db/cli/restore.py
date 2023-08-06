fd41830fb8e3b574c37511c1af5568b3ed936022
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:19 +0000

    Add package slice-db/2.0.0/slice_db-2.0.0-py3-none-any.whl

---
+++ packages/slice-db/slice_db-2.0.0-py3-none-any.whl/slice_db/cli/restore.py
@@ -0,0 +1,21 @@
+import contextlib
+
+import asyncpg
+
+from ..restore import RestoreIo, RestoreParams, restore
+from .common import open_bytes_read
+
+
+async def restore_main(args):
+    params = RestoreParams(
+        include_schema=args.include_schema,
+        parallelism=args.jobs,
+        transaction=args.transaction,
+    )
+
+    async with asyncpg.create_pool() as pool:
+        io = RestoreIo(
+            conn=lambda: pool.acquire(), input=lambda: open_bytes_read(args.input)
+        )
+
+        await restore(io, params)
