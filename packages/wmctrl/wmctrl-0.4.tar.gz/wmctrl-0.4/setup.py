7c6401006a92e99b20bbb07539ec6055d6e721c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:59 +0000

    Add package wmctrl/0.4/wmctrl-0.4.tar.gz

---
+++ packages/wmctrl/wmctrl-0.4.tar.gz/wmctrl-0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='wmctrl',
+    version='0.4',
+    author='Antonio Cuni',
+    author_email='anto.cuni@gmail.com',
+    py_modules=['wmctrl'],
+    url='https://github.com/antocuni/wmctrl',
+    license='MIT',
+    description='A tool to programmatically control windows inside X',
+    long_description='A tool to programmatically control windows inside X'
+)
