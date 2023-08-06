ddae8ccfb7d31c0aefb8f842b654f1591a405201
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:31 +0000

    Add package ctextinct/1.0.0/ctextinct-1.0.0.tar.gz

---
+++ packages/ctextinct/ctextinct-1.0.0.tar.gz/ctextinct-1.0.0/ctextinct/__main__.py
@@ -0,0 +1,14 @@
+def main():
+    # root = tk.Tk()
+    # cyber_chatbot = CyberChatbot(root)
+    # feedback_form = FeedbackForm(root)
+    # cyber_news = CyberNews(root)
+    # security_incident = SecurityIncident(root)
+    # ct_extinct = CTExtinct(root)
+    # app = CTExtinct()
+    # app.mainloop()
+
+    if __name__ == "__main__":
+        main()
+    else:
+        print('This module cannot be imported or used by another module. Please run code from app.py file')
