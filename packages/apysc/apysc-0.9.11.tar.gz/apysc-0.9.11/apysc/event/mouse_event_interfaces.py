3bcc1614a4ee2dc5904b32ad9048a7e0799c715c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:07 +0000

    Add package apysc/0.9.11/apysc-0.9.11.tar.gz

---
+++ packages/apysc/apysc-0.9.11.tar.gz/apysc-0.9.11/apysc/event/mouse_event_interfaces.py
@@ -0,0 +1,15 @@
+"""Class implementation for inheritance of each mouse event interfaces.
+"""
+
+from apysc.event.click_interface import ClickInterface
+from apysc.event.double_click_interface import DoubleClickInterface
+from apysc.event.mouse_down_interface import MouseDownInterface
+from apysc.event.mouse_over_interface import MouseOverInterface
+from apysc.event.mouse_up_interface import MouseUpInterface
+
+
+class MouseEventInterfaces(
+        ClickInterface, DoubleClickInterface, MouseDownInterface,
+        MouseUpInterface, MouseOverInterface):
+    """Class implementation for inheritance of each mouse event interfaces.
+    """
