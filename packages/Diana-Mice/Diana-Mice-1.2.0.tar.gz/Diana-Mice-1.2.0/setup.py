cf9e0eeccc9978827ef4b3e93f2ec8398ee75d0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:37 +0000

    Add package Diana-Mice/1.2.0/Diana-Mice-1.2.0.tar.gz

---
+++ packages/Diana-Mice/Diana-Mice-1.2.0.tar.gz/Diana-Mice-1.2.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+      name = 'Diana-Mice',
+      version = '1.2.0',
+      py_modules=['嘉然今天吃什么'],
+      author = '1196499173',
+      author_email='1196499173@qq.com',
+      url='https://space.bilibili.com/672328094/',
+      description='圣嘉然Diana之神鼠兔三相性大典'
+      )
