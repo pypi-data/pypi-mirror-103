57b66add00a3ff155ae98320874e575820ffc3a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:03 +0000

    Add package python-bol-retailer-api/0.3.0/python-bol-retailer-api-0.3.0.tar.gz

---
+++ packages/python-bol-retailer-api/python-bol-retailer-api-0.3.0.tar.gz/python-bol-retailer-api-0.3.0/bol/models/transports.py
@@ -0,0 +1,12 @@
+from .base import BaseModel
+
+class Transport(BaseModel):
+
+    def __init__(self,
+        transporterCode=None,
+        trackAndTrace=None
+    ):
+
+        self.transporterCode = transporterCode
+        self.trackAndTrace = trackAndTrace
+        self.errors = False
