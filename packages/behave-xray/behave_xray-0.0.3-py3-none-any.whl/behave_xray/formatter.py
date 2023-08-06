9e295d5d1aedb52d1195a074f77206ef46907630
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:51 +0000

    Add package behave-xray/0.0.3/behave_xray-0.0.3-py3-none-any.whl

---
+++ packages/behave-xray/behave_xray-0.0.3-py3-none-any.whl/behave_xray/formatter.py
@@ -0,0 +1,100 @@
+import re
+from collections import defaultdict
+from os import environ
+
+from behave.formatter.base import Formatter
+from behave.model import Status as ScenarioStatus
+from behave_xray.model import XrayStatus, TestCase, TestExecution
+from behave_xray.xray_publisher import XrayPublisher
+
+
+def get_test_execution_tag(tag):
+    match = re.match(r"^jira\.test_?execution\('(.+)'\)$", tag, flags=re.IGNORECASE)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+
+def get_test_plan_tag(tag):
+    match = re.match(r"^jira\.test_?plan\('(.+)'\)$", tag, flags=re.IGNORECASE)
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+
+def get_test_case_tag(tag):
+    match = re.match(r"^(allure|jira)\.test_?case\(['\"](.+)['\"]\)$", tag, flags=re.IGNORECASE)
+    if match:
+        return match.group(2)
+    # for outline scenario
+    match = re.match(r"^(allure|jira)\.test_?case(.+)$", tag, flags=re.IGNORECASE)
+    if match:
+        return match.group(2)
+    else:
+        return None
+
+
+class XrayFormatter(Formatter):
+    description = 'Jira XRAY formatter'
+
+    def __init__(self, stream, config):
+        super().__init__(stream, config)
+        self._scenario_keys = defaultdict(lambda: TestCase())
+        self.current_feature = None
+        self.current_scenario = None
+        self.test_execution = TestExecution()
+        jira_url = environ["XRAY_API_BASE_URL"]
+        auth = (environ["XRAY_API_USER"], environ["XRAY_API_PASSWORD"])
+        self.xray_publisher = XrayPublisher(base_url=jira_url, auth=auth)
+
+    def __repr__(self):
+        return f'{self.__class__.__name__}()'
+
+    def feature(self, feature):
+        self.current_feature = feature
+        if feature.tags:
+            for tag in feature.tags:
+                te = get_test_execution_tag(tag)
+                if te:
+                    self.test_execution.test_execution_key = te
+                tp = get_test_plan_tag(tag)
+                if tp:
+                    self.test_execution.test_plan_key = tp
+
+    def scenario(self, scenario):
+        self.current_scenario = scenario
+        if scenario.tags:
+            for tag in scenario.tags:
+                tk = get_test_case_tag(tag)
+                if tk:
+                    self._scenario_keys[scenario].test_key = tk
+
+    def result(self, result):
+        if self.current_scenario.status == ScenarioStatus.untested:
+            return
+
+        self._scenario_keys[self.current_scenario].duration = result.duration
+
+        if self.current_scenario.status == ScenarioStatus.passed:
+            self._scenario_keys[self.current_scenario].status = XrayStatus.PASS
+        if result.status == ScenarioStatus.failed:
+            self._scenario_keys[self.current_scenario].status = XrayStatus.FAIL
+            self._scenario_keys[self.current_scenario].comment = result.error_message
+        if result.status == ScenarioStatus.untested:
+            self._scenario_keys[self.current_scenario].status = XrayStatus.TODO
+        if result.status == ScenarioStatus.skipped:
+            self._scenario_keys[self.current_scenario].status = XrayStatus.ABORTED
+            self._scenario_keys[self.current_scenario].comment = self.current_scenario.skip_reason
+
+    def eof(self):
+        if self.config.dry_run:
+            return
+        while self._scenario_keys:
+            _, xray = self._scenario_keys.popitem()
+            if xray.test_key:
+                self.test_execution.append(xray)
+        if self.test_execution.tests:
+            self.xray_publisher.publish(self.test_execution)
+        self.test_execution.flush()
---
+++ packages/behave-xray/behave_xray-0.0.3-py3-none-any.whl/behave_xray/xray_publisher.py
@@ -0,0 +1,58 @@
+import json
+import logging
+from typing import Union
+
+import requests
+from behave_xray.model import TestExecution
+from requests.auth import AuthBase
+
+TEST_EXEXUTION_ENDPOINT = '/rest/raven/2.0/import/execution'
+
+logging.basicConfig()
+_logger = logging.getLogger(__name__)
+_logger.setLevel(logging.INFO)
+
+
+class XrayError(Exception):
+    """Custom exception for Jira XRAY"""
+
+
+class XrayPublisher:
+
+    def __init__(self, base_url: str, auth: Union[AuthBase, tuple]) -> None:
+        if base_url.endswith('/'):
+            base_url = base_url[:-1]
+        self.base_url = base_url
+        self.auth = auth
+
+    @property
+    def endpoint_url(self) -> str:
+        return self.base_url + TEST_EXEXUTION_ENDPOINT
+
+    def publish_xray_results(self, url: str, auth: AuthBase, data: dict) -> dict:
+        headers = {'Accept': 'application/json',
+                   'Content-Type': 'application/json'}
+        try:
+            response = requests.request(method='POST', url=url, headers=headers, json=data, auth=auth)
+        except requests.exceptions.ConnectionError as e:
+            _logger.exception('ConnectionError to JIRA service %s', self.base_url)
+            raise XrayError(e)
+        else:
+            try:
+                response.raise_for_status()
+            except Exception as e:
+                _logger.error('Could not post to JIRA service %s. Response status code: %s',
+                              self.base_url, response.status_code)
+                raise XrayError from e
+            return response.json()
+
+    def publish(self, test_execution: TestExecution) -> bool:
+        try:
+            result = self.publish_xray_results(self.endpoint_url, self.auth, test_execution.as_dict())
+        except XrayError:
+            _logger.error('Could not publish results to Jira XRAY')
+            return False
+        else:
+            key = result['testExecIssue']['key']
+            _logger.info('Uploaded results to JIRA XRAY Test Execution: %s', key)
+            return True
