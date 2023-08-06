be349497a63aceba745706f8d84b228733cea5e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:52 +0000

    Add package DecimScanner/1.3.2a0/DecimScanner-1.3.2a0.tar.gz

---
+++ packages/DecimScanner/DecimScanner-1.3.2a0.tar.gz/DecimScanner-1.3.2a0/setup.py
@@ -0,0 +1,27 @@
+import pathlib
+from setuptools import setup
+from setuptools import find_packages
+
+README = (pathlib.Path(__file__).parent / "README.md").read_text()
+
+setup(name="DecimScanner",
+      version="1.3.2_a",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      description="A threaded scanner package for python",
+      author="Cinnamon1212",
+      url="https://github.com/Cinnamon1212/",
+      install_requires=['scapy', 'pybluez', 'bs4'],
+      packages=find_packages(),
+      keywords=["python", "threaded scanners", "TCP", "UDP", "ICMP", "Penetration testing", "pentesting", "scapy"],
+      classifiers=[
+          "Development Status :: 4 - Beta",
+          "Intended Audience :: Developers",
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+          "Operating System :: Unix"
+      ],
+      entry_points={
+        "console_scripts": ["DecimScanner=DecimScanner.__main__:main"]
+      }
+)
