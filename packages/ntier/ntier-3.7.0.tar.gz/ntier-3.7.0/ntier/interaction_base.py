2905d120f255c142fc3a06253256546c7d5d618b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package ntier/3.7.0/ntier-3.7.0.tar.gz

---
+++ packages/ntier/ntier-3.7.0.tar.gz/ntier-3.7.0/ntier/interaction_base.py
@@ -0,0 +1,54 @@
+"""Defines a base class for interactions. Interactions are the web-socket version of
+transactions.
+"""
+from typing import Generic, Iterable, TypeVar
+
+InputType = TypeVar("InputType")
+MessageType = TypeVar("MessageType")
+
+
+class StopInteraction(Exception):
+    """Exception to signal that an interaction wants to stop communicating."""
+
+
+class InteractionBase(Generic[InputType, MessageType]):
+    """Base class for implementing websocket interactions."""
+
+    @property
+    def is_open(self) -> bool:
+        return getattr(self, "_is_open", False)
+
+    @is_open.setter
+    def is_open(self, value: bool):
+        setattr(self, "_is_open", value)
+
+    async def initialize_state(self, data: InputType):
+        """Initialize the interaction's state."""
+
+    async def open(self, data: InputType) -> Iterable[MessageType]:
+        """Start up the interaction. Initialize state and perform whatever other actions need to
+        occur before messages are sent.
+        """
+        if self.is_open:
+            raise Exception("Interaction has already been opened")
+        await self.initialize_state(data)
+        return []
+
+    async def close(self):
+        """Shut down the interaction, close upstream connections"""
+        if not self.is_open:
+            raise Exception("Interaction is not open")
+        self.is_open = False
+
+    async def send(self, messages: Iterable[MessageType]) -> Iterable[MessageType]:
+        """Send messages to the interaction."""
+        raise NotImplementedError()
+
+    async def receive(self) -> Iterable[MessageType]:
+        """Receive messages from the interaction."""
+        raise NotImplementedError()
+
+    @staticmethod
+    def error(message: str):
+        """Signal that an error occurred and the interaction needs to stop."""
+        raise StopInteraction(message)
