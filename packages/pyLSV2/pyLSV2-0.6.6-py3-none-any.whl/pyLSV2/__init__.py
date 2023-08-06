65095f7b437cf4421f36c1d5c18978f2df5fee12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:27 +0000

    Add package pyLSV2/0.6.6/pyLSV2-0.6.6-py3-none-any.whl

---
+++ packages/pyLSV2/pyLSV2-0.6.6-py3-none-any.whl/pyLSV2/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""A pure Python3 implementation of the LSV2 protocol"""
+from .client import LSV2
+from .const import *
+from .translate_messages import *
+
+__version__ = '0.6.6'
