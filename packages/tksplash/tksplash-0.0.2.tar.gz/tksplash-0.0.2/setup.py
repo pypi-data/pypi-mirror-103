373dc36d2dbe3d5a934770a3e272f06a519b5afb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:43 +0000

    Add package tksplash/0.0.2/tksplash-0.0.2.tar.gz

---
+++ packages/tksplash/tksplash-0.0.2.tar.gz/tksplash-0.0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2'
+DESCRIPTION = 'Creates splash screens for GUIs'
+# Setting up
+setup(
+    name="tksplash",
+    version=VERSION,
+    author="MrHola21 (Gautam Singh)",
+    author_email="projpy6969@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'Guis', 'GUI', 'Gui', 'splash screen'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
