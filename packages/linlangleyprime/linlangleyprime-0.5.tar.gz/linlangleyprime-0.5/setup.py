41772df502b4e6ba0b55790b9890ef3626d1c3d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:34 +0000

    Add package linlangleyprime/0.5/linlangleyprime-0.5.tar.gz

---
+++ packages/linlangleyprime/linlangleyprime-0.5.tar.gz/linlangleyprime-0.5/setup.py
@@ -0,0 +1,37 @@
+import setuptools
+ 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+ 
+setuptools.setup(
+    name='linlangleyprime', # a unique name for PyPI
+    version='0.5',
+    author='Lin Chen, Yanhua Feng',
+    author_email='lin.chen@ieee.org, yf@vims.edu',
+    description='Demo for building a Python project',
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url='http://lin-chen-langley.github.io',
+    project_urls = {
+        'PyPI': 'https://pypi.org/manage/project/linlangleyprime/releases/',
+        'Conda': 'https://anaconda.org/linchenVA/linlangleyprime'
+        },
+    classifiers=[
+      'Development Status :: 4 - Beta',
+      'Environment :: X11 Applications :: GTK',
+      'Intended Audience :: End Users/Desktop',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: GNU General Public License (GPL)',
+      'Operating System :: POSIX :: Linux',
+      'Programming Language :: Python',
+      'Topic :: Desktop Environment',
+      'Topic :: Text Processing :: Fonts'
+      ],
+    install_requires=requirements,
+    package_dir={'':'src'}, # location to find the packages
+    packages=setuptools.find_packages(where="src"),
+    #packages=['primepackage', ], # packages and subpackages containing .py files
+    python_requires=">=3.9",
+    scripts=['src/generator',], # the executable files will be installed for user
+    license='Creative Commons Attribution-Noncommercial-Share Alike license',
+)
