7327cbff1b3aa61be78fa4e6e7870ae1331b36af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:17 +0000

    Add package The-nester/1.0.1/The_nester-1.0.1.tar.gz

---
+++ packages/The-nester/The_nester-1.0.1.tar.gz/The_nester-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+        name = 'The_nester',
+        version = '1.0.1',
+        py_modules = ['nester'],
+        author = 'lebrone',
+        author_email = 'musirzx@gmail.com',
+        url = 'http://www.sir-bron.com',
+        description = 'A simple function to display nested lists',
+        )
+#xhxhxhxh
