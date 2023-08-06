16774de1c03db58ffeae05729dc5bb3c9c01cd51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:55 +0000

    Add package topicextractor/0.0.0/topicextractor-0.0.0.tar.gz

---
+++ packages/topicextractor/topicextractor-0.0.0.tar.gz/topicextractor-0.0.0/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup
+
+def readme():
+    with open("README.txt") as f:
+        readme = f.read()
+    return readme
+
+classifiers = [
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+    ]
+
+setup(
+    name = "topicextractor",
+    version = "0.0.0",
+    author = "KJ Chung",
+    author_email = "kjchung495@yonsei.ac.kr",
+    description = "Extracts keywords with 'TF-IDF' algorithm",
+    long_description = readme(),
+    long_description_content_type = "text/markdown",
+    license = "Apache Software License 2.0",
+    keywords = "TFIDF, topic analysis",
+    url = "https://github.com/kjchung495/topicextractor",
+    classifiers = classifiers,
+    packages = ["topicextractor"],
+    include_package_data = True,
+    install_requires = []
+)
+
