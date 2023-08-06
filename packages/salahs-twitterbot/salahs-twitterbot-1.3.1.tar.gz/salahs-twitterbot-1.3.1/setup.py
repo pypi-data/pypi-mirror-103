b8bb6ded542c976a3757de9a41d240c040224e57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:14 +0000

    Add package salahs-twitterbot/1.3.1/salahs-twitterbot-1.3.1.tar.gz

---
+++ packages/salahs-twitterbot/salahs-twitterbot-1.3.1.tar.gz/salahs-twitterbot-1.3.1/setup.py
@@ -0,0 +1,47 @@
+from os import path
+
+from setuptools import find_packages, setup
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    DESCRIPTION = f.read()
+
+
+tests_deps = [
+    "pytest-coverage",
+    "httpretty>=0.9.6,<1.0.0",
+    "coverage[toml]",
+]
+
+scripts_deps = ["yweather>=0.1,<1.0"]
+
+
+setup(
+    name="salahs-twitterbot",
+    version="1.3.1",
+    description="twitterbot to remove/view favorites/tweets and view trending tweets",
+    long_description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "License :: OSI Approved :: MIT License",
+    ],
+    author="Salah Ahmed",
+    author_email="salah93@crosscompute.com",
+    url="https://github.com/salah93/twitterbot",
+    keywords="web twitterbot twitter",
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=["oauth2>=1.9.0.post1,<2.0.0"],
+    tests_require=tests_deps,
+    extras_require={"test": tests_deps, "scripts": scripts_deps},
+    entry_points={
+        "console_scripts": {
+            "delete_old_tweets = twitterbot.scripts.delete_tweets:main",
+            "get_trending_tweets = twitterbot.scripts.get_trending:main",
+            "remove_favorites = twitterbot.scripts.remove_favorites:main",
+        }
+    },
+)
