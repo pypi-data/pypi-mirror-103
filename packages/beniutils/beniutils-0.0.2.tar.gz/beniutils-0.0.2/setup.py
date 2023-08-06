4784f325f9aa14ae20462b4227ff59366e745915
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:33 +0000

    Add package beniutils/0.0.2/beniutils-0.0.2.tar.gz

---
+++ packages/beniutils/beniutils-0.0.2.tar.gz/beniutils-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'beniutils',
+    version = '0.0.2',
+    keywords='beni',
+    description = 'utils library for Beni',
+    license = 'MIT License',
+    url = 'https://pypi.org/project/beniutils/',
+    author = 'Beni',
+    author_email = 'benimang@126.com',
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = 'any',
+    install_requires = [],
+)
