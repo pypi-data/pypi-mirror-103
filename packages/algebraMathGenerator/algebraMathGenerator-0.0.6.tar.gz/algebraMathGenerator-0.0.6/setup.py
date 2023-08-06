f892487d92cfbfafc9e8421d6d9b93a68b35f0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:55 +0000

    Add package algebraMathGenerator/0.0.6/algebraMathGenerator-0.0.6.tar.gz

---
+++ packages/algebraMathGenerator/algebraMathGenerator-0.0.6.tar.gz/algebraMathGenerator-0.0.6/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+
+classifiers = [
+  'Development Status :: 5 - Production/Stable',
+  'Intended Audience :: Education',
+  'Operating System :: Microsoft :: Windows :: Windows 10',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+
+    
+setup(
+    name='algebraMathGenerator',
+    version='0.0.6',
+    description='An easy way to generate algebraic equations and even solve them',
+    long_description=open('README.md').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    long_description_content_type='text/markdown',
+    url='',  
+    author='Megabrains',
+    author_email='aadibhuskute@yahoo.com',
+    license='MIT', 
+    classifiers=classifiers,
+    keywords='algebra', 
+    packages=find_packages(),
+    install_requires=[''] 
+)
