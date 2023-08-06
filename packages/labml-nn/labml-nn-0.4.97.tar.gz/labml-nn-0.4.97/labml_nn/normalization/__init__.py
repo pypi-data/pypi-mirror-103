3fc7503b6013e9b742169d810a13ec1d71171073
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:29 +0000

    Add package labml-nn/0.4.97/labml-nn-0.4.97.tar.gz

---
+++ packages/labml-nn/labml-nn-0.4.97.tar.gz/labml-nn-0.4.97/labml_nn/normalization/__init__.py
@@ -0,0 +1,14 @@
+"""
+---
+title: Normalization Layers
+summary: >
+ A set of PyTorch implementations/tutorials of normalization layers.
+---
+
+# Normalization Layers
+
+* [Batch Normalization](batch_norm/index.html)
+* [Layer Normalization](layer_norm/index.html)
+* [Instance Normalization](instance_norm/index.html)
+* [Group Normalization](group_norm/index.html)
+"""
