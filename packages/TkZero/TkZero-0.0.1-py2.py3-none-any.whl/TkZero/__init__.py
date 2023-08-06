7646455225a4e563933a12dd1e65253ad6305c16
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:13 +0000

    Add package TkZero/0.0.1/TkZero-0.0.1-py2.py3-none-any.whl

---
+++ packages/TkZero/TkZero-0.0.1-py2.py3-none-any.whl/TkZero/__init__.py
@@ -0,0 +1,14 @@
+"""
+TkZero - a sane and Pythonic wrapper around Tkinter.
+"""
+# TODO: Create into proper Python package - https://antonz.org/python-packaging/
+
+__version_info__ = (0, 0, 1)
+__version__ = ""
+for index, digit in enumerate(__version_info__):
+    __version__ += str(digit)
+    if index < len(__version_info__) - 1:
+        __version__ += "."
+# "Prototype", "Development", or "Production"
+__status__ = "Prototype"
+__author__ = "UnsignedArduino at https://github.com/UnsignedArduino"
