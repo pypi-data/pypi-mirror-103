33ed330b66d5dc9a45ba28d323774efaf83fa467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:11 +0000

    Add package ZenitAI/0.2/zenitai-0.2.tar.gz

---
+++ packages/ZenitAI/zenitai-0.2.tar.gz/zenitai-0.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+
+setup(
+    name="zenitai",
+    version="0.2",
+    packages=find_packages(),
+    descrption="A collection of tools and utils for data analysis. Contains functions and tools for WOE-transformations and other utils",
+)
+
+
+print(find_packages())
---
+++ packages/ZenitAI/zenitai-0.2.tar.gz/zenitai-0.2/zenitai/transform/__init__.py
@@ -0,0 +1,7 @@
+from transform.woe import (WoeTransformer, WoeTransformerRegularized,
+woeTransformer, woe_apply, group_plot, grouping, statistic, monotonic_borders)
+
+__all__ = [
+  'WoeTransformer',
+  'WoeTransformerRegularized',
+  ]
