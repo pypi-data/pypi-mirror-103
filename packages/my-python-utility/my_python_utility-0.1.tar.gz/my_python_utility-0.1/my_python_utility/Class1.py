7262cfe9e2b9c4c36d33f3a9b07e1680e21209b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:18 +0000

    Add package my-python-utility/0.1/my_python_utility-0.1.tar.gz

---
+++ packages/my-python-utility/my_python_utility-0.1.tar.gz/my_python_utility-0.1/my_python_utility/Class1.py
@@ -0,0 +1,11 @@
+import numpy as np
+from scrapeasy import Webpage
+import time
+
+class Class1:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
