8b4ac7493ff1aed5e50e4408832e29bfe4dfaa37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:53 +0000

    Add package hydbedscraper/0.0.8/hydbedscraper-0.0.8.tar.gz

---
+++ packages/hydbedscraper/hydbedscraper-0.0.8.tar.gz/hydbedscraper-0.0.8/src/hydbedscraper/city.py
@@ -0,0 +1,28 @@
+import logging
+from enum import Enum
+
+from hydbedscraper.drivers import hyderabad, ahmedabad, vadodara
+
+
+class City(Enum):
+    AHMEDABAD = "ahmedabad"
+    HYDERABAD = "hyderabad"
+    VADODARA = "vadodara"
+
+    def work(self):
+        logging.info(f"working at {self.value}")
+        if self is City.AHMEDABAD:
+            return ahmedabad.work()
+        elif self is City.HYDERABAD:
+            return hyderabad.work()
+        elif self is City.VADODARA:
+            return vadodara.work()
+        else:
+            raise NotImplementedError(self)
+
+
+def get_city_from_str(city_name: str) -> City:
+    try:
+        return City(city_name)
+    except ValueError as exc:
+        raise ValueError(f"Supported cities: {[city for city in City]}") from exc
