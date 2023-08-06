c14546968a0ab0b2e71e5916c313de10801c2860
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:57 +0000

    Add package enotification/0.0.2/enotification-0.0.2.tar.gz

---
+++ packages/enotification/enotification-0.0.2.tar.gz/enotification-0.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+from setuptools import version
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="enotification",
+    version="0.0.2",
+    author="DiamondI",
+    author_email="920076768@qq.com",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/DiamondI/enotification",
+    project_urls={
+        "Bug Tracker": "https://github.com/DiamondI/enotification/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "enotification"},
+    python_requires=">=3.6"
+)
