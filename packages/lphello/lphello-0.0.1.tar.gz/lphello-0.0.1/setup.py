c8f586f42617d7408d1522cd1a71fab47abe94e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:45 +0000

    Add package lphello/0.0.1/lphello-0.0.1.tar.gz

---
+++ packages/lphello/lphello-0.0.1.tar.gz/lphello-0.0.1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+__packages__ = ['hello_package','hello_package.sub_folder']
+
+setup(
+    name = "lphello",
+    version = "0.0.1",
+    description = "hello world",
+    author = 'gxf',
+    author_email = 'feng.gao@laipac.com',
+    url = 'https://github.com/gxfca/gitTest',
+    packages = __packages__,
+    package_data = {'':['*.dat']},
+#    packages = find_packages(where = "hello_package"),
+#    package_dir ={"":"hello_package"}
+    entry_points={
+    'console_scripts': [
+        'hello=hello_package.hello:print_hello',
+    ],
+    },
+    
+    )
+
