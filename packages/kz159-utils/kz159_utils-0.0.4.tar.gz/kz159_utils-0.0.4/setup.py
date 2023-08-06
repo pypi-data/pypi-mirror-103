71c3c8f44f78e1cc78baaf2b0370993ecf292064
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:55 +0000

    Add package kz159-utils/0.0.4/kz159_utils-0.0.4.tar.gz

---
+++ packages/kz159-utils/kz159_utils-0.0.4.tar.gz/kz159_utils-0.0.4/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='kz159_utils',
+    version='0.0.4',
+    packages=['kz159_utils'],
+    url='https://github.com/kz159/simple_config',
+    license='MIT',
+    author='kz159',
+    author_email='me@kz159.ru',
+    description=''
+)
