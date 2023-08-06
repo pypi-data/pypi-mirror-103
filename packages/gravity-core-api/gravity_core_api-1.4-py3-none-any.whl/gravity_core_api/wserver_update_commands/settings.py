e469de611726d795db3b20d75260d2f6a9d887da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:01 +0000

    Add package gravity-core-api/1.4/gravity_core_api-1.4-py3-none-any.whl

---
+++ packages/gravity-core-api/gravity_core_api-1.4-py3-none-any.whl/gravity_core_api/wserver_update_commands/settings.py
@@ -0,0 +1,11 @@
+from gravity_core_api.wserver_update_commands.functions import *
+
+
+all_keys = {'trash_cats': {'execute_function': trash_cat_execute},
+            'trash_types': {'execute_function': trash_type_execute}}
+
+
+# Данные локальной базы данных (WDB)
+trash_cats_tablename = 'trash_cats'
+trash_types_tablename = 'trash_types'
+
