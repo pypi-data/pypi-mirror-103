b542b971a57bf859a68612f21da59218ae04b0d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:30 +0000

    Add package Ovomaltino/0.0.7/Ovomaltino-0.0.7.tar.gz

---
+++ packages/Ovomaltino/Ovomaltino-0.0.7.tar.gz/Ovomaltino-0.0.7/setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="Ovomaltino",
+    version="0.0.7",
+    author="Matheus Nobre Gomes",
+    author_email="matt-gomes@live.com",
+    description="Multi agent system using social theories",
+    license="GPLv3+",
+    keywords="Ovomaltino",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ovomaltino/Ovomaltino",
+    packages=['ovomaltino'],
+    install_requires=[
+        "numpy",
+        "pandas",
+        "requests"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: OS Independent",
+    ],
+)
