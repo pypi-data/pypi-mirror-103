5de79b5cb4e82137cc186948452f15b477c21d6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:40 +0000

    Add package SC-search/0.0.1/SC_search-0.0.1-py3-none-any.whl

---
+++ packages/SC-search/SC_search-0.0.1-py3-none-any.whl/SC_search/__init__.py
@@ -0,0 +1,33 @@
+r"""
+The Cell_BLAST package
+"""
+
+# from .utils import in_ipynb
+
+# if not in_ipynb():
+#     import matplotlib
+#     matplotlib.use("agg")
+
+# from . import (blast, config, data, directi, latent, metrics, prob, rmbatch,
+#                utils)
+
+
+name = "SC_search"
+
+__copyright__ = "BME"
+
+__author__ = "Cheng Zhang"
+
+# __all__ = [
+#     "blast",
+#     "data",
+#     "directi",
+#     "latent",
+#     "metrics",
+#     "prob",
+#     "rmbatch",
+#     "utils",
+#     "config"
+# ]
+
+__version__ = "0.0.1"
