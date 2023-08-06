afb35109c6aec01cea738cd006bffa970b5cc32d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:32:17 +0000

    Add package xk-user/0.0.2/xk_user-0.0.2.tar.gz

---
+++ packages/xk-user/xk_user-0.0.2.tar.gz/xk_user-0.0.2/README.md
@@ -0,0 +1,3 @@
+```
+venv\Scripts\python.exe -m grpc_tools.protoc -I./user --python_out=./user --grpc_python_out=. ./user/user.proto
+```
---
+++ packages/xk-user/xk_user-0.0.2.tar.gz/xk_user-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="xk_user",
+    version="0.0.2",
+    description="xing kuang SDK",
+    long_description="eds sdk for python",
+    license="MIT Licence",
+
+    url="http://test.com",
+    author="zhanglei",
+    author_email="test@gmail.com",
+
+    # When your source code is in a subdirectory under the project root, e.g.
+    # `src/`, it is necessary to specify the `package_dir` argument.
+
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=["protobuf==3.15.8",
+                      "grpcio==1.37.0",
+                      "grpcio-tools==1.37.0",
+                      "six==1.15.0"],
+
+    scripts=[],
+
+)
