ed53a2caf3f399b2e0403531099b9c5681c5429f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:52 +0000

    Add package similar-documents/0.0.1/similar_documents-0.0.1.tar.gz

---
+++ packages/similar-documents/similar_documents-0.0.1.tar.gz/similar_documents-0.0.1/similar_documents/text_converter.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+from typing import Callable
+
+from markdown import Markdown
+from bs4 import BeautifulSoup
+
+Converter = Callable[[str], str]
+
+
+def _createMarkdownToText():
+    md = Markdown(extensions=["full_yaml_metadata"])
+
+    def md2text(source: str) -> str:
+        body = md.convert(source)
+        meta = md.Meta
+        text = html(str(body))
+        if type(meta) is dict:
+            for key in meta.keys():
+                if key.upper() == "TITLE":
+                    text = meta[key] + "\n" + text
+                    break
+
+        return text
+
+    return md2text
+
+
+markdown = _createMarkdownToText()
+
+
+def html(source: str) -> str:
+    return "".join(
+        BeautifulSoup(source, features="html.parser").findAll(text=True)
+    ).strip()
+
+
+def text(source: str) -> str:
+    return source.strip()
