b2833c30e7e12141da334b36a4954de3148abce5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:59 +0000

    Add package aetcd3/0.1.0a6/aetcd3-0.1.0a6.tar.gz

---
+++ packages/aetcd3/aetcd3-0.1.0a6.tar.gz/aetcd3-0.1.0a6/docs/conf.py
@@ -0,0 +1,22 @@
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.viewcode',
+]
+
+templates_path = []
+
+source_suffix = '.rst'
+master_doc = 'index'
+
+project = 'aetcd3'
+copyright = '2020, Andrey Martyanov'
+author = 'Andrey Martyanov'
+
+version = '0.1.0a2'
+release = '0.1.0a2'
+
+autodoc_member_order = 'bysource'
+
+html_theme = 'sphinx_rtd_theme'
+html_theme_options = {}
+html_static_path = []
---
+++ packages/aetcd3/aetcd3-0.1.0a6.tar.gz/aetcd3-0.1.0a6/docs/index.rst
@@ -0,0 +1,20 @@
+Welcome to aetcd3's documentation!
+==================================
+
+Contents:
+
+.. toctree::
+   :maxdepth: 2
+
+   readme
+   installation
+   usage
+   contributing
+   changelog
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
