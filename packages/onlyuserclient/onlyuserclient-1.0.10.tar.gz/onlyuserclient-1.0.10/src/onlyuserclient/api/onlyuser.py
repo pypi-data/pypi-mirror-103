55a3684830577af806a965e8a3f68ae5a9fbbd6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:56 +0000

    Add package onlyuserclient/1.0.10/onlyuserclient-1.0.10.tar.gz

---
+++ packages/onlyuserclient/onlyuserclient-1.0.10.tar.gz/onlyuserclient-1.0.10/src/onlyuserclient/api/onlyuser.py
@@ -0,0 +1,42 @@
+from simple_rest_client.resource import Resource
+from .base import BaseAPI
+from onlyuserclient.settings import api_settings
+
+__all__ =('onlyuserapi',)
+
+class RolepermResource(Resource):
+    '''角色权限访问
+    '''
+    actions = {
+        #获得部门包含的用户ID 
+        "userids_department":{'method': 'POST', 'url': '/user-role-perms/userids-department/'},
+        "userids_branch":{'method': 'POST', 'url': '/user-role-perms/userids-branch/'},
+        "userids_organization":{'method': 'POST', 'url': '/user-role-perms/userids-organization/'}
+    }
+
+class UserResource(Resource):
+    '''用户帐号资源
+    '''
+    actions = {
+        "retrieve":{'method': 'GET', 'url': '/users/{}/'},
+    }
+
+class OrganizationResource(Resource):
+    '''组织树资源
+    '''
+    actions = {
+        "retrieve":{'method': 'GET', 'url': '/organizations/{}/'},
+    }
+
+class ApplicationResource(Resource):
+    '''应用程序资源
+    '''
+    actions = {
+        "retrieve":{'method': 'GET', 'url': '/applications/{}/'},
+    }    
+
+onlyuserapi = BaseAPI(pfx=api_settings.ONLYUSER_PFX)
+onlyuserapi.add_resource(resource_name='roleperms', resource_class=RolepermResource)
+onlyuserapi.add_resource(resource_name='users', resource_class=UserResource)
+onlyuserapi.add_resource(resource_name='organizations', resource_class=OrganizationResource)
+onlyuserapi.add_resource(resource_name='applications', resource_class=ApplicationResource)
