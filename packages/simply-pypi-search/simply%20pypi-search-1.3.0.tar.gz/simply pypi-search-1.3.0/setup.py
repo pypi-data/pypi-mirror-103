2e159c33b7808657dd56a4ac62df20ea9ebe5d08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:32 +0000

    Add package simply-pypi-search/1.3.0/simply%20pypi-search-1.3.0.tar.gz

---
+++ packages/simply-pypi-search/simply%20pypi-search-1.3.0.tar.gz/simply pypi-search-1.3.0/setup.py
@@ -0,0 +1,32 @@
+import pathlib
+import setuptools
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="simply pypi-search",
+    version="1.3.0",
+    description="A simply tool to search for packages on pypi.org",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mccoderpy/simply-pypi-search",
+    author="mccoder.py",
+    author_email="mccuber04@outlook.de",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    include_package_data=True,
+    install_requires=["aiohttp", "bs4"],
+    python_requires=">=3.6"
+)
---
+++ packages/simply-pypi-search/simply%20pypi-search-1.3.0.tar.gz/simply pypi-search-1.3.0/src/simply_pypi_search/__init__.py
@@ -0,0 +1,113 @@
+import builtins
+
+from bs4 import BeautifulSoup
+from aiohttp import ClientSession
+
+
+class PyPiSearch:
+    def __init__(self):
+        self.search = self.pypi_search
+        self.usr_agent = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/61.0.3163.100 Safari/537.36'}
+
+    @classmethod
+    async def pypi_search(cls, keyword: str) -> list:
+        """
+        Return the search-results in an :class:`list` with :class:`PyPiProject` 's
+
+        :param keyword:
+        :return: [result1, result2, result3, etc.]
+        """
+        html = await cls().pypi_get_results(keyword)
+        soup = BeautifulSoup(html, 'html.parser')
+        ul = soup.find('ul', attrs='unstyled')
+        if ul is None:
+            return NoResultsFound(keyword)
+        else:
+            return list(cls().parse_pypi_results(html, keyword))
+
+    async def pypi_get_results(self, keyword):
+        search_term = keyword.replace(" ", "+")
+        async with ClientSession(headers=self.usr_agent) as suche:
+            such_ergebnis = await suche.get(url=f"https://pypi.org/search/?q={search_term}&o")
+            such_ergebnis.raise_for_status()
+            results = await such_ergebnis.text()
+        return results
+
+    def parse_pypi_results(self, raw_html, keyword):
+
+        if ul is None:
+            return NoResultsFound(keyword)
+        else:
+            result_block = ul.find_all('li')
+            for result in result_block:
+                link = result.find('a', href=True)['href']
+                name = result.find('span', attrs={'class': 'package-snippet__name'})
+                version = result.find('span', attrs={'class': 'package-snippet__version'})
+                description = result.find('p', attrs={'class': 'package-snippet__description'})
+                release = result.find('time')
+                if release is not None:
+                    release = release.decode_contents().strip("\n \n")
+                if name is not None:
+                    name = name.decode_contents()
+                if version is not None:
+                    version = version.decode_contents()
+                if description is not None:
+                    description = description.decode_contents()
+                if name and link:
+                    yield PyPiProject(
+                        {"name": name, "description": description, "version": version, "released": release,
+                         "link": "https://pypi.org" + link})
+
+
+class PyPiProject:
+    """
+    The :class:`type` the parts off an :func:`~PyPiSearch.pypi_search` returned :class:`list` are out
+
+    :class: PyPiProject
+
+    """
+
+    def __init__(self, data: dict):
+        self.Name = data.get('name')
+        self.Description: str = data.get('description')
+        self.Version: str = data.get('version')
+        self.Released: str = data.get('released')
+        self.Link: str = data.get('link')
+
+    @property
+    def name(self):
+        return self.Name
+
+    @property
+    def description(self):
+        return self.Description
+
+    @property
+    def version(self):
+        return self.Version
+
+    @property
+    def released(self):
+        return self.Released
+
+    @property
+    def link(self):
+        return self.Link
+
+
+class SearchError:
+    def __init__(self, args):
+        self.args = args
+
+    @property
+    def error(cls):
+        return str(cls.args)
+
+
+class NoResultsFound(SearchError):
+    def __init__(self, keyword):
+        self.keyword = keyword
+        no_results = 'No Results Found for the Keyword %s' % keyword
+        super().__init__(no_results)
