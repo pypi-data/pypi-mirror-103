413f7d517ccf71d770e1efb76245c5ecb41b2962
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:32 +0000

    Add package momotor-bundles/3.4.0/momotor-bundles-3.4.0.tar.gz

---
+++ packages/momotor-bundles/momotor-bundles-3.4.0.tar.gz/momotor-bundles-3.4.0/src/momotor/bundles/exception.py
@@ -0,0 +1,30 @@
+class BundleError(Exception):
+    pass
+
+
+class BundleFormatError(BundleError):
+    pass
+
+
+class CircularDependencies(BundleFormatError):
+    pass
+
+
+class InvalidDependencies(BundleFormatError):
+    pass
+
+
+class InvalidRefError(BundleFormatError):
+    pass
+
+
+class InvalidBundle(BundleFormatError):
+    pass
+
+
+class LxmlMissingError(BundleError):
+    pass
+
+
+class BundleLoadError(BundleError):
+    pass
