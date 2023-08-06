f395c13f431f750f7b81e95d97775a58d8329467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:38 +0000

    Add package mongomantic/0.4.2/mongomantic-0.4.2.tar.gz

---
+++ packages/mongomantic/mongomantic-0.4.2.tar.gz/mongomantic-0.4.2/mongomantic/__init__.py
@@ -0,0 +1,21 @@
+# type: ignore[attr-defined]
+"""A MongoDB Python ORM, built on Pydantic and PyMongo."""
+
+try:
+    from importlib.metadata import PackageNotFoundError, version
+except ImportError:  # pragma: no cover
+    from importlib_metadata import PackageNotFoundError, version
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+
+from mongomantic.core.base_repository import BaseRepository
+from mongomantic.core.database import connect, disconnect
+from mongomantic.core.index import Index
+from mongomantic.core.mongo_model import MongoDBModel
+
+__all__ = ["BaseRepository", "MongoDBModel", "connect", "disconnect", "Index"]
