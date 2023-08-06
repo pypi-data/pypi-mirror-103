7444b35388f1334d23c7538015d4d41f447c384a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:39 +0000

    Add package ipydraw/0.1.1a6/ipydraw-0.1.1a6-py2.py3-none-any.whl

---
+++ packages/ipydraw/ipydraw-0.1.1a6-py2.py3-none-any.whl/ipydraw/_version.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Copyright (c) Travis DePrato.
+# Distributed under the terms of the Modified BSD License.
+
+version_info = (0, 1, 1)
+version_suffix = "a6"
+__version__ = ".".join(map(str, version_info)) + version_suffix
