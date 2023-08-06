c2ea45763b3069f5bbb08784f7e97054cd72c5a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:48 +0000

    Add package ulogcorn/0.3.2/ulogcorn-0.3.2-py3-none-any.whl

---
+++ packages/ulogcorn/ulogcorn-0.3.2-py3-none-any.whl/ulogcorn/__init__.py
@@ -0,0 +1,5 @@
+from ulogcorn.logger import UnifyHandler, StubbedGunicornLogger, StandaloneApplication
+
+__version__ = '0.3.2'
+
+__all__ = ["StubbedGunicornLogger", "UnifyHandler", "StandaloneApplication", "__version__"]
