800f90b324d1492c64dd30d1576f429b6a564a79
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:15 +0000

    Add package mongosql/2.0.15.post1/mongosql-2.0.15.post1.tar.gz

---
+++ packages/mongosql/mongosql-2.0.15.post1.tar.gz/mongosql-2.0.15.post1/mongosql/util/__init__.py
@@ -0,0 +1,17 @@
+from .selectinquery import selectinquery
+from .counting_query_wrapper import CountingQuery
+from .reusable import Reusable
+from .mongoquery_settings_handler import MongoQuerySettingsHandler
+from .marker import Marker
+from .settings_dict import MongoQuerySettingsDict, StrictCrudHelperSettingsDict
+from .method_decorator import method_decorator, method_decorator_meta
+from .bulk import \
+    EntityDictWrapper, load_many_instance_dicts, \
+    model_primary_key_columns_and_names, entity_dict_has_primary_key
+
+try:
+    import nplus1loader
+except ImportError:
+    pass
+else:
+    from nplus1loader import raiseload_col, raiseload_rel, raiseload_all
