b7c07cb85def9cbe464be987e80f2ca990396b42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:28 +0000

    Add package Riki-Usermanager/1.0.1/Riki%20Usermanager-1.0.1.tar.gz

---
+++ packages/Riki-Usermanager/Riki%20Usermanager-1.0.1.tar.gz/Riki Usermanager-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="Riki Usermanager",
+    version="1.0.1",
+    description="User Manager utilities for the Riki",
+    author="Arseny Poga",
+    author_email="pogaa1@nku.edu",
+    packages=find_packages(),
+)
