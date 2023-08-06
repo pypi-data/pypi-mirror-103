d92f2a8f5a0c1107eacd7d1b1484ac954058d9fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:42 +0000

    Add package deep-net/0.1.1/deep-net-0.1.1.tar.gz

---
+++ packages/deep-net/deep-net-0.1.1.tar.gz/deep-net-0.1.1/setup.py
@@ -0,0 +1,42 @@
+"""Copyright (c) 2021, Deep Net. All Rights Reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="deep-net",
+    version="0.1.1",
+    author="Nadun De Silva",
+    author_email="nadunrds@gmail.com",
+    description="A Simple Library for Neural Networks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nadundesilva/deep-net",
+    project_urls={
+        "Bug Tracker": "https://github.com/nadundesilva/deep-net/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=["numpy >= 1.19.5"],
+)
