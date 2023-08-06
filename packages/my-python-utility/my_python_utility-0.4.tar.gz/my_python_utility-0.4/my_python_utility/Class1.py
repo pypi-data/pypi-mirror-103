c172dee7b51360015b094479e86e8e37642c254b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:23 +0000

    Add package my-python-utility/0.4/my_python_utility-0.4.tar.gz

---
+++ packages/my-python-utility/my_python_utility-0.4.tar.gz/my_python_utility-0.4/my_python_utility/Class1.py
@@ -0,0 +1,12 @@
+from my_python_utility import Class2
+
+class Class1:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
+
+        p1 = Class2("Jingle", 36)
+        p1.myfunc()
