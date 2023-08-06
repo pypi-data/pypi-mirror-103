138543f6acdd29b95ed0ff3e3ddaa25d7a60f14f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:47 +0000

    Add package tsundere/1.1.0/tsundere-1.1.0.tar.gz

---
+++ packages/tsundere/tsundere-1.1.0.tar.gz/tsundere-1.1.0/setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+    name='tsundere',
+    version='1.1.0',
+    py_modules=['tsundere'],
+    author='1196499173',
+    author_email='1196499173@qq.com',
+    url='https://github.com/1196499173/python',
+    description='I won\'t tell you how it works'
+)
