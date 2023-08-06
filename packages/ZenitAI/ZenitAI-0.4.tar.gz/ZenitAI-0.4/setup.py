be9e89d604d1525f688a450cea61b7f87f1d5e1c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:11 +0000

    Add package ZenitAI/0.4/ZenitAI-0.4.tar.gz

---
+++ packages/ZenitAI/ZenitAI-0.4.tar.gz/ZenitAI-0.4/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ZenitAI",
+    version="0.4",
+    packages=find_packages(),
+    descrption="A collection of tools and utils for data analysis. Contains functions and tools for WOE-transformations and other utils",
+)
