4af66e6da6180d33bdd091c24a875fef581cd895
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:22 +0000

    Add package apikey/0.2.4/apikey-0.2.4.tar.gz

---
+++ packages/apikey/apikey-0.2.4.tar.gz/apikey-0.2.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+
+
+def read(fname):
+    with open(os.path.join(os.path.dirname(__file__), fname)) as fp:
+        s = fp.read()
+    return s
+
+
+def get_version(path):
+    with open(path, "r") as fp:
+        lines = fp.read()
+    for line in lines.split("\n"):
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(name='apikey',
+      version=get_version("apikey/__init__.py"),
+      description='save and load API keys from a file',
+      long_description=read('README.rst'),
+      url='http://github.com/ulf1/apikey',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='Apache License 2.0',
+      packages=['apikey'],
+      python_requires='>=3.6',
+      zip_safe=True)
