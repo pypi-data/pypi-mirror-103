9f56d9433cc1f6fa1024c9354e5fcdc88714f683
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:40 +0000

    Add package jtlib/0.1.1/jtlib-0.1.1-py3-none-any.whl

---
+++ packages/jtlib/jtlib-0.1.1-py3-none-any.whl/jtlib/emailer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import win32com.client as win32
+import os
+
+
+def create_outlook_message(body, subject, to, cc='', bcc='', attachments=[], auto_send=False):
+    outlook = win32.gencache.EnsureDispatch('Outlook.Application')
+
+    message = outlook.CreateItem(0)
+    message.To = recipients
+    if cc:
+        message.CC = cc
+    if bcc:
+        message.BCC = bcc
+    message.Subject = subject
+    message.Body = body
+    message.Recipients.ResolveAll()
+
+    if attachments:
+        for a in attachments:
+            message.attachments.Add(a)
+
+    if auto_send:
+        message.Send()
+    else:
+        message.Display(False)
+
+
+if __name__ == '__main__':
+    pass
