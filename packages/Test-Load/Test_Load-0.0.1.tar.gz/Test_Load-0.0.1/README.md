592a2f7b83dba7765d2f2a25abe4a01aaa9953c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:29 +0000

    Add package Test-Load/0.0.1/Test_Load-0.0.1.tar.gz

---
+++ packages/Test-Load/Test_Load-0.0.1.tar.gz/Test_Load-0.0.1/README.md
@@ -0,0 +1,7 @@
+# Test_Load
+#Test chuc nang cai dat online PIP
+# Example Package
+
+This is a simple example package. You can use
+[Github-flavored Markdown](https://guides.github.com/features/mastering-markdown/)
+to write your content.
---
+++ packages/Test-Load/Test_Load-0.0.1.tar.gz/Test_Load-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+setup(
+    name='Test_Load',
+    version='0.0.1',
+    packages=['Test_Load'],
+    url='https://github.com/huynhquoc1990/Test_Load.git',
+    license='MIT',
+    author='Huỳnh Tấn Quốc',
+    author_email='huynhquoc1990@gamil.com',
+    description='Test Load Pip',
+    long_description=long_description
+)
