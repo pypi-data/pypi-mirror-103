e0d7e0fd110df14643395eec0aee0a5849b451ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:41 +0000

    Add package functionalytics/0.1.3/functionalytics-0.1.3.tar.gz

---
+++ packages/functionalytics/functionalytics-0.1.3.tar.gz/functionalytics-0.1.3/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md") as file:
+    long_description = file.read()
+
+
+setup(
+    name="functionalytics",
+    version="0.1.3",
+    author="Elias Dabbas",
+    author_email="eliasdabbas@gmail.com",
+    description="Analytics and logging for your functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "functionalytics"},
+    packages=find_packages(where="functionalytics"),
+    python_requires=">=3.6",
+)
