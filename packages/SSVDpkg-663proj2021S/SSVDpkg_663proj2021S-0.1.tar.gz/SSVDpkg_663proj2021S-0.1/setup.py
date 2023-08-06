4fc1eb593406fb81ad923283c68219dca81b1b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:14 +0000

    Add package SSVDpkg-663proj2021S/0.1/SSVDpkg_663proj2021S-0.1.tar.gz

---
+++ packages/SSVDpkg-663proj2021S/SSVDpkg_663proj2021S-0.1.tar.gz/SSVDpkg_663proj2021S-0.1/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(name='SSVDpkg_663proj2021S',
+      version='0.1',
+      description='SSVD algorithm for biclustering',
+      author='Aihua Li, Yuxuan Chen',
+      author_email='yc450@duke.edu',
+      packages=['SSVDpkg_663proj2021S'],
+      url='https://github.com/YuxuanMonta/STA663-final-project-AY',
+      classifiers=[
+                  'Intended Audience :: Developers',
+                  'Topic :: Software Development :: Libraries :: Python Modules',
+                  'License :: OSI Approved :: MIT License',
+                  'Programming Language :: Python :: 3',
+                  ],
+      python_requires=">=3.6",
+      zip_safe=False)
+
