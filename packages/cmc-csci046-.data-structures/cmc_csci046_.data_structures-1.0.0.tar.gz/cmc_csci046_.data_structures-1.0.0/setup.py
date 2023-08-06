3ff5eff528846fb33909d84ac669c075fdec3fd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:32 +0000

    Add package cmc-csci046-.data-structures/1.0.0/cmc_csci046_.data_structures-1.0.0.tar.gz

---
+++ packages/cmc-csci046-.data-structures/cmc_csci046_.data_structures-1.0.0.tar.gz/cmc_csci046_.data_structures-1.0.0/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+ name="cmc_csci046_.data_structures",
+ version="1.0.0",
+ description="Implement efficent data structures",
+ long_description=README,
+ long_description_content_type="text/markdown",
+ url="https://github.com/michaelhess17/containers-oop/tree/master",
+ author="Michael Hess",
+ author_email="mhess21@cmc.edu",
+ license="MIT",
+ classifiers=[
+     "License :: OSI Approved :: MIT License",
+     "Programming Language :: Python :: 3",
+     "Programming Language :: Python :: 3.7",
+             ],
+ packages=["containers"],
+ include_package_data=True,
+ install_requires=["pytest", "attrs", "hypothesis"])
