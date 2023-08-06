18c6cfabf7e1b891c39113d0047b88c819a4cbd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:55 +0000

    Add package nes-torch/1.0.0/nes-torch-1.0.0.tar.gz

---
+++ packages/nes-torch/nes-torch-1.0.0.tar.gz/nes-torch-1.0.0/example.py
@@ -0,0 +1,114 @@
+from typing import Callable, List, Optional
+from dataclasses import field
+from itertools import tee
+
+import gym
+from pipcs import Config
+import torch
+from torch import nn
+import numpy as np
+
+from nes import NES, Policy, default_config
+
+
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+
+class Agent(Policy):
+    def __init__(self, input_size, hidden_layers, output_size, output_func,
+                 hidden_act, output_act=None, bias=True):
+        super().__init__()
+        layers: List[nn.Module] = []
+        layer_sizes = [input_size] + hidden_layers + [output_size]
+        for idx, (layer1, layer2) in enumerate(pairwise(layer_sizes)):
+            layers.append(nn.Linear(layer1, layer2, bias=bias))
+            if idx < len(layer_sizes) - 2:
+                layers.append(hidden_act())
+        if output_act is not None:
+            layers.append(output_act())
+        self.output_func = output_func
+        self.seq = nn.Sequential(*layers)
+
+    def rollout(self, env):
+        done = False
+        obs = env.reset()
+        total_reward = 0
+        while not done:
+            obs_tensor = torch.from_numpy(obs).float()
+            action = self.output_func(self.seq(obs_tensor))
+            obs, reward, done, _ = env.step(action)
+            total_reward += reward
+        return total_reward
+
+
+def after_optimize_hook(self):
+    reward = self.eval_policy(self.policy)
+    if reward > self.config.user_variables.best_reward:
+        self.config.user_variables.best_reward = reward
+    best_reward = self.config.user_variables.best_reward
+    print(f'Gen: {self.gen} Test Reward: {reward} Best Reward: {best_reward}')
+
+
+config = Config(default_config)
+
+
+def make_env(id):
+    return gym.make(id)
+
+
+@config('environment')
+class EnvironmentConfig():
+    make_env = make_env
+    id: str = 'LunarLander-v2'
+
+
+@config('policy')
+class PolicyConfig():
+    policy = Agent
+    env = NES.make_env(**config.environment.to_dict())
+    input_size: int = env.observation_space.shape[0]
+    if isinstance(env.action_space, gym.spaces.Discrete):
+        output_size: int = env.action_space.n
+        output_func: Callable[[int], int] = lambda x: x.argmax().item()
+    else:
+        output_size: int = env.action_space.shape[0]
+        output_func: Callable[[int], int] = lambda x: x.detach().numpy()
+    hidden_layers: List[int] = field(default_factory=lambda: [64, 32])
+    hidden_act: nn.Module = nn.ReLU
+    output_act: Optional[nn.Module] = nn.Tanh
+    bias: bool = True
+
+
+@config('optimizer')
+class OptimizerConfig():
+    lr = 0.02
+    optim_type = torch.optim.Adam
+
+
+@config('nes')
+class NESConfig():
+    n_rollout = 5
+    n_step = 500
+    l2_decay = 0.005
+    population_size = 256
+    sigma = 0.02
+    seed = 123123
+    after_optimize_hook = after_optimize_hook
+
+
+@config('user_variables')
+class UserVariables():
+    """Users can initialize variables as config to use in the hook function."""
+    best_reward: float = -np.inf
+
+
+if __name__ == '__main__':
+    nes = NES(config)
+    nes.train()
+
+    # Evaluate trained policy
+    print(f'Reward: {nes.eval_policy(nes.policy)}')
---
+++ packages/nes-torch/nes-torch-1.0.0.tar.gz/nes-torch-1.0.0/nes/config.py
@@ -0,0 +1,65 @@
+from typing import Optional, Type, Callable, Any
+
+import torch
+from pipcs import Config, Required, required
+
+from .nes import Policy, NES
+
+
+def after_optimize_hook(self):
+    pass
+
+
+default_config = Config()
+
+
+@default_config('environment')
+class EnvironmentConfig():
+    """**name: environment**
+
+    :ivar Callable[[Any], gym.Env] id: Function that returns a gym environment
+    """
+    make_env: Required[Callable[[Any], 'gym.Env']] = required
+
+
+@default_config('policy')
+class PolicyConfig():
+    """**name: policy**
+
+    :ivar Required[Type[Policy]] policy: torch.nn.Module with a rollout method
+    :ivar str device: torch device
+    """
+    policy: Required[Type[Policy]] = required
+    device: str = 'cpu'
+
+
+@default_config('optimizer')
+class OptimizerConfig():
+    """**name: optimizer**
+
+    :ivar float lr: Learning rate
+    :ivar Required[Type[torch.optim.Optimizer]] optim_type: torch optim module
+    """
+    lr: float = 0.02
+    optim_type: Required[Type[torch.optim.Optimizer]] = required
+
+
+@default_config('nes')
+class NESConfig():
+    """**name: nes**
+
+    :ivar int population_size: Population Size, higher means lower variance in
+        gradient calculation but higher memory consumption and longer training time.
+    :ivar int n_step: Number of training steps
+    :ivar float sigma: Standart deviation for population sampling
+    :ivar int n_rollout: Number of episodes per sampled policy.
+    :ivar Optional[int] seed: Random seed
+    :ivar Callable[[NES],None] after_optimize_hook: Executed after optim.step()
+    """
+    n_rollout: int = 1
+    n_step: Required[int] = required
+    l2_decay: float = 0.005
+    population_size: Required[int] = required
+    sigma: float = 0.02
+    seed: Optional[int] = None
+    after_optimize_hook: Callable[[NES], None] = after_optimize_hook
