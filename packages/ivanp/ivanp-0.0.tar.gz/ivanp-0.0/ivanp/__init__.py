96e042f4faa7dc7dd91222793f33dc2fb58eed4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:26 +0000

    Add package ivanp/0.0/ivanp-0.0.tar.gz

---
+++ packages/ivanp/ivanp-0.0.tar.gz/ivanp-0.0/ivanp/__init__.py
@@ -0,0 +1,1 @@
+from first_package.ivanp import *
---
+++ packages/ivanp/ivanp-0.0.tar.gz/ivanp-0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ivanp',
+      version='0.0',
+      description='',
+      packages=['ivanp'],
+      author_email='ivan.buryy.19@mail.ru',
+      zip_safe=False)
