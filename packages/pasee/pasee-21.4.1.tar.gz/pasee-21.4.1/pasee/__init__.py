23003e6889d6f3142bfcc979f83f7c6dffeff260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:18 +0000

    Add package pasee/21.4.1/pasee-21.4.1.tar.gz

---
+++ packages/pasee/pasee-21.4.1.tar.gz/pasee-21.4.1/pasee/__init__.py
@@ -0,0 +1,32 @@
+"""HTTP server managing users.
+"""
+
+
+# Version format is: YY.MM[.PATCH] (no leading 0s).
+__version__ = "21.4.1"
+
+
+class PaseeException(Exception):
+    """An exception parent of all Pasee exceptions."""
+
+
+class MissingSettings(ValueError, PaseeException):
+    """A mandatory setting is missing from the configuration file."""
+
+
+class Unauthorized(PaseeException):
+    """An exception raised when someone is *not* authorized to do an action."""
+
+    def __init__(self, reason, **kwargs):
+        self.reason = reason
+        super().__init__(**kwargs)
+
+
+class Unauthenticated(PaseeException):
+    """An exception used to distinguish incoming requests showing authentication
+    and requests without authentication
+    """
+
+    def __init__(self, reason, **kwargs):
+        self.reason = reason
+        super().__init__(**kwargs)
