00214d5e244e39f4a12411d4c482bd320403bfaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:01 +0000

    Add package gaboon/0.1/gaboon-0.1.tar.gz

---
+++ packages/gaboon/gaboon-0.1.tar.gz/gaboon-0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gaboon",
+    version="0.1",
+    author="Aniket Pal",
+    author_email="aniketindian8@gmail.com",
+    description="A collection of Algorithms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=['gaboon'],
+    url="https://github.com/betaoverflow/gaboon",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
