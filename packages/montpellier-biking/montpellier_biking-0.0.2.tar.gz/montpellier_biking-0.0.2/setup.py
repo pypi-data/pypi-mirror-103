bcd56f62b1ace902b45c5d5ed3d52804adb5127f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:27 +0000

    Add package montpellier-biking/0.0.2/montpellier_biking-0.0.2.tar.gz

---
+++ packages/montpellier-biking/montpellier_biking-0.0.2.tar.gz/montpellier_biking-0.0.2/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+from montpellier_biking import __version__ as current_version
+
+setup(
+  name='montpellier_biking',
+  version=current_version,
+  description='Visualization of the bicycle flux in Montpellier',
+  url='https://github.com/Poncheele/Montpellier_Biking',
+  author='Alleau Julie, Chaoui Wiam, Poncheele Clement et Seck, Gade',
+  author_email='julie.alleau@etu.umontpellier.fr',
+  license='MIT',
+  packages=['montpellier_biking', 'montpellier_biking.io',
+            'montpellier_biking.model',
+            'montpellier_biking.vis',
+            'montpellier_biking.data'],
+  zip_safe=False,
+  include_package_data=True
+)
