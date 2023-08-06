23ae9fe88a2c3fcf2903e0bce7d20f130a804710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:28 +0000

    Add package Office365-REST-Python-Client/2.3.3/Office365_REST_Python_Client-2.3.3-py3-none-any.whl

---
+++ packages/Office365-REST-Python-Client/Office365_REST_Python_Client-2.3.3-py3-none-any.whl/examples/sharepoint/files/download_file.py
@@ -0,0 +1,12 @@
+import os
+import tempfile
+
+from office365.sharepoint.client_context import ClientContext
+from tests import test_team_site_url, test_client_credentials
+
+ctx = ClientContext(test_team_site_url).with_credentials(test_client_credentials)
+file_url = '/sites/team/Shared Documents/big_buck_bunny.mp4'
+download_path = os.path.join(tempfile.mkdtemp(), os.path.basename(file_url))
+with open(download_path, "wb") as local_file:
+    file = ctx.web.get_file_by_server_relative_url(file_url).download(local_file).execute_query()
+print("[Ok] file has been downloaded: {0}".format(download_path))
