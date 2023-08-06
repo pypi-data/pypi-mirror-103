537f11f01bafc328fc7ec76093f2139ac953b189
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:55:06 +0000

    Add package ground/3.8.0/ground-3.8.0-py3-none-any.whl

---
+++ packages/ground/ground-3.8.0-py3-none-any.whl/ground/__init__.py
@@ -0,0 +1,3 @@
+"""Basis of computational geometry."""
+
+__version__ = '3.8.0'
---
+++ packages/ground/ground-3.8.0-py3-none-any.whl/ground/hints.py
@@ -0,0 +1,19 @@
+from .core.hints import (Box,
+                         Contour,
+                         Coordinate,
+                         Multipoint,
+                         Multipolygon,
+                         Multisegment,
+                         Point,
+                         Polygon,
+                         Segment)
+
+Box = Box
+Contour = Contour
+Coordinate = Coordinate
+Multipoint = Multipoint
+Multisegment = Multisegment
+Point = Point
+Segment = Segment
+Polygon = Polygon
+Multipolygon = Multipolygon
