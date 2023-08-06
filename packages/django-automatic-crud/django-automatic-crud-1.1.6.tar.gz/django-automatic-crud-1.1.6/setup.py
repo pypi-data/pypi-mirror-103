bf65c5d3f7ef760e3719ab757b84c7a60e97c672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:53:58 +0000

    Add package django-automatic-crud/1.1.6/django-automatic-crud-1.1.6.tar.gz

---
+++ packages/django-automatic-crud/django-automatic-crud-1.1.6.tar.gz/django-automatic-crud-1.1.6/setup.py
@@ -0,0 +1,33 @@
+import os
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setup(
+    name='django-automatic-crud',
+    version='1.1.6',
+    packages=['automatic_crud'],
+    include_package_data=True,
+    license='BSD License',
+    description='CRUDS Automáticos con Django',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/developerpe/django-automatic-crud',
+    author='Oliver Sandoval',
+    author_email='developerpeperu@gmail.com',
+    install_requires=[
+        'Django>=2.2',
+        'openpyxl==3.0.7',
+    ],
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.3'
+    ]
+)
