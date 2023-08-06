7d36966d5afc6d30bd99c61eba5f3c2bf670bf5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:27 +0000

    Add package codefast/0.0.3/codefast-0.0.3.tar.gz

---
+++ packages/codefast/codefast-0.0.3.tar.gz/codefast-0.0.3/codefast/__init__.py
@@ -0,0 +1,17 @@
+import codefast.utils as utils
+
+# Export methods and variables
+debug = utils.debug
+warning = utils.warning
+error = utils.error
+info = utils.info
+
+json = utils.JsonIO
+text = utils.TextIO
+net = utils.Network
+
+p = utils.p
+pp = utils.pp
+say = utils.TextIO.say
+
+
