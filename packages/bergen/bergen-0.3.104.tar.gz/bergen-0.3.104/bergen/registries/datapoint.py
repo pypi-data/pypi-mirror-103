6ada754f0661b86bd8cd721fc1647e267413c35c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:34:57 +0000

    Add package bergen/0.3.104/bergen-0.3.104.tar.gz

---
+++ packages/bergen/bergen-0.3.104.tar.gz/bergen-0.3.104/bergen/registries/datapoint.py
@@ -0,0 +1,64 @@
+from bergen.schema import DataPoint
+from bergen.wards.graphql.aiohttp import AIOHttpGraphQLWard
+from bergen.auths.base import BaseAuthBackend
+from typing import Callable, Dict
+from bergen.enums import DataPointType
+from bergen.clients.base import BaseWard
+import logging
+import asyncio
+from bergen.console import console
+logger = logging.getLogger(__name__)
+
+datapointregistry = None
+
+class DataPointRegistry(object):
+
+
+    def __init__(self) -> None:
+        self.pointIDPointMap: Dict[str, DataPoint] = {}
+        self.pointIDWardMap: Dict[str, BaseWard] = {}
+        self.pointIDNegotiationMap: Dict[str, BaseWard] = {}
+
+
+        self.builders =  {
+                # Default Builders for standard
+                DataPointType.GRAPHQL: lambda datapoint, bergen: AIOHttpGraphQLWard(host=datapoint.outward, port=datapoint.port, token=bergen.auth.getToken(), protocol="http", loop=bergen.loop, needs_negotiation=datapoint.needsNegotiation)
+        }
+
+    def registerClientBuilder(self, type:str , builder: Callable):
+        self.builders[type] = builder
+
+    def createWardForDatapoint(self, point, bergen) -> BaseWard:
+
+
+        if point.id in self.pointIDWardMap:
+            return self.pointIDWardMap[point.id]
+
+        logger.info(f"Creating new Ward for Datapoint {point}")
+
+
+        if point.type in self.builders:
+            builder = self.builders[point.type]
+            self.pointIDWardMap[point.id]  = builder(point, bergen)
+
+            return self.pointIDWardMap[point.id]
+        else:
+            raise NotImplementedError(f"We have no idea how to build the ward for this Datapoint {point.type}")
+
+    async def configureWards(self):
+        wards = [ward for id, ward in self.pointIDWardMap.items()]
+        names = [point.name for id, point in self.pointIDWardMap.items()]
+        await asyncio.gather(*[ward.configure() for ward in wards])
+
+        negotiation_results = await asyncio.gather(*[ward.negotiate() for ward in wards])
+        return {name: result for name, result in zip(names, negotiation_results)}
+
+
+
+
+
+def get_datapoint_registry() -> DataPointRegistry:
+    global datapointregistry
+    if datapointregistry is None:
+        datapointregistry = DataPointRegistry()
+    return datapointregistry
