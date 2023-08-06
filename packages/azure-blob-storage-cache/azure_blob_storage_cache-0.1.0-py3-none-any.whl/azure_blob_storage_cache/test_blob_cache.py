63fba7008e6bd6cad76bc5277bb205f80225bd9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:05 +0000

    Add package azure-blob-storage-cache/0.1.0/azure_blob_storage_cache-0.1.0-py3-none-any.whl

---
+++ packages/azure-blob-storage-cache/azure_blob_storage_cache-0.1.0-py3-none-any.whl/azure_blob_storage_cache/test_blob_cache.py
@@ -0,0 +1,13 @@
+
+from unittest import TestCase
+from . import BlobCache,cache
+from unittest.mock import patch
+
+class TestBlobCache(TestCase):
+
+    def test_bc(self):
+        pass
+
+    def test_decorator(self):
+        pass
+
