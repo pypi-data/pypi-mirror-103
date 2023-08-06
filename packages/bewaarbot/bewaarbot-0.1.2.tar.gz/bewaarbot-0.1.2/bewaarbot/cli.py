505073c133981436391087e91a1bdcd36297b7b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:35 +0000

    Add package bewaarbot/0.1.2/bewaarbot-0.1.2.tar.gz

---
+++ packages/bewaarbot/bewaarbot-0.1.2.tar.gz/bewaarbot-0.1.2/bewaarbot/cli.py
@@ -0,0 +1,15 @@
+"""We use this file as an example for some module."""
+
+
+def start_bot() -> None:
+
+    print("Starting bewaarbot")
+
+    # Main server loop
+    while True:
+
+        # DO SOMETHING
+        return
+
+
+__all__ = ["start_bot"]
