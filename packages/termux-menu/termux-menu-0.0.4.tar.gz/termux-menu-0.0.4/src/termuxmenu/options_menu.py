e78ae034d8c19663836870e0f5868dd4621e73af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:28 +0000

    Add package termux-menu/0.0.4/termux-menu-0.0.4.tar.gz

---
+++ packages/termux-menu/termux-menu-0.0.4.tar.gz/termux-menu-0.0.4/src/termuxmenu/options_menu.py
@@ -0,0 +1,15 @@
+"""
+
+    Termux Options Menu
+
+    AUTHOR: Carlos Pomares (https://www.github.com/pomaretta) 
+    LAST REV: 2021/04/20 
+
+"""
+
+from termuxmenu.default_menu import DefaultMenu
+
+class OptionsMenu(DefaultMenu):
+    def __init__(self, escape: str,options: list) -> None:
+        super().__init__(escape)
+        self.options = options
---
+++ packages/termux-menu/termux-menu-0.0.4.tar.gz/termux-menu-0.0.4/src/termuxmenu/question.py
@@ -0,0 +1,25 @@
+"""
+
+    Termux Question
+
+    AUTHOR: Carlos Pomares (https://www.github.com/pomaretta) 
+    LAST REV: 2021/04/20 
+
+"""
+
+import re
+from termuxmenu.validation_exception import ValidationException
+
+class Question():
+    shell = "> "
+    @staticmethod
+    def ask(message: str,escape: str) -> str:
+        print(("\n" + escape + "{}").format(message),end="")
+        return input(("\n" + escape + "{}").format(Question.shell))
+    @staticmethod
+    def ask(message: str,escape: str,pattern: str) -> str:
+        print(("\n" + escape + "{}").format(message),end="")
+        answer = input(("\n" + escape + "{}").format(Question.shell))
+        if not re.match(pattern=pattern,string=answer):
+            raise ValidationException("Validation failed, doesn't match with: {}".format(pattern))
+        return answer
