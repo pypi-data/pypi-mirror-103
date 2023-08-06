947bc9c8fdd6905196fcb7dda2a80dd9e585f50d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:57 +0000

    Add package kshingle/0.6.3/kshingle-0.6.3.tar.gz

---
+++ packages/kshingle/kshingle-0.6.3.tar.gz/kshingle-0.6.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+import pypandoc
+
+
+def get_version(path):
+    with open(path, "r") as fp:
+        lines = fp.read()
+    for line in lines.split("\n"):
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    raise RuntimeError("Unable to find version string.")
+
+
+setup(name='kshingle',
+      version=get_version("kshingle/__init__.py"),
+      description="Split strings into (character-based) k-shingles",
+      long_description=pypandoc.convert('README.md', 'rst'),
+      url='http://github.com/ulf1/kshingle',
+      author='Ulf Hamster',
+      author_email='554c46@gmail.com',
+      license='Apache License 2.0',
+      packages=['kshingle'],
+      install_requires=[
+          'numba>=0.52.0'
+      ],
+      python_requires='>=3.6',
+      zip_safe=False)
