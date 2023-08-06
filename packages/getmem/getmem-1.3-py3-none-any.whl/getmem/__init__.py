946c39a4b8ee97275682b3cd97e7fc54aad2da60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:38 +0000

    Add package getmem/1.3/getmem-1.3-py3-none-any.whl

---
+++ packages/getmem/getmem-1.3-py3-none-any.whl/getmem/__init__.py
@@ -0,0 +1,21 @@
+"""
+
+                888   888b     d888                      
+                888   8888b   d8888                      
+                888   88888b.d88888                      
+.d88b.  .d88b. 888888888Y88888P888 .d88b. 88888b.d88b.  
+d88P"88bd8P  Y8b888   888 Y888P 888d8P  Y8b888 "888 "88b 
+888  88888888888888   888  Y8P  88888888888888  888  888 
+Y88b 888Y8b.    Y88b. 888   "   888Y8b.    888  888  888 
+"Y88888 "Y8888  "Y888888       888 "Y8888 888  888  888 
+    888                                                 
+Y8b d88P                                                 
+"Y88P"                  
+
+Getmem by Shaurya Pratap Singh
+MIT LICENCE © SHAURYA PRATAP SINGH 2-0-2-1
+
+"""
+__version__ = 1.3
+__author__ = "Shaurya Pratap Singh"
+
