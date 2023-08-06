0585c5ab78adbf18374f3c7b6aff4e604c5af231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:51 +0000

    Add package pygitscrum/0.3.0/pygitscrum-0.3.0-py3-none-any.whl

---
+++ packages/pygitscrum/pygitscrum-0.3.0-py3-none-any.whl/pygitscrum/git_daily.py
@@ -0,0 +1,36 @@
+"""
+--daily scripts
+"""
+
+from termcolor import colored
+from pygitscrum.args import compute_args
+from pygitscrum.git import (
+    command_git_check,
+)
+
+
+def git_daily(files):
+    """
+    entry point for --daily
+    """
+    since = compute_args().daily
+    for repo in files:
+        author = command_git_check(repo, ["config", "user.name"])
+
+        log = command_git_check(
+            repo,
+            [
+                "--no-pager",
+                "log",
+                "--since=" + since,
+                "--branches=*",
+                "--date=format:%Y-%m-%d %H:%M",
+                "--author=" + author.rstrip(),
+                "--format=%ad - %h --- %s",
+                "--date-order",
+                "--reverse",
+            ],
+        )
+        if log != "":
+            print(colored(repo, "green"))
+            print(colored(log, "yellow").rstrip())
