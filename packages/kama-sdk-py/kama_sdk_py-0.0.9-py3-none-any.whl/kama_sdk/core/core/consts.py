4f7d29557355c1e9f7618010e2f5fa8bc3273049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:38 +0000

    Add package kama-sdk-py/0.0.9/kama_sdk_py-0.0.9-py3-none-any.whl

---
+++ packages/kama-sdk-py/kama_sdk_py-0.0.9-py3-none-any.whl/kama_sdk/core/core/consts.py
@@ -0,0 +1,26 @@
+http_api_ktea = 'server'
+local_exec_ktea = 'local_executable'
+virtual_ktea = 'virtual_ktea'
+
+FILE_PREFIX = "file"
+SUPPLIER_PREFIX = "get"
+
+pos = 'positive'
+neg = 'negative'
+rng = 'running'
+brk = 'broken'
+idl = 'idle'
+
+TARGET_STANDARD = 'standard'
+TARGET_INLIN = 'inline'
+TARGET_STATE = 'state'
+TARGET_PREFS = 'prefs'
+
+TARGET_TYPES = [TARGET_STANDARD, TARGET_INLIN, TARGET_STATE, TARGET_PREFS]
+DEFAULT_TARGET = TARGET_STANDARD
+
+MAIN_WORKER = 'default'
+TELEM_WORKER = 'telem'
+
+
+statuses = [pos, neg, rng, idl]
