3bdb4adf84f3c90cb1e1cf919097df976136d558
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:02 +0000

    Add package db-ud/0.0.5/db_ud-0.0.5.tar.gz

---
+++ packages/db-ud/db_ud-0.0.5.tar.gz/db_ud-0.0.5/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+setuptools.setup(
+    name="db_ud",
+    version="0.0.5",
+    author="lovic",
+    author_email="",
+    description="database connection",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Operating System :: POSIX :: Linux",
+    ],
+    install_requires=[
+        'pymysql',
+        'pandas',
+        'requests'
+    ],
+    python_requires='>=3.6'
+)
