ef34e1ef81d274a6786794568e80d91c7a80dbbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:32 +0000

    Add package pyimporters-csv/0.1.19/pyimporters_csv-0.1.19-py3-none-any.whl

---
+++ packages/pyimporters-csv/pyimporters_csv-0.1.19-py3-none-any.whl/pyimporters_csv/text.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+from typing import Type, Dict, Any, Generator, Union, List
+from fastapi import Query
+from progress.bar import Bar
+from pydantic import BaseModel
+from pydantic.dataclasses import dataclass
+from pyimporters_plugins.base import KnowledgeParserOptions, KnowledgeParserBase, Term
+
+
+@dataclass
+class TXTOptions(KnowledgeParserOptions):
+    """
+    Options for the TXT knowledge import
+    """
+    encoding: str = Query('utf-8', description="Encoding of the file")
+
+
+TXTOptionsModel = TXTOptions.__pydantic_model__
+
+
+class TXTKnowledgeParser(KnowledgeParserBase):
+    def parse(self, source: Path, options: Union[BaseModel, Dict[str, Any]], bar: Bar) -> Generator[Term, None, None]:
+        options = TXTOptionsModel(**options) if isinstance(options, dict) else options
+        bar.max = file_len(source)
+        bar.start()
+        with source.open("r", encoding=options.encoding) as fin:
+            for line in fin:
+                bar.next()
+                term = line.strip()
+                if term:
+                    yield Term(identifier=term)
+        bar.finish()
+
+    @classmethod
+    def get_schema(cls) -> KnowledgeParserOptions:
+        return TXTOptions
+
+    @classmethod
+    def get_model(cls) -> Type[BaseModel]:
+        return TXTOptionsModel
+
+    @classmethod
+    def get_extensions(cls) -> List[str]:
+        return ["txt", "text"]
+
+
+def file_len(input_file: Path):
+    with open(input_file) as f:
+        nr_of_lines = sum(1 for line in f)
+    return nr_of_lines
