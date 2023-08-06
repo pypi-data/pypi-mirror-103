8b04531e51581d21ef5208676f2b57ca4d745e4e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:51 +0000

    Add package er1s/0.1/er1s-0.1.tar.gz

---
+++ packages/er1s/er1s-0.1.tar.gz/er1s-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='er1s',
+      version='0.1',
+      description='er1s',
+      url='http://github.com/eris9/eris9',
+      author='Eris9/Sakurai07',
+      author_email='blzzardst0rm@gmail.com',
+      license='MIT',
+      packages=['Er1s'],
+      zip_safe=False)
