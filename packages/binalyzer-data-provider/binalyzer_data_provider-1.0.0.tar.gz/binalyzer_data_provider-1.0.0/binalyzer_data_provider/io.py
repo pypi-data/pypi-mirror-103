8d4162fa975491559a24768e12d415de4f857a0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:14 +0000

    Add package binalyzer-data-provider/1.0.0/binalyzer_data_provider-1.0.0.tar.gz

---
+++ packages/binalyzer-data-provider/binalyzer_data_provider-1.0.0.tar.gz/binalyzer_data_provider-1.0.0/binalyzer_data_provider/io.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+    binalyzer_data_provider.io
+    ~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    Data providers are used by templates for reading, modifying or writing data.
+
+    :copyright: 2020 Denis Vasilík
+    :license: MIT
+"""
+
+from binalyzer_core import DataProviderBase
