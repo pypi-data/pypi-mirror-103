7a47a0f8adba62d4f8f9b802219c7fc05db465ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:20 +0000

    Add package crypto-bot/3.0.0/crypto_bot-3.0.0-py3-none-any.whl

---
+++ packages/crypto-bot/crypto_bot-3.0.0-py3-none-any.whl/crypto_bot/app.py
@@ -0,0 +1,39 @@
+# Init application
+import asyncio
+import sys
+
+from crypto_bot import bots
+from crypto_bot.config import ConfigLoader, init_logger
+from crypto_bot.exchanges import Exchange
+from crypto_bot.price_indexer import PriceIndexer
+
+try:
+    cfg = sys.argv[1]
+except:
+    cfg = None
+
+bot_list = []
+bots.config_loader = ConfigLoader(cfg)
+config = bots.config_loader.active_config
+
+logger = init_logger(config['process']['log_level'])
+logger.info("Config loaded")
+
+exchanges = [Exchange.create(c, dict(d)) for c, d in config['exchanges'].items()]
+indexer = PriceIndexer(exchanges, config['process']['update_rate'])
+
+indexer.wait_exchanges()
+init_coins = set(config['discord']['bots'].values())
+for c in init_coins:
+    indexer.add_new_coin(c)
+
+logger.info("Start Bots")
+loop = asyncio.get_event_loop()
+for i, c in enumerate(config['discord']['bots'].items()):
+    chat_id = str(i + 1) if i + 1 > 9 else "0{}".format(i + 1)
+    bot = bots.create_bot(*c, chat_id, config['discord']['command_roles'], indexer)
+    loop.create_task(bot.start())
+    bot_list.append(bot)
+
+indexer.run()
+loop.run_forever()
---
+++ packages/crypto-bot/crypto_bot-3.0.0-py3-none-any.whl/crypto_bot/bots.py
@@ -0,0 +1,153 @@
+import asyncio
+import logging
+import threading
+import time
+from random import random
+
+import discord
+from discord.ext.commands import Bot, MissingRequiredArgument, CommandNotFound
+
+from crypto_bot.error import CoinNotFoundException
+
+config_loader = None
+
+
+class CoinAssociation:
+
+    def __init__(self, coin, membership):
+        self.coin = coin
+        self.update(coin)
+        self.membership = membership
+
+    def update(self, coin):
+        self.coin = coin.upper()
+        self.image = None
+
+
+class CryptoBot(Bot):
+
+    def __init__(self, token, coin, chat_id, command_roles, indexer, *args, **kwargs):
+        super(CryptoBot, self).__init__(*args, **kwargs)
+        self.token = token
+        self.coin = coin.upper()
+        self.chat_id = chat_id
+        self.command_roles = {c.lower() for c in command_roles}
+        self.associations = {}
+        self.logger = logging.getLogger("{} bot".format(coin))
+        self.logger.info("Starting {} bot...".format(self.coin))
+        self.indexer = indexer
+
+    async def set_coin(self, guild, symbol):
+        symbol = str(symbol).lower()
+        coin = self.indexer.get_coin(symbol, wait=True)
+        self.associations[guild].update(symbol)
+        await self.update()
+        return coin
+
+    async def ready(self):
+        threading.Thread(target=self.update_memberships).start()
+        await self.status_loop()
+
+    async def status_loop(self):
+        while True:
+            await self.update()
+            await asyncio.sleep(0.5)
+
+    async def update(self):
+        for g, a in self.associations.items():
+            try:
+                c = self.indexer.get_coin(a.coin)
+                act = discord.Activity(type=discord.ActivityType.watching, name="{0} % {1}".format(c.perc, c.direction))
+                await a.membership.edit(nick="!{0} {1} {2}".format(self.chat_id, a.coin, c.price))
+                await self.change_presence(status=discord.Status.online, activity=act)
+            except Exception as e:
+                self.logger.error(e)
+
+    def update_memberships(self):
+        while True:
+            for g in self.guilds:
+                if g.id not in self.associations:
+                    m = [u for u in g.members if u.id == self.user.id]
+                    if not m:
+                        self.logger.warning("No matching member id found for {}".format(g.id))
+                        continue
+                    elif len(m) > 1:
+                        self.logger.warning("Multiple matches found for id for {} - using first match".format(g.id))
+                    self.associations[g.id] = CoinAssociation(self.coin, m[0])
+            time.sleep(60)
+
+    def describe(self):
+        data = {}
+        for a in self.associations.values():
+            data[a.membership.guild.name] = a.membership.nick
+        return data
+
+    async def start(self):
+        await super().start(self.token)
+
+
+def create_bot(token, coin, chat_id, command_roles, indexer):
+    bot = CryptoBot(command_prefix="!{} ".format(chat_id.lstrip("0")),
+                    token=token,
+                    coin=coin,
+                    chat_id=chat_id,
+                    command_roles=command_roles,
+                    indexer=indexer,
+                    case_insensitive=True)
+
+    @bot.event
+    async def on_ready():
+        bot.logger.info("#{} - {} is ready!".format(bot.chat_id, bot.coin))
+        await bot.ready()
+
+    @bot.command(name='feed', help='Get some soup')
+    async def feed(ctx):
+        if random() >= 0.7:
+            await ctx.send("You may eat today {}, Qapla'!".format(ctx.author.name))
+        else:
+            await ctx.send("No soup for you!")
+
+    @bot.command(name='set', help='Sets a specific coin by symbol. Usage: ![#] set DOGE - # indicates bot number')
+    async def set_coin(ctx, symbol):
+        if not ctx.author.id == ctx.guild.owner_id and \
+                not {r.name.lstrip('@').lower() for r in ctx.author.roles} & bot.command_roles:
+            await ctx.send("You do not have permission to use this function")
+            return
+        try:
+            await ctx.send("Attempting to set coin to {}".format(symbol))
+            coin = await bot.set_coin(ctx.guild.id, symbol)
+            if config_loader.is_home_id(ctx.guild.id):
+                config_loader.update_bot_coin(bot.token, symbol)
+            await ctx.send("Set bot #{0} to {1} - {2} successfully!"
+                           .format(bot.chat_id, coin.symbol.upper(), coin.name))
+        except CoinNotFoundException as e:
+            await ctx.send(e)
+        except Exception as e:
+            await ctx.send("An unexpected error occured")
+            bot.logger.error(e)
+
+    @bot.command(name='price', help='Get a price. Usage: 1[#] price DOGE - # indicates bot number')
+    async def get_price(ctx, symbol):
+        try:
+            c = bot.indexer.get_coin(symbol, wait=True)
+            msg = "{}/{}: ${}, change: {}% - indexed from {}"\
+                .format(symbol.upper(), c.name, c.price, c.perc, c.last_exchange)
+            await ctx.send(msg)
+        except Exception as e:
+            await ctx.send("Error: {}".format(e))
+
+    @bot.event
+    async def on_command_error(ctx, error):
+        if isinstance(error, CommandNotFound):
+            await ctx.send(error.args[0])
+        raise error
+
+    @set_coin.error
+    async def on_error(ctx, error):
+        if isinstance(error, MissingRequiredArgument):
+            await ctx.send("Missing required parameter: coin name - EG: !1 SET BTC")
+        else:
+            bot.logger.error("{} - {}".format(type(error), error.args[0]))
+            raise error
+
+    return bot
