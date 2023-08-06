bc2e4c4632442c2af63864fcac25c34b1917c8ca
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:34:34 +0000

    Add package scoss/0.0.4/scoss-0.0.4.tar.gz

---
+++ packages/scoss/scoss-0.0.4.tar.gz/scoss-0.0.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("README.md", mode='r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+PROJECT_URLS = {
+    'Bug Tracker': 'https://github.com/ngocjr7/scoss/issues',
+    'Documentation': 'https://github.com/ngocjr7/scoss/blob/master/README.md',
+    'Source Code': 'https://github.com/ngocjr7/scoss'
+}
+
+with open('requirements.txt') as f:
+    install_requires = f.read().strip().split('\n')
+
+
+setup(name='scoss',
+      description='A Source Code Similarity System',
+      author='Ngoc Bui',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      author_email='ngocjr7@gmail.com',
+      project_urls=PROJECT_URLS,
+      version='0.0.4', 
+      entry_points='''
+        [console_scripts]
+        scoss=scoss.cli:scoss_command
+      ''',
+      packages=find_packages(),
+      install_requires=install_requires,
+      python_requires='>=3.6')
