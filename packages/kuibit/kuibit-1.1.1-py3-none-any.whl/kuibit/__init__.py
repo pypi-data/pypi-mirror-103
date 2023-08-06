f783f3429284d65b3334c658075b9f2aacc5e6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:45 +0000

    Add package kuibit/1.1.1/kuibit-1.1.1-py3-none-any.whl

---
+++ packages/kuibit/kuibit-1.1.1-py3-none-any.whl/kuibit/__init__.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+# Copyright (C) 2020-2021 Gabriele Bozzola
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <https://www.gnu.org/licenses/>.
+
+"""kuibit provides utilities to post-process simulations performed with the
+Einstein Toolkit (or any Cactus-based code).
+
+"""
+
+__version__ = "1.1.1"
+
+__bibtex__ = """\
+@article{kuibit,
+  doi = {10.21105/joss.03099},
+  url = {https://doi.org/10.21105/joss.03099},
+  year = {2021},
+  publisher = {The Open Journal},
+  volume = {6},
+  number = {60},
+  pages = {3099},
+  author = {Gabriele Bozzola},
+  title = {kuibit: Analyzing Einstein Toolkit simulations with Python},
+  journal = {Journal of Open Source Software},
+  archivePrefix = {arXiv},
+  eprint = {2104.06376},
+  primaryClass = {gr-qc}
+}
+"""
