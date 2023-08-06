9b4cbf9ce2e9f33eb6c9dc4764ef2718d1ae7062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:37 +0000

    Add package mipkit/1.4.1/mipkit-1.4.1-py3-none-any.whl

---
+++ packages/mipkit/mipkit-1.4.1-py3-none-any.whl/mipkit/__init__.py
@@ -0,0 +1,11 @@
+from .images import *
+from .vis import *
+from .faces import *
+from .sys import *
+from .multiprocessing import *
+from .utils import *
+from .pytorch import *
+from .stats import *
+
+__version__ = '1.4.1'
+__author__ = "Cong M. Vo"
