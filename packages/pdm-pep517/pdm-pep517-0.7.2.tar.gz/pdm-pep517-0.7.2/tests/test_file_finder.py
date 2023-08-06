0f0cd1f77e4f9c34e23b2142d8532584b9c3d921
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:48 +0000

    Add package pdm-pep517/0.7.2/pdm-pep517-0.7.2.tar.gz

---
+++ packages/pdm-pep517/pdm-pep517-0.7.2.tar.gz/pdm-pep517-0.7.2/tests/test_file_finder.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+
+import pytest
+
+from pdm.pep517 import utils
+from pdm.pep517.base import Builder, is_same_or_descendant_path
+from tests import FIXTURES
+
+
+def test_auto_include_tests_for_sdist():
+    builder = Builder(FIXTURES / "projects/demo-package-with-tests")
+    with utils.cd(builder.location):
+        sdist_files = builder.find_files_to_add(True)
+        wheel_files = builder.find_files_to_add(False)
+
+    sdist_only_files = ("tests/__init__.py", "LICENSE", "pyproject.toml")
+    include_files = ("my_package/__init__.py",)
+    for file in include_files:
+        path = Path(file)
+        assert path in sdist_files
+        assert path in wheel_files
+
+    for file in sdist_only_files:
+        path = Path(file)
+        assert path in sdist_files
+        assert path not in wheel_files
+
+
+@pytest.mark.parametrize(
+    "target,path,expect",
+    [
+        ("a/b", "a", True),
+        ("a/b/c", "a/b/c", True),
+        ("b/c", "a", False),
+        ("a", "a/b", False),
+        ("a", "b/c", False),
+    ],
+)
+def test_is_same_or_descendant_path(target, path, expect):
+    assert is_same_or_descendant_path(target, path) == expect
