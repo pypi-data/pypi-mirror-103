f8064f91ed4ccea007e68ea25503114dba851e83
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:07 +0000

    Add package sm-tools/0.0.2/sm_tools-0.0.2.tar.gz

---
+++ packages/sm-tools/sm_tools-0.0.2.tar.gz/sm_tools-0.0.2/src/step_parser/constants.py
@@ -0,0 +1,9 @@
+import os
+
+
+ROOT_DIR = os.path.abspath(os.sep)
+ERROR_LOG = "sm_tools_error.log"
+
+# These numbers in a stepchart indicate notes that require stepping on
+# 1 = note, 2 = hold, 4 = roll
+NOTE_TYPES = ["1", "2", "4"]
