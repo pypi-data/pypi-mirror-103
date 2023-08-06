1c2400e597dcf435e54339edcee77ad31922d174
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:29 +0000

    Add package ocitoollib/0.1.0/ocitoollib-0.1.0.tar.gz

---
+++ packages/ocitoollib/ocitoollib-0.1.0.tar.gz/ocitoollib-0.1.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='ocitoollib',
+    packages=find_packages(include=['ocitoollib']),
+    version='0.1.0',
+    description='ocitool Python library',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='James Jeong',
+    author_email='james1.jeong@gmail.com',
+    license='MIT',
+    install_requires=['oci'],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    test_suite='tests',
+    python_requires=">=3.6",
+)
