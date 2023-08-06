e1429fbfd5d2c1cb8b112e8b6f3a80c6526ba1cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:40 +0000

    Add package canvassyncer/2.0.0/canvassyncer-2.0.0.tar.gz

---
+++ packages/canvassyncer/canvassyncer-2.0.0.tar.gz/canvassyncer-2.0.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="canvassyncer",
+    version="2.0.0",
+    description="A canvas file syner",
+    url="https://github.com/BoYanZh/Canvas-Syncer",
+    author="SJTU JI Tech",
+    author_email="bomingzh@sjtu.edu.cn",
+    packages=find_packages(),
+    python_requires=">=3.6",
+    entry_points={"console_scripts": ["canvassyncer=canvassyncer:main",],},
+    project_urls={
+        "Bug Reports": "https://github.com/BoYanZh/Canvas-Syncer/issues",
+        "Source": "https://github.com/BoYanZh/Canvas-Syncer",
+    },
+    install_requires=["aiohttp", "aiofiles", "tqdm"],
+)
