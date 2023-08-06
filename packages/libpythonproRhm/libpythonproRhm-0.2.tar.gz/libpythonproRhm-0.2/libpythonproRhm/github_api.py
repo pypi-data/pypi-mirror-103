5915ffaa8551507e0c99a50b8edc6ae716ceed88
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:33:59 +0000

    Add package libpythonproRhm/0.2/libpythonproRhm-0.2.tar.gz

---
+++ packages/libpythonproRhm/libpythonproRhm-0.2.tar.gz/libpythonproRhm-0.2/libpythonproRhm/github_api.py
@@ -0,0 +1,18 @@
+import requests as requests
+
+
+def buscar_avatar(usuario):
+    """
+    Busca o avatar de um usuário no Github
+
+    :param usuario: str com nome de usuário no Github
+    :return: str
+    """
+    url = f'https://api.github.com/users/{usuario}'
+    resp = requests.get(url)
+    return resp.json()['avatar_url']
+
+
+if __name__ == '__main__':
+
+    print(buscar_avatar('RicardoHMoreno'))
