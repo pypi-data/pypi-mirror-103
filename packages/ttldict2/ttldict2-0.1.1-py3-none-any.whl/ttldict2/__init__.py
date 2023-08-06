454888ca55c878febf3f40017b7a0ca7970b1cb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:09 +0000

    Add package ttldict2/0.1.1/ttldict2-0.1.1-py3-none-any.whl

---
+++ packages/ttldict2/ttldict2-0.1.1-py3-none-any.whl/ttldict2/__init__.py
@@ -0,0 +1,14 @@
+"""
+ttldict2
+Another dictionary with expiring keys.
+
+:copyright: 2021 Nándor Mátravölgyi
+:license: Apache2, see LICENSE for more details.
+"""
+
+__author__ = "Nándor Mátravölgyi"
+__copyright__ = "Copyright 2021 Nándor Mátravölgyi"
+__author_email__ = "nandor.matra@gmail.com"
+__version__ = "0.1.1"
+
+from .impl import TTLDict
