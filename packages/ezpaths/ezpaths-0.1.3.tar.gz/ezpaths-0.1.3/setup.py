94caeb8c892eb30efef0ec719120ec6965d6f1a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:53 +0000

    Add package ezpaths/0.1.3/ezpaths-0.1.3.tar.gz

---
+++ packages/ezpaths/ezpaths-0.1.3.tar.gz/ezpaths-0.1.3/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+setup(
+    name="ezpaths",
+    packages=["ezpaths"],
+    version="0.1.3",
+    license="MIT",
+    description="Python module which provides shorthand methods for frequent os.path one-liners.",
+    author="Bradley Fallon",
+    author_email="bradley.fallon+code@gmail.com",
+    url="https://github.com/Gastropod/ezpaths",
+    download_url="https://github.com/Gastropod/ezpaths/archive/v_01.tar.gz",
+    keywords=["PATHS", "UTILS", "OS"],
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+    ],
+)
