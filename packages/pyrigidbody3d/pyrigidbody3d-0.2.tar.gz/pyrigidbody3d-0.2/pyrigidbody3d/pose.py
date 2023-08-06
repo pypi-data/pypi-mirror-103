6bb169f353b2da4e050b96edd1a1f473bff5dccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:47 +0000

    Add package pyrigidbody3d/0.2/pyrigidbody3d-0.2.tar.gz

---
+++ packages/pyrigidbody3d/pyrigidbody3d-0.2.tar.gz/pyrigidbody3d-0.2/pyrigidbody3d/pose.py
@@ -0,0 +1,31 @@
+# pylint: disable=invalid-name
+# Lint as: python3
+"""Pose definition of a 3D coordinate frame.
+
+Notation:
+
+  pose_a = pose.Pose
+
+"""
+
+import numpy as onp
+
+from pyrigidbody3d import quaternion
+
+
+class Pose(object):
+  """Pose is a coordinate frame specified as position and orientation quaternion."""
+
+  def __init__(self, position=onp.zeros(3), orientation=quaternion.identity()):
+    self.position = position
+    self.orientation = quaternion.identity()
+
+  def transform(self, point):
+    """Transform a point by a pose."""
+    return quaternion.rotate(self.orientation, point) + self.position
+
+  def inverse_transform(self, point):
+    """Inverse transform a point by a pose."""
+    point_out = point - self.position
+    inverse_orientation = quaternion.inverse(self.orientation)
+    return quaternion.rotate(inverse_orientation, point_out)
