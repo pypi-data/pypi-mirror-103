954a7cffbe9bc99338d894efdca01d92197516e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:53 +0000

    Add package hspylib/0.9.21/hspylib-0.9.21-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.9.21-py3-none-any.whl/hspylib/core/crud/db/__init__.py
@@ -0,0 +1,14 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.9.21
+#
+# Package: main.hspylib.core.crud.db
+
+__all__ = [
+  'db_repository', 
+  'firebase', 
+  'mysql', 
+  'postgres', 
+  'sql', 
+  'sql_factory'
+]
