0fb8b78c3228f0f2e14f7815b55fc2d52ac7efef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:36:39 +0000

    Add package hspylib/0.9.22/hspylib-0.9.22-py3-none-any.whl

---
+++ packages/hspylib/hspylib-0.9.22-py3-none-any.whl/hspylib/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.9.22
+#
+# Package: main.hspylib
+
+__all__ = [
+  'core', 
+  'modules', 
+  'ui'
+]
---
+++ packages/hspylib/hspylib-0.9.22-py3-none-any.whl/hspylib/modules/mock/__init__.py
@@ -0,0 +1,11 @@
+# _*_ coding: utf-8 _*_
+#
+# HSPyLib v0.9.22
+#
+# Package: main.hspylib.modules.mock
+
+__all__ = [
+  'mock_request', 
+  'mock_server', 
+  'mock_server_handler'
+]
