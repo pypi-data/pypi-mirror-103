6c5f3234491bccc52f66f9d85afbba0b166382b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:43 +0000

    Add package companynameparser/0.1.1/companynameparser-0.1.1.tar.gz

---
+++ packages/companynameparser/companynameparser-0.1.1.tar.gz/companynameparser-0.1.1/companynameparser/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing(xuming624@qq.com)
+@description: 
+"""
+
+from companynameparser import parser
+from companynameparser.parser import Parser
+
+__version__ = "0.1.1"
+
+np = Parser()
+parse = np.parse
+parse_one = np.parse_one
