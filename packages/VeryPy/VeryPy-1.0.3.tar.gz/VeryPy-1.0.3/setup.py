028374614377249c5baae8b872fece528d9370e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:56 +0000

    Add package VeryPy/1.0.3/VeryPy-1.0.3.tar.gz

---
+++ packages/VeryPy/VeryPy-1.0.3.tar.gz/VeryPy-1.0.3/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.0.3'
+DESCRIPTION = 'An inline run-time file integrity checker'
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+
+
+setup(
+  name = 'VeryPy', 
+  version = VERSION,        
+  packages = ['VeryPy'],      
+  license='MIT',        
+  description = DESCRIPTION, 
+  long_description_content_type = "text/markdown",
+  long_description = long_description,  
+  author = 'Sombodee (Landon Hutchins) - Travis Mackey - Joe Alagoa ',                   
+ 
+  keywords = ['run-time', 'Fernet', 'SHA-1', 'Integrity'],   
+  
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',           
+    'Topic :: Software Development :: Build Tools',
+       
+    'Programming Language :: Python :: 3',      
+    
+  ],
+)
