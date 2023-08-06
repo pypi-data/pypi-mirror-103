24d471e1855a9b46c5c3ce560d7c877a5eb445c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:22 +0000

    Add package elaina/0.10.17/elaina-0.10.17.tar.gz

---
+++ packages/elaina/elaina-0.10.17.tar.gz/elaina-0.10.17/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name='elaina',
+    version='0.10.17',
+    description='elaina',
+    py_modules=['elaina'],
+	packages=setuptools.find_packages(),
+
+    long_description='',
+    author='userElaina',
+    author_email='userElaina@google.com',
+    url='https://github.com/userElaina',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+    ],
+    keywords='elaina',
+    python_requires='>=3.6',
+)
