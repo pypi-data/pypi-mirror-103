8a45963b4c20a4cef1dd1f1fb9cc7d35d386a029
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:41 +0000

    Add package xk-user/0.0.1/xk_user-0.0.1-py3-none-any.whl

---
+++ packages/xk-user/xk_user-0.0.1-py3-none-any.whl/xk_user/user_client.py
@@ -0,0 +1,28 @@
+import grpc
+import sys
+import os
+
+p = os.path.abspath(os.path.basename(__file__))
+if p not in sys.path:
+    sys.path.append(p)
+
+import user_pb2
+import user_pb2_grpc
+
+
+
+def run(addr):
+    u = User(addr)
+    res = u.login()
+    return res
+
+
+class User:
+    def __init__(self, addr):
+        self.addr = addr
+
+    def login(self) -> user_pb2.LoginResponse:
+        channel = grpc.insecure_channel(self.addr)
+        stub = user_pb2_grpc.UserStub(channel)
+        response = stub.login(user_pb2.LoginRequest(name='you'))
+        return response
