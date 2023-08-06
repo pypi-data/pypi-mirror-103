e12fb3782bc26b21728faa10f99f4dd37b1452aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:13 +0000

    Add package byid/2021.4.24/byid-2021.4.24.tar.gz

---
+++ packages/byid/byid-2021.4.24.tar.gz/byid-2021.4.24/README.md
@@ -0,0 +1,1 @@
+# Fetch metadata records by identifier.
---
+++ packages/byid/byid-2021.4.24.tar.gz/byid-2021.4.24/setup.py
@@ -0,0 +1,36 @@
+import os
+import setuptools
+
+ROOT = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(ROOT, 'README.md')).read()
+
+setuptools.setup(
+    name="byid",
+    version="2021.4.24",
+    author="Donatus Herre",
+    author_email="pypi@herre.io",
+    license="MIT",
+    description="Fetch metadata records by identifier.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/herreio/byid",
+    project_urls={
+        "Bug Tracker": "https://github.com/herreio/byid/issues",
+    },
+    packages=setuptools.find_packages(),
+    install_requires=["requests", "tqdm", "ijson", "xmltodict"],
+    entry_points={
+      'console_scripts': [
+        'DOI = byid.doi.__main__:main',
+        'DOI-RA = byid.doi.__main__:main_ra',
+        ],
+    },
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "License :: OSI Approved :: MIT License"
+    ],
+)
