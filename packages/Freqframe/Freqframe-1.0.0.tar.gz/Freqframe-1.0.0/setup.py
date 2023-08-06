5f46ee08ed244e4d10ce48ba117b0819ba45f942
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:14 +0000

    Add package Freqframe/1.0.0/Freqframe-1.0.0.tar.gz

---
+++ packages/Freqframe/Freqframe-1.0.0.tar.gz/Freqframe-1.0.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup,Extension,find_packages
+classifiers=[
+'Development Status :: 5 - Production/Stable',
+'Intended Audience :: Developers',
+'Operating System :: Microsoft :: Windows :: Windows 10',
+'License :: OSI Approved :: MIT License',
+'Programming Language :: Python :: 3.7'
+]
+with open("README.txt","r") as fh:
+     long_discription=fh.read()
+
+setup(name="Freqframe",
+     version="1.0.0",
+     discription="It count the frequency of words",
+     long_description=long_discription,
+     long_description_content_type="text/markdown",
+     url="",
+     author="Syed M Ahmed Hassan Shah",
+     author_email="ahmedhassan.11012@gmail.com",
+     classifiers=classifiers,
+     keywords=['text processing','data analysis','data science'],
+     packages=find_packages(),
+     install_requires=["Pandas"]
+     )
