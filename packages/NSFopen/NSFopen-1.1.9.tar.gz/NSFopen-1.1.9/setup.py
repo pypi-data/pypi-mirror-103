c7f23dbd303235e0078e50735359d4993b384f35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:37 +0000

    Add package NSFopen/1.1.9/NSFopen-1.1.9.tar.gz

---
+++ packages/NSFopen/NSFopen-1.1.9.tar.gz/NSFopen-1.1.9/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 12 12:34:04 2019
+
+@author: nelson
+"""
+
+from setuptools import find_packages, setup
+# from distutils.core import setup
+
+setup(
+    name='NSFopen',
+    version='1.1.9',
+    description='Read data and parameters from Nanosurf NID files',
+    author='Nanosurf',
+    author_email='scripting@nanosurf.com',
+    url="https://www.nanosurf.com",
+    packages = find_packages(),
+    include_package_data = True,
+    package_data = {
+        'NSFopen': ['example/*.nid',
+                    'example/Calculate_Roughness/*.*',
+                    'example/Lateral_Force/*.*',
+                    'example/Plotting_Data/*.*']},
+    # install_requires=["numpy","time","pandas"]
+    # package_dir = {'NSFopen':'NSFopen',
+    #                'NSFopen.read':'NSFopen//read',
+    #                'NSFopen.process':'NSFopen//process'},
+    # packages=['NSFopen','NSFopen.read','NSFopen.process']
+    )
