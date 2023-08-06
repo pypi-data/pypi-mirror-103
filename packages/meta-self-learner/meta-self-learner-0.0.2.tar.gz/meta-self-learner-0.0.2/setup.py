ca25ac975a82d511b037d9323f0c80aaa8d5f45b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:15 +0000

    Add package meta-self-learner/0.0.2/meta-self-learner-0.0.2.tar.gz

---
+++ packages/meta-self-learner/meta-self-learner-0.0.2.tar.gz/meta-self-learner-0.0.2/setup.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+'''
+__author__ = 'Ajay Arunachalam'
+__version__ = '0.0.2'
+__date__ = '27.4.2021'
+'''
+
+import sys
+
+from setuptools import find_packages, setup
+
+
+def setup_package():
+    metadata = dict(
+        name='meta-self-learner',
+        version='0.0.2',
+        description='Meta Ensemble Self-Learning with Optimization Objective Functions',
+        author='Ajay Arunachalam',
+        author_email='ajay.arunachalam08@gmail.com',
+        license='GNU General Public License',
+        url='https://github.com/ajayarunachalam/meta-self-learner',
+        packages=find_packages(),
+        py_modules=['msl/MetaLearning', 'msl/cf_matrix'],
+        long_description=open('./README.rst').read(),
+        long_description_content_type="text/x-rst",
+        install_requires=open('./requirements.txt').read().split()
+    )
+
+    setup(**metadata)
+
+
+if (__name__ == '__main__'):
+    setup_package()
