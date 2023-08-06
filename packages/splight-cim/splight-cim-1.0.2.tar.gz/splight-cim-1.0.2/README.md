de527cd94b1d998f70e5fc8c46488d12c6addb64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:45 +0000

    Add package splight-cim/1.0.2/splight-cim-1.0.2.tar.gz

---
+++ packages/splight-cim/splight-cim-1.0.2.tar.gz/splight-cim-1.0.2/README.md
@@ -0,0 +1,10 @@
+# cimlib
+
+Python library for parsing CIM files
+
+## Funcionamiento
+
+Para probar correr `test.py`. (Cuidado con el path del archivo que se intenta parsear). Tambien se puede imprimir en json sin guardarlo usando la funcion `get_json()` en vez de `save_json()`.
+
+
+reademeafknaelfknae'lfknalekf
---
+++ packages/splight-cim/splight-cim-1.0.2.tar.gz/splight-cim-1.0.2/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="splight-cim",
+    version="1.0.2",
+    description="Parse CIM RDF XML files and get output in JSON format",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://bitbucket.org/splight-dev/test/src/master/",
+    author="Splight",
+    author_email="splight@splight-ae.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages=["cimlib"],
+    include_package_data=True,
+)
