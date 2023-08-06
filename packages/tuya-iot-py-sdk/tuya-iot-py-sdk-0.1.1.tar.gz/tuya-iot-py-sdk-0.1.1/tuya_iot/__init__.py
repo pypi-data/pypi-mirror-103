6a6b891a26f77bc97eec9d920fc5dad9972fdde2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:36 +0000

    Add package tuya-iot-py-sdk/0.1.1/tuya-iot-py-sdk-0.1.1.tar.gz

---
+++ packages/tuya-iot-py-sdk/tuya-iot-py-sdk-0.1.1.tar.gz/tuya-iot-py-sdk-0.1.1/tuya_iot/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from .openapi import TuyaOpenAPI
+from .openmq import TuyaOpenMQ
+from .asset import TuyaAssetManager
+from .device import TuyaDeviceManager, TuyaDevice
