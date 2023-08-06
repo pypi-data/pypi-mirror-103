77d4165fe16899c4b1c868d5b2f80c9724f7a154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:24 +0000

    Add package tagscounteralexdoka/0.1.2/tagscounteralexdoka-0.1.2.tar.gz

---
+++ packages/tagscounteralexdoka/tagscounteralexdoka-0.1.2.tar.gz/tagscounteralexdoka-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name="tagscounteralexdoka",
+    packages=["tagscounteralexdoka"],
+    # find_packages(),
+    version="0.1.2",
+    author="Aliaksandr Dakutovich",
+    author_email="alex.doka@gmail.com",
+    description="app counting tags on web page",
+    long_description=readme(),
+    license="MIT",
+    include_package_data=True,
+    entry_points={'console_scripts': ['tagcounter = tagscounteralexdoka.tagcounter:main']},
+    install_requires=[
+         'argparse',
+         'datetime',
+         'certifi==2020.12.5',
+         'chardet', 
+         'future==0.18.2',
+         'PyYAML==5.4.1',
+         'idna==2.10',
+         'tk',
+         'requests==2.25.1',
+         'urllib3'
+      ]
+)
+
