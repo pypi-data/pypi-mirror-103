c90d3dc0f9c6e734440d826a402ff7003f8f1b76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:47 +0000

    Add package testindatadev/0.0.6/testindatadev-0.0.6.tar.gz

---
+++ packages/testindatadev/testindatadev-0.0.6.tar.gz/testindatadev-0.0.6/testindatadev/dataset/labeldata.py
@@ -0,0 +1,23 @@
+import json
+
+class LabelData():
+    def __init__(self):
+        self.labels = []
+
+    def AddLabels(self, label="", instance="", attrs={}, type="", data={}, index=""):
+        label = {
+            "label":label,
+            "instance":instance,
+            "type":type,
+            "attrs":attrs,
+            "data":data,
+        }
+
+        if index:
+            label["index"] = index
+
+        self.labels.append(label)
+
+    def ToString(self):
+        return json.dumps(self.labels)
+
