bcb41c8254c34f14d5dfff457d998577a49fc5b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:21 +0000

    Add package my-python-utility/0.2/my_python_utility-0.2.tar.gz

---
+++ packages/my-python-utility/my_python_utility-0.2.tar.gz/my_python_utility-0.2/my_python_utility/Class1.py
@@ -0,0 +1,7 @@
+class Class1:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def myfunc(self):
+        print("Hello my name is " + self.name)
---
+++ packages/my-python-utility/my_python_utility-0.2.tar.gz/my_python_utility-0.2/my_python_utility/__init__.py
@@ -0,0 +1,2 @@
+from my_python_utility.Class1 import Class1
+from my_python_utility.Class2 import Class2
