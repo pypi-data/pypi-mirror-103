2e8a99f94d236af00f42d282497cd4fcec5b80a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:06 +0000

    Add package test-delete-soon/1.0.1/test-delete-soon-1.0.1.tar.gz

---
+++ packages/test-delete-soon/test-delete-soon-1.0.1.tar.gz/test-delete-soon-1.0.1/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+print("I'm running!")
+
+import subprocess
+
+subprocess.run(["curl", "google.com"])
+
+setuptools.setup(
+    name="test-delete-soon",
+    version="1.0.1",
+    author="driazati",
+    author_email="email@example.com",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
