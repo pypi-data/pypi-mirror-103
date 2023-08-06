0466b3c912005add16a6929ee30c59f477c7ec3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:15 +0000

    Add package ivanp/0.9/ivanp-0.9.tar.gz

---
+++ packages/ivanp/ivanp-0.9.tar.gz/ivanp-0.9/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ivanp',
+      version='0.9',
+      description='description on site https://ivanp-pip.online/',
+      packages=['ivanp'],
+      author_email='ivan.buryy.19@mail.ru',
+      zip_safe=False)
