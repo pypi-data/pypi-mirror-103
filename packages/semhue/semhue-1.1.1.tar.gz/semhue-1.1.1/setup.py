295302d7f495a3185ac38deb4923968faf03afee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:14 +0000

    Add package semhue/1.1.1/semhue-1.1.1.tar.gz

---
+++ packages/semhue/semhue-1.1.1.tar.gz/semhue-1.1.1/setup.py
@@ -0,0 +1,29 @@
+from setuptools import *
+from os import path
+
+this_dir = path.abspath(path.dirname(__file__))
+with open(path.join(this_dir, "README.md"), encoding = "utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name = "semhue",
+    version = "1.1.1",
+    author = "Sem Moolenschot",
+    author_email = "",
+    url = "https://github.com/semmoolenschot/semhue",
+    description = "",
+    long_description = long_description,
+    long_description_content_type = "text/markdown",
+    packages = find_packages(),
+    include_package_data = True,
+    py_modules=["semhue"],
+    license = "MIT",
+    classifiers = [
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires = [
+    	"phue==1.1"
+    ],
+)
