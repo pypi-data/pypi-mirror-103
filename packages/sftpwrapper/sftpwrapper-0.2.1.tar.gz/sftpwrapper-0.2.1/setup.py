f5e15036d03cca5e839763f2219ef7fbe31940f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:54:07 +0000

    Add package sftpwrapper/0.2.1/sftpwrapper-0.2.1.tar.gz

---
+++ packages/sftpwrapper/sftpwrapper-0.2.1.tar.gz/sftpwrapper-0.2.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as file:
+        return file.read()
+
+
+setup(
+    name='sftpwrapper',
+    version='0.2.1',
+    license='MIT',
+    author='Kyle Wolfe',
+    author_email='kwolfe@northampton.edu',
+    python_requires='>3.6',
+    install_requires=['paramiko'],
+    packages=[
+        'sftpwrapper'
+    ],
+    url='',
+    description='A python program that facilitates uploading and downloading of files to/from an SFTP server',
+    long_description=readme(),
+    include_package_data=True,
+    keywords='sftp ftp',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Programming Language :: Python'
+    ],
+)
