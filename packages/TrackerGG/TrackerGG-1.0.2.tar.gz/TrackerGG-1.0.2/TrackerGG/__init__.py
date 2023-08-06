ae74d0ea313c2f123a3d4eb1f1ba6cc0c6c264ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:58 +0000

    Add package TrackerGG/1.0.2/TrackerGG-1.0.2.tar.gz

---
+++ packages/TrackerGG/TrackerGG-1.0.2.tar.gz/TrackerGG-1.0.2/TrackerGG/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+"""
+Copyright (c) 2021 DevRuby
+
+MIT License
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+"""
+
+from .exceptions import ApiError
+from .exceptions import UserError
+from .models import Client
+from .models import CsgoStatData
+from .models import CsgoProfileData
+from .models import CsgoStats
+from .models import PlatformInfo
+from .models import Platform
+from .info import __version__
+from .info import __author__
+from .info import __license__
+from .info import __copyright__
+
+__all__ = ['exceptions', 'info', 'models']
---
+++ packages/TrackerGG/TrackerGG-1.0.2.tar.gz/TrackerGG-1.0.2/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TrackerGG",
+    version="1.0.2",
+    author="DevRuby",
+    author_email="devruby7777@gmail.com",
+    description="Game Stat Library with Tracker API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dev-ruby/TrackerGG",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=[
+        "requests",
+        "aiohttp"
+    ],
+    python_requires=">=3.3",
+)
