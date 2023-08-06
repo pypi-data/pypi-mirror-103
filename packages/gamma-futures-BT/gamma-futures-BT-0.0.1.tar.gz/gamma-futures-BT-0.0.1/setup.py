024a371dcb6a018eece91afab60d940979d6e7d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:26 +0000

    Add package gamma-futures-BT/0.0.1/gamma-futures-BT-0.0.1.tar.gz

---
+++ packages/gamma-futures-BT/gamma-futures-BT-0.0.1.tar.gz/gamma-futures-BT-0.0.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="gamma-futures-BT", # Replace with your own username
+    version="0.0.1",
+    author="Gaurav Singh",
+    author_email="gauravsingh0109@gmail.com",
+    description="A small backtesting package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    project_urls={
+        
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
