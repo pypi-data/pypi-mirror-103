10589016a0f5eb7052e6b90140af4af757d96735
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:56 +0000

    Add package emcpy/0.1.0/emcpy-0.1.0-py3-none-any.whl

---
+++ packages/emcpy/emcpy-0.1.0-py3-none-any.whl/methods.py
@@ -0,0 +1,28 @@
+from util import online_player, town_data, nation_data, resident_data
+import requests
+"""
+More useful Methods will be added soon
+Give suggestions!
+"""
+def getAllOnline():
+    data = online_player("")
+    return data
+
+def getAllTowns():
+    data = town_data("")
+    return data
+
+def getAllNations():
+    data = nation_data("")
+    return data
+
+def getAllResidents():
+    data = resident_data("")
+    return data
+
+def getPlayerFace(name: str):
+    if name == "":
+        raise FileNotFoundError("Argument missing: name")
+    data = requests.get(f'https://earthmc.net/map/tiles/faces/16x16/{name}.png')
+    if data.status_code == 404:
+        raise FileNotFoundError("File not found")
