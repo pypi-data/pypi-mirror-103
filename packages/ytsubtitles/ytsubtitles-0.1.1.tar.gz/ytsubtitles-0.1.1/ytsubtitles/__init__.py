8fd1d71997c6b4ec2d88b2f6c7607d80d9c2239f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:44 +0000

    Add package ytsubtitles/0.1.1/ytsubtitles-0.1.1.tar.gz

---
+++ packages/ytsubtitles/ytsubtitles-0.1.1.tar.gz/ytsubtitles-0.1.1/ytsubtitles/__init__.py
@@ -0,0 +1,3 @@
+from ytsubtitles.subtitles_downloader import download_subs_arguments
+
+__all__ = ["download_subs_arguments"]
---
+++ packages/ytsubtitles/ytsubtitles-0.1.1.tar.gz/ytsubtitles-0.1.1/ytsubtitles/main.py
@@ -0,0 +1,5 @@
+from ytsubtitles.subtitles_downloader import download_subs_arguments
+
+
+if __name__ == '__main__':
+    download_subs_arguments()
