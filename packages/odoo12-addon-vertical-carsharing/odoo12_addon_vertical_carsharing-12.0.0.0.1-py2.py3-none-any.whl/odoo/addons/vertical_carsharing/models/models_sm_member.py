ff3a3ef5eca2cf8af683f3368b6a1562576f388a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:16 +0000

    Add package odoo12-addon-vertical-carsharing/12.0.0.0.1/odoo12_addon_vertical_carsharing-12.0.0.0.1-py2.py3-none-any.whl

---
+++ packages/odoo12-addon-vertical-carsharing/odoo12_addon_vertical_carsharing-12.0.0.0.1-py2.py3-none-any.whl/odoo/addons/vertical_carsharing/models/models_sm_member.py
@@ -0,0 +1,22 @@
+import time
+
+from odoo import models, fields, api
+from odoo.tools.translate import _
+
+class sm_member(models.Model):
+  _inherit = 'res.partner'
+  _name = 'res.partner'
+
+  image_dni = fields.Char(string=_("DNI image"))
+  driving_license_expiration_date = fields.Char(string=_("Driving license expiration date"))
+  image_driving_license = fields.Char(string=_("Driving license image"))
+  representative = fields.Char(string=_("Represented person "))
+  representative_dni = fields.Char(string=_("Represented person DNI/NIF"))
+  related_representative_member_id = fields.Many2one('res.partner',string=_("Related represented member"))
+
+  # for email templates
+  member_email_date = fields.Char(string=_("Current date"), compute='get_current_date', store=False)
+
+  def get_current_date(self):
+    for record in self:
+      record.member_email_date = time.strftime("%d/%m/%Y")
