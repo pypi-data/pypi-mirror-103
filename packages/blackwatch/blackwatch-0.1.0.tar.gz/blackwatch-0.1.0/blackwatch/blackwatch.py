4dda127c1e04a6c12aaec3abe43475ec8d78c9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:21 +0000

    Add package blackwatch/0.1.0/blackwatch-0.1.0.tar.gz

---
+++ packages/blackwatch/blackwatch-0.1.0.tar.gz/blackwatch-0.1.0/blackwatch/blackwatch.py
@@ -0,0 +1,17 @@
+import logging
+from cleo import Application
+
+from .commands import WatchCommand
+
+
+logger = logging.getLogger(__name__)
+console = logging.StreamHandler()
+logger.addHandler(console)
+
+
+class BlackWatchApplication(Application):
+    _version = "0.1.0"
+
+
+application = BlackWatchApplication()
+application.add(WatchCommand())
---
+++ packages/blackwatch/blackwatch-0.1.0.tar.gz/blackwatch-0.1.0/setup.py
@@ -0,0 +1,24 @@
+from distutils.core import setup
+
+
+setup(
+    name="blackwatch",
+    packages=["blackwatch"],
+    version="0.1.0",
+    license="MIT",
+    description="Watches a folder for file system events and run optional command (if required)",
+    author="Naveen Anil",
+    author_email="blackwatch@nvn-nil.in",
+    url="https://github.com/nvn-nil/blackwatch",
+    download_url="https://github.com/nvn-nil/blackwatch/archive/refs/tags/0.1.0.tar.gz",
+    keywords=["DEVTOOL", "UTILITY", "CLI"],
+    install_requires=["cleo==0.8.1", "watchdog==2.0.3"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+    ],
+)
