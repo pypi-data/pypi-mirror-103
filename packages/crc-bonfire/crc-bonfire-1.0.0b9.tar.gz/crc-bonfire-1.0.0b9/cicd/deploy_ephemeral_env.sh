9fcb51c884d9cd19d57b575767324042f3fce851
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:02 +0000

    Add package crc-bonfire/1.0.0b9/crc-bonfire-1.0.0b9.tar.gz

---
+++ packages/crc-bonfire/crc-bonfire-1.0.0b9.tar.gz/crc-bonfire-1.0.0b9/cicd/deploy_ephemeral_env.sh
@@ -0,0 +1,15 @@
+source $CICD_ROOT/_common_deploy_logic.sh
+
+# Deploy k8s resources for app and its dependencies (use insights-stage instead of insights-production for now)
+# -> use this PR as the template ref when downloading configurations for this component
+# -> use this PR's newly built image in the deployed configurations
+result=$(bonfire config deploy \
+    --ref-env insights-stage \
+    --app $APP_NAME \
+    --set-template-ref $COMPONENT_NAME=$GIT_COMMIT \
+    --set-image-tag $IMAGE=$IMAGE_TAG \
+    --get-dependencies)
+
+if [ $? -eq 0 ]; then
+    export NAMESPACE=$result
+fi
