0fdecf07cd43c3cdee9ecdecfd40ff7c55d70b97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:50 +0000

    Add package NlpToolkit-Math/1.0.13/NlpToolkit-Math-1.0.13.tar.gz

---
+++ packages/NlpToolkit-Math/NlpToolkit-Math-1.0.13.tar.gz/NlpToolkit-Math-1.0.13/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='NlpToolkit-Math',
+    version='1.0.13',
+    packages=['Math'],
+    url='https://github.com/StarlangSoftware/Math-Py',
+    license='',
+    author='olcaytaner',
+    author_email='olcay.yildiz@ozyegin.edu.tr',
+    description='Math library'
+)
