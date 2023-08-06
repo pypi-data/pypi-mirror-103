8272139963b41d133e34bd04f9fbec131c7d5150
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:03 +0000

    Add package cortx-jupyter/0.1.84/cortx_jupyter-0.1.84.tar.gz

---
+++ packages/cortx-jupyter/cortx_jupyter-0.1.84.tar.gz/cortx_jupyter-0.1.84/cortx_jupyter/cortx_file_manager.py
@@ -0,0 +1,40 @@
+import json 
+from .utils import (
+    _get_base64_file,
+    _save_base64_file
+    )
+from collections import Config
+
+Config = namedtuple('Config', [
+    'logger', 'prefix', 'region', 's3_bucket', 's3_host', 's3_auth',
+    'multipart_uploads', 'endpoint_url'
+])
+
+config_file_path = "credentials.json" 
+
+def _config():
+    config = {}
+    with open(config_file_path) as fp:
+       config = json.load(fp)
+
+    return Config(
+            region=config.region_name,
+            bucket_name=config.bucket_name,
+            host_name=config.host_name,
+            cortx_authenticator=config.authentication.get_credentials,
+            prefix=config.prefix,
+            multipart_uploads=config.multipart_uploads,
+            endpoint_url=config.endpoint_url
+        )
+
+def load_data(file_name):
+    if _check_file_exists(file_name):
+        return _get_base64_file(_config(), file_name, True)
+
+# Valid Formats: csv, json
+def save_data(file_name, data, format):
+    _save_base64_file(_config(), None, data, file_name)
+
+
+
+    
