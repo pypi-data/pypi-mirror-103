b0bdcb832112787c84929ee774871894e3ce099f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:45 +0000

    Add package python-console-creator/1.0.1/python_console_creator-1.0.1-py3-none-any.whl

---
+++ packages/python-console-creator/python_console_creator-1.0.1-py3-none-any.whl/console_creator/__init__.py
@@ -0,0 +1,26 @@
+import setuptools
+
+try:
+    with open("README.md", "r", encoding="utf-8") as fh:
+        long_description = fh.read()
+except FileNotFoundError:
+    long_description = "README.md not found."
+
+setuptools.setup(
+    name="python_console_creator",
+    version="1.0.1",
+    author="megat69",
+    description="Makes creating a console a simple task !",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/megat69/Lib_ConsoleCreator",
+    project_urls={},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
