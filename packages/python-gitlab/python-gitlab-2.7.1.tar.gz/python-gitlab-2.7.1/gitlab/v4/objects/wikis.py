f621499e4a1c10d6ecb76fb074c61c6bb48b529c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:53:20 +0000

    Add package python-gitlab/2.7.1/python-gitlab-2.7.1.tar.gz

---
+++ packages/python-gitlab/python-gitlab-2.7.1.tar.gz/python-gitlab-2.7.1/gitlab/v4/objects/wikis.py
@@ -0,0 +1,24 @@
+from gitlab.base import RequiredOptional, RESTManager, RESTObject
+from gitlab.mixins import CRUDMixin, ObjectDeleteMixin, SaveMixin
+
+
+__all__ = [
+    "ProjectWiki",
+    "ProjectWikiManager",
+]
+
+
+class ProjectWiki(SaveMixin, ObjectDeleteMixin, RESTObject):
+    _id_attr = "slug"
+    _short_print_attr = "slug"
+
+
+class ProjectWikiManager(CRUDMixin, RESTManager):
+    _path = "/projects/%(project_id)s/wikis"
+    _obj_cls = ProjectWiki
+    _from_parent_attrs = {"project_id": "id"}
+    _create_attrs = RequiredOptional(
+        required=("title", "content"), optional=("format",)
+    )
+    _update_attrs = RequiredOptional(optional=("title", "content", "format"))
+    _list_filters = ("with_content",)
