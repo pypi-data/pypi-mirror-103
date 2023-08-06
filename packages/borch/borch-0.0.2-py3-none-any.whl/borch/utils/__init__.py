b2de57b1aaacbba155cc9f51714f7c324cadc5c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:00 +0000

    Add package borch/0.0.2/borch-0.0.2-py3-none-any.whl

---
+++ packages/borch/borch-0.0.2-py3-none-any.whl/borch/utils/__init__.py
@@ -0,0 +1,13 @@
+"""
+Utility functions
+=================
+Mix of utility functions used in various places of ppl. The functions are separated
+into sub modules with minimal dependencies.
+
+
+NOTE:
+The utils package is not allowed to include imports from other parts of ppl. Any
+function that depends on an import from `ppl.*.**` where * is not utils should be
+placed in an appropriate `ppl.*.utils` file.
+
+"""
