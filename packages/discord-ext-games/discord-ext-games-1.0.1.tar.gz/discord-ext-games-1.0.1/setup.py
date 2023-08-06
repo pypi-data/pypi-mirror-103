236326ef3e139f9b001cdc28c91095516b694101
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:21 +0000

    Add package discord-ext-games/1.0.1/discord-ext-games-1.0.1.tar.gz

---
+++ packages/discord-ext-games/discord-ext-games-1.0.1.tar.gz/discord-ext-games-1.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+
+setup(
+    name="discord-ext-games",
+    author="Wiper-R",
+    url="https://github.com/Wiper-R/discord-ext-games",
+    version="1.0.1",
+    packages=["discord.ext.games"],
+    license="MIT",
+    description="This library provides bunch of games that can be played on discord.",
+    install_requires=["discord.py>=1.6.1"],
+    python_requires=">=3.8.9",
+)
