ca1e9bc7fdf01c84e4025b8db970df6e0a133da2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:26 +0000

    Add package ios-debug/0.0.0.36/ios-debug-0.0.0.36.tar.gz

---
+++ packages/ios-debug/ios-debug-0.0.0.36.tar.gz/ios-debug-0.0.0.36/iosdebug/processing/starting/add_mock_registrations_to_container_builder.py
@@ -0,0 +1,33 @@
+import iosdebug.logger as logger
+import re
+
+
+def add_mock_registrations_to_container_builder(swift_files, registrations):
+    print("Looking for ContainerBuilder.swift file")
+    try:
+        container_builder_file = [
+            file for file in swift_files if "ContainerBuilder.swift" in file
+        ][0]
+    except IndexError:
+        print("ContainerBuilder.swift file not found")
+    with open(container_builder_file, "r") as file:
+        content = file.read()
+        registration_rows = (
+            "// MARK: - DO NOT EDIT THIS PART WHILE IN DEBUG MODE ↓\n        "
+            + "\n        ".join(registrations)
+            + "\n        // MARK: - DO NOT EDIT THIS PART WHILE IN DEBUG MODE ↑"
+        )
+        repository_registration = re.findall(
+            r"static func registerRepository[\s\S]*?\n    \}", content
+        )[0]
+        repository_registration_processed = repository_registration.replace(
+            "return container", registration_rows + "\n        return container"
+        )
+
+    if registration_rows not in content:
+        with open(container_builder_file, "w") as file:
+            file.write(
+                content.replace(
+                    repository_registration, repository_registration_processed
+                )
+            )
