de18fa9b9e3ebe8ca180e30bc7952a5ef224fb95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:42 +0000

    Add package tinmanlol/1.3.0/tinmanlol-1.3.0.tar.gz

---
+++ packages/tinmanlol/tinmanlol-1.3.0.tar.gz/tinmanlol-1.3.0/tinmanlol.py
@@ -0,0 +1,24 @@
+'''
+这个“tinmanlol”模块，提供了一个名为print_lol()的函数，其作用是打印列表，顺利显示出其中可能包含（也可能不包含）的嵌套列表。
+'''
+'''
+取一个位置参数，名为“the_list”，这可以是任何Python列表，也可以是包含嵌套列表的列表。所指定列表中的每个数据项会（递归地）输出到屏幕上，各数据项各占一行。
+'''
+def print_lol(the_list, indent=False, level=0):  
+    for x in the_list:
+        if isinstance(x, list):
+            print_lol(x, indent, level+1)
+        else:
+            if indent:
+                for tab_stop in range(level):
+                    print("\t",end='')
+            print(x)
+
+'''
+movies=['The Holy Grail',1975,'Terry Jones & Terry Gilliam',91,['Graham Chapman',['Michael Palin','John Cleese','Terry Gilliam','Eric Idle','Terry Jones']]]
+print_lol(movies)
+'''
+'''
+names=['John','Eric',['Cleese','Idle'],'Michael',['Palin']]
+print_lol(names,True,2)
+'''
