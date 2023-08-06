5f8078265b15421b2616c3d85ba358f59cb35db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:27 +0000

    Add package spherical-stats/0.2/spherical_stats-0.2.tar.gz

---
+++ packages/spherical-stats/spherical_stats-0.2.tar.gz/spherical_stats-0.2/setup.py
@@ -0,0 +1,19 @@
+from distutils.core import setup
+#from setuptools import setup
+ 
+setup(
+    name='spherical_stats',   
+    version='0.2', 
+    description='Spherical statistics in Python',
+    author='Daniel Schmitz',
+    author_email='danielschmitzsiegen@gmail.com',
+    license='MIT',
+    packages=['spherical_stats'],
+    url='https://github.com/dschmitz89/spherical_stats',
+    download_url = 'https://github.com/dschmitz89/spherical_stats/archive/refs/tags/0.2.tar.gz',
+    install_requires=[
+        'numpy',
+        'numba>0.44',
+        'scipy>0.11'
+    ]             
+)
---
+++ packages/spherical-stats/spherical_stats-0.2.tar.gz/spherical_stats-0.2/spherical_stats/__init__.py
@@ -0,0 +1,6 @@
+from .esag import ESAG
+from .acg import ACG
+from .plot_utils import sphere, spherical_hist
+from .coordinate_systems import *
+from .descriptive_stats import *
+from .utils import *
