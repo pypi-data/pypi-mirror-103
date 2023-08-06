5636907b844b7ae66908294c9373f1b28bec9c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:11 +0000

    Add package superpehasher/0.6/superpehasher-0.6.tar.gz

---
+++ packages/superpehasher/superpehasher-0.6.tar.gz/superpehasher-0.6/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+from setuptools import find_packages
+from distutils.core import setup
+
+"""Returns contents of README.md."""
+with open("README.md", "r", encoding="utf-8") as readme_fp:
+    long_description = readme_fp.read()
+
+setup(
+  name = 'superpehasher',
+  packages = ['superpehasher'],
+  version = '0.6',
+  license='Apache',
+  author='Thomas Roccia @fr0gger_',
+  description = 'SuperPEHasher is a wrapper written in Python3 for several hash algorithms dedicated to PE file.',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  url = 'https://github.com/fr0gger/SuperPeHasher',
+  keywords = ['hashes', 'md5', 'sha256'],
+  install_requires=[
+          'mmh3',
+          'pefile',
+          'pyimpfuzzy',
+          'ssdeep',
+          'r2pipe',
+          'bitstring'
+      ],
+  classifiers=[
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: Apache Software License',
+    'Programming Language :: Python :: 3',
+  ],
+)
