63287b28f2de0ab3f9505501850135c141fef2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:46 +0000

    Add package gr-cli/0.4.0/gr_cli-0.4.0-py3-none-any.whl

---
+++ packages/gr-cli/gr_cli-0.4.0-py3-none-any.whl/gr/main.py
@@ -0,0 +1,11 @@
+import typer
+from gr.cmd import ch, repo
+
+app = typer.Typer()
+
+app.add_typer(ch.app, name="ch")
+app.add_typer(ch.app, name="pr")
+app.add_typer(repo.app, name="repo")
+
+if __name__ == "__main__":
+    app()
