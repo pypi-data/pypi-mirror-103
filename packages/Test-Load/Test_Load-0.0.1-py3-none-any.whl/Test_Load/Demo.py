7346008d962c619dab6cda9dc367d7033cd0df50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:28 +0000

    Add package Test-Load/0.0.1/Test_Load-0.0.1-py3-none-any.whl

---
+++ packages/Test-Load/Test_Load-0.0.1-py3-none-any.whl/Test_Load/Demo.py
@@ -0,0 +1,24 @@
+def XuatDuLieu(name):
+    print('Hi',name)
+# import setuptools
+# with open("README.md", "r", encoding="utf-8") as fh:
+#     long_description = fh.read()
+#
+# setuptools.setup(
+#     name='Test_Load',
+#     version='1.0.0',
+#     url='https://github.com/huynhquoc1990/Test_Load.git',
+#     license='MIT',
+#     author='Quoc',
+#     author_email='huynhquoc1990@gmail.com',
+#     description='A small example package',
+#     long_description=long_description,
+#     long_description_content_type="text/markdown",
+#     classifiers=[
+#         "Programming Language :: Python :: 3",
+#         "License :: OSI Approved :: MIT License",
+#         "Operating System :: OS Independent",
+#     ],
+#     packages=['Test_Load'],
+#     python_requires=">=3.6"
+# )
