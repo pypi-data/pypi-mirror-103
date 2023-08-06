a783704d42743a7d4e7ac0d156e145933af6000e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:54:20 +0000

    Add package zort/1.8.3/zort-1.8.3.tar.gz

---
+++ packages/zort/zort-1.8.3.tar.gz/zort-1.8.3/zort/photometry.py
@@ -0,0 +1,25 @@
+#! /usr/bin/env python
+"""
+photometry.py
+A set of functions for performing photometric calculations on lightcurves.
+"""
+
+import numpy as np
+
+
+def magnitudes_to_fluxes(mag, magerr=None, zero_point=22.0):
+    flux = 10 ** ((zero_point - mag) / 2.5)
+    if magerr is not None:
+        fluxerr = flux * magerr / 1.086
+    else:
+        fluxerr = None
+    return flux, fluxerr
+
+
+def fluxes_to_magnitudes(flux, fluxerr=None, zero_point=22.0):
+    mag = zero_point - 2.5 * np.log10(flux)
+    if fluxerr is not None:
+        magerr = 1.086 * fluxerr / flux
+    else:
+        magerr = None
+    return mag, magerr
