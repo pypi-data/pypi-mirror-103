67d6f066da36d849c0c61daa1cebffbe4c10db53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:09:27 +0000

    Add package phylactery/0.2.3/phylactery-0.2.3.tar.gz

---
+++ packages/phylactery/phylactery-0.2.3.tar.gz/phylactery-0.2.3/phylactery/__init__.py
@@ -0,0 +1,13 @@
+import sys
+
+PY2 = sys.version.startswith('2')
+
+# Not python2 compatible
+if not PY2:
+    from phylactery.unionfind import UnionFind
+    from phylactery.vptree import VPTree
+
+# python2 compatible
+from phylactery.bitset import BitSet
+from phylactery.fuzzydict import FuzzyDict
+from phylactery.triedict import TrieDict
