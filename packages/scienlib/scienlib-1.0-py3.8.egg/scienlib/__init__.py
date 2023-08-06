e9ac3ef8a6bf825a04bd63afaaecab9a440b760b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:38 +0000

    Add package scienlib/1.0/scienlib-1.0-py3.8.egg

---
+++ packages/scienlib/scienlib-1.0-py3.8.egg/scienlib/__init__.py
@@ -0,0 +1,19 @@
+from .math import *
+from .security import *
+
+basic = basic()
+fisic = fisic()
+scanner = scanner()
+
+velocity = basic.velocity
+divider = basic.divider
+average = basic.average
+
+sigmoid = fisic.sigmoid
+
+scanports = scanner.scanports
+scanport = scanner.scanport
+scanip = scanner.scanip
+scanweb = scanner.scanweb
+velocitywifi = scanner.velocitywifi
+
