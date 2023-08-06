a7c2e91ff37f63e9c94355c29cdb71b46a045018
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:01 +0000

    Add package turtledrawing/1.0.2/turtledrawing-1.0.2.tar.gz

---
+++ packages/turtledrawing/turtledrawing-1.0.2.tar.gz/turtledrawing-1.0.2/setup.py
@@ -0,0 +1,33 @@
+import pathlib
+from setuptools import setup
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="turtledrawing",
+    version="1.0.2",
+    description="Draw Shapes with the Turtle Module.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    author="orcawhale38",
+    author_email="orcawhale2008@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+    ],
+    packages=["shapes"],
+    include_package_data=True,
+    url="https://github.com/NotOrca22/learning-turtle",
+    entry_points={
+        "console_scripts": [
+            "realpython=reader.__main__:main",
+        ]
+    },
+)
