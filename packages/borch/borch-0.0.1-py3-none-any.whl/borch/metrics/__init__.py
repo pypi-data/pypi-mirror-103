e5a751aefd255b5e6ee3a119dddd10abd3892911
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:00 +0000

    Add package borch/0.0.1/borch-0.0.1-py3-none-any.whl

---
+++ packages/borch/borch-0.0.1-py3-none-any.whl/borch/metrics/__init__.py
@@ -0,0 +1,11 @@
+"""
+Module implementing metrics for torch tensors
+
+"""
+from borch.metrics.metrics import (
+    mean_squared_error,
+    accuracy,
+    accuracy_logit,
+    confusion_matrix,
+    binary_roc_auc,
+)
---
+++ packages/borch/borch-0.0.1-py3-none-any.whl/borch/ppl/utils/__init__.py
@@ -0,0 +1,9 @@
+""""
+Utility functions for ppl
+"""
+
+
+from borch.ppl.utils.model_conversion import model_to_neg_log_prob_closure
+from borch.ppl.utils.inference_conversion import rsample_else_sample
+from borch.ppl.utils.random_variable_utils import named_observed_random_variables
+from borch.ppl.utils.module_utils import *
