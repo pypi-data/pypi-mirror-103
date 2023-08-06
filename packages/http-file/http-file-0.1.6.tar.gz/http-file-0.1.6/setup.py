6b6b1343e9eda6a37ac0391b32bd6a9c91297b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:52:56 +0000

    Add package http-file/0.1.6/http-file-0.1.6.tar.gz

---
+++ packages/http-file/http-file-0.1.6.tar.gz/http-file-0.1.6/setup.py
@@ -0,0 +1,22 @@
+import pathlib
+from setuptools import find_packages, setup
+
+ROOT_DIR = pathlib.Path(__file__).parent
+
+setup(
+    name="http-file",
+    packages=find_packages(),
+    include_package_data=True,
+    version="0.1.6",
+    description="...",
+    long_description=(ROOT_DIR / "README.md").read_text(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+    ],
+    install_requires=('urllib3','requests'),
+    entry_points={},
+    author="Tyshko Stanislav",
+    author_email="admin@toqp.ru",
+    url="https://github.com/RivalStorms/http_file",
+)
