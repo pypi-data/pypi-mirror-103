1f614b67b0f64985101a4a1920b798fc2c5e5a12
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:53 +0000

    Add package ch1p/0.0.2/ch1p-0.0.2.tar.gz

---
+++ packages/ch1p/ch1p-0.0.2.tar.gz/ch1p-0.0.2/ch1p/state.py
@@ -0,0 +1,28 @@
+import os, json
+
+from .functions import _get_vars
+
+
+class State:
+    def __init__(self, default=None, **kwargs):
+        file, = _get_vars([
+            ('file', 'STATE_FILE')
+        ], kwargs)
+
+        if default is None:
+            default = {}
+
+        self.file = file
+        self.default = default
+
+    def read(self) -> dict:
+        if not os.path.exists(self.file):
+            self.write(self.default)
+            return self.default
+
+        with open(self.file, 'r') as f:
+            return json.loads(f.read())
+
+    def write(self, state: dict):
+        with open(self.file, 'w') as f:
+            f.write(json.dumps(state))
