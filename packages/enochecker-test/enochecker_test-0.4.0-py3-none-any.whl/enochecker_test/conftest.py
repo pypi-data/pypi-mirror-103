df3892807b47849640282149d8f75a00841dfeba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:44 +0000

    Add package enochecker-test/0.4.0/enochecker_test-0.4.0-py3-none-any.whl

---
+++ packages/enochecker-test/enochecker_test-0.4.0-py3-none-any.whl/enochecker_test/conftest.py
@@ -0,0 +1,7 @@
+def pytest_addoption(parser):
+    parser.addoption("--checker-address", action="store", type=str)
+    parser.addoption("--checker-port", action="store", type=int)
+    parser.addoption("--service-address", action="store", type=str)
+    parser.addoption("--flag-variants", action="store", type=int)
+    parser.addoption("--noise-variants", action="store", type=int)
+    parser.addoption("--havoc-variants", action="store", type=int)
