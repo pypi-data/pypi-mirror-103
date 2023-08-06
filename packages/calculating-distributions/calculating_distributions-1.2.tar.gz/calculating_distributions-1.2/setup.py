a8955a5b1fd27dbc025a0c02e21db269d5cf4749
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:57 +0000

    Add package calculating-distributions/1.2/calculating_distributions-1.2.tar.gz

---
+++ packages/calculating-distributions/calculating_distributions-1.2.tar.gz/calculating_distributions-1.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='calculating_distributions',
+      version='1.2',
+      description='Gaussian and Binomial Distributions',
+      packages=['calculating_distributions'],
+      author= 'Rodrigo Guarneros',
+      author_email = 'rodrigo.guarneros@gmail.com',
+      zip_safe=False)
