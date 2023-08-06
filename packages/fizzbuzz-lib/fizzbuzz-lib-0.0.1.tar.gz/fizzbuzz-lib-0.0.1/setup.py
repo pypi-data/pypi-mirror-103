a1767335b1638f047b3703e7ae8c0b7fd98c9dbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:50 +0000

    Add package fizzbuzz-lib/0.0.1/fizzbuzz-lib-0.0.1.tar.gz

---
+++ packages/fizzbuzz-lib/fizzbuzz-lib-0.0.1.tar.gz/fizzbuzz-lib-0.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+setup(
+name='fizzbuzz-lib',
+version='0.0.1',
+describtion='this is a feezbuzz game package. ',
+url='',
+author_Email='sadra85aba@gmail.com',
+authorname='sadra ',
+License='MIT',
+keywords='',
+install_requiers=['']
+ )
