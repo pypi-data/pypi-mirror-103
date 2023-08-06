2578aa5b844e1d7d8be862e19470f906f0b07f95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:22:24 +0000

    Add package pyrconsole/0.0.3/pyrconsole-0.0.3.tar.gz

---
+++ packages/pyrconsole/pyrconsole-0.0.3.tar.gz/pyrconsole-0.0.3/README.md
@@ -0,0 +1,12 @@
+pyrconsole
+======
+
+Python RConsole
+
+Install
+-------
+
+.. code:: bash
+
+    $ pip install pyrconsole
+
---
+++ packages/pyrconsole/pyrconsole-0.0.3.tar.gz/pyrconsole-0.0.3/setup.py
@@ -0,0 +1,29 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+requires = ['requests>=2.25.1']
+
+setuptools.setup(
+    name="pyrconsole",
+    version="0.0.3",
+    author="Guifang Zhou",
+    author_email="guifang03@126.com",
+    description="Python RConsole",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gzhou/pyrconsole",
+    project_urls={
+        "Bug Tracker": "https://github.com/gzhou/pyrconsole/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"pyr": "pyr"},
+    packages=['pyr'],
+    install_requires = requires,
+    python_requires=">=3.6",
+)
