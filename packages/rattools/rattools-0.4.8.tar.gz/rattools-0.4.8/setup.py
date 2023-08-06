17c6dfe4221a50a446dfb8343360e10ce8d3c75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:40 +0000

    Add package rattools/0.4.8/rattools-0.4.8.tar.gz

---
+++ packages/rattools/rattools-0.4.8.tar.gz/rattools-0.4.8/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+setup(
+    name="rattools",
+    version="0.4.8",
+    description="Este es un paquete de ejemplo",
+    author="Ldog",
+    author_email="domgutluis@gmail.com",
+    url="",
+    packages=['rattools'],
+    install_requires=[            # I get to this in a second
+          'yfinance',
+          'pandas',
+          'sklearn',
+          'matplotlib',
+          'seaborn']
+)
