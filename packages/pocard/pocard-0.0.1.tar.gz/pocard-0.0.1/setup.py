80ceb3ff912f557f91c43f2a6348d15962ea7db6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:43 +0000

    Add package pocard/0.0.1/pocard-0.0.1.tar.gz

---
+++ packages/pocard/pocard-0.0.1.tar.gz/pocard-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+from os import path
+base_dir = path.abspath(path.dirname(__file__))
+setup(
+  name = 'pocard',        
+  packages = ['pocard'],   
+  version = '0.0.1',    
+  license='MIT',     
+  description = 'PokemonCard Generator', 
+  author = 'Krypton Byte',                  
+  author_email = 'galaxyvplus6434@gmail.com',     
+  url = 'https://github.com/krypton-byte/pocard',   
+  download_url = 'https://github.com/krypton-byte/pocard/archive/0.0.1.tar.gz',    
+  keywords = ['POkemon', "pokemon", "yugioh", "card", "maker", "generator"], 
+  install_requires=[           
+          'pillow',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      
+    'Intended Audience :: Developers',      
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',  
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
