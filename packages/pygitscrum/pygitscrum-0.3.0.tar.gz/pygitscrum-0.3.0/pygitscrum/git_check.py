fb8ea52d4c9f6268d2ca242f3b135bae79b41638
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:51 +0000

    Add package pygitscrum/0.3.0/pygitscrum-0.3.0.tar.gz

---
+++ packages/pygitscrum/pygitscrum-0.3.0.tar.gz/pygitscrum-0.3.0/pygitscrum/git_check.py
@@ -0,0 +1,92 @@
+"""
+--check scripts
+"""
+
+from termcolor import colored
+from pygitscrum.git import (
+    command_git_call,
+    command_git_call_print,
+    command_git_check,
+    command_git_check_en,
+    command_git_check_en_print,
+)
+from pygitscrum.args import compute_args
+from pygitscrum.scan import absolute_path_without_git
+
+
+def git_check(files):
+    """
+    entry point for --check
+    """
+    files_to_work = []
+    for repo in files:
+        if compute_args().debug:
+            print(
+                "debug : " + absolute_path_without_git(repo) + " ..."
+            )
+
+        ############################################
+        # UPDATE + FETCH
+        ############################################
+        command_git_check_en_print(repo, ["remote", "update"], True)
+        command_git_check_en_print(repo, ["fetch", "--all"], True)
+        ############################################
+
+        ############################################
+        # GIT STATUS
+        ############################################
+        while "Your branch is up to date" not in command_git_check_en(
+            repo, ["status"]
+        ):
+            print(colored(absolute_path_without_git(repo), "yellow"))
+            print(
+                colored(
+                    command_git_check(repo, ["status"]),
+                    "yellow",
+                )
+            )
+            answer = input("p(ull)/P(ush)/s(how)/S(how all)/q(uit)? ")
+            if answer == "p":
+                command_git_call_print(repo, ["pull"], True)
+            elif answer == "P":
+                command_git_call_print(repo, ["push"], True)
+            elif answer == "S":
+                command_git_call(
+                    repo, ["log", "-p", "HEAD..FETCH_HEAD"]
+                )
+                command_git_call(
+                    repo, ["log", "-p", "FETCH_HEAD..HEAD"]
+                )
+            elif answer == "s":
+                command_git_call(
+                    repo,
+                    [
+                        "log",
+                        "-p",
+                        "--name-only",
+                        "HEAD..FETCH_HEAD",
+                    ],
+                )
+                command_git_call(
+                    repo,
+                    [
+                        "log",
+                        "-p",
+                        "--name-only",
+                        "FETCH_HEAD..HEAD",
+                    ],
+                )
+            else:
+                files_to_work.append(absolute_path_without_git(repo))
+                break
+
+    ############################################
+    if len(files_to_work) > 0:
+        print("")
+        print(
+            colored(
+                "Folders that will be checked : \n"
+                + "\n".join(files_to_work),
+                "yellow",
+            )
+        )
---
+++ packages/pygitscrum/pygitscrum-0.3.0.tar.gz/pygitscrum-0.3.0/pygitscrum/git_search.py
@@ -0,0 +1,37 @@
+"""
+--search scripts
+"""
+
+from termcolor import colored
+from pygitscrum.git import (
+    command_git_check,
+)
+from pygitscrum.args import compute_args
+
+
+def git_search(files):
+    """
+    entry point for --search
+    """
+    keyword = compute_args().search.lower()
+    for repo in files:
+        first = True
+        log = command_git_check(
+            repo,
+            [
+                "--no-pager",
+                "log",
+                "--branches=*",
+                "--date=format:%Y-%m-%d %H:%M",
+                "--all",
+                "--format=%ad - %h --- %S- %s - %ae - %aN",
+                "--date-order",
+            ],
+        )
+        if log != "":
+            for line_log in log.split("\n"):
+                if keyword in line_log.lower():
+                    if first:
+                        print(colored(repo, "green"))
+                        first = False
+                    print(colored(line_log.strip(), "yellow"))
