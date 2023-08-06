bfbb223b10d31ca5ac1028f45c32c301c238834c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:05 +0000

    Add package structlinks/1.0.8/structlinks-1.0.8.tar.gz

---
+++ packages/structlinks/structlinks-1.0.8.tar.gz/structlinks-1.0.8/structlinks/__init__.py
@@ -0,0 +1,10 @@
+from structlinks.LinkedList import *
+from structlinks.DoublyLinkedList import *
+from structlinks.Graph import *
+from structlinks.Matrix import *
+from structlinks.Stack import *
+from structlinks.Queue import *
+import structlinks.SortingAlgorithms as sorting_algorithms
+import structlinks.SearchingAlgorithms as searching_algorithms
+
+
