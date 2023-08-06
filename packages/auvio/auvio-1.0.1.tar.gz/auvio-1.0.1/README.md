0fe4e93ec1056ab4ae6be8c4fa41cf75366e245b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:22 +0000

    Add package auvio/1.0.1/auvio-1.0.1.tar.gz

---
+++ packages/auvio/auvio-1.0.1.tar.gz/auvio-1.0.1/README.md
@@ -0,0 +1,25 @@
+# Auvio
+
+Let your zuvio roll the call automatically, don’t get up for roll call anymore, give yourself a good morning.
+
+
+![](https://img.shields.io/pypi/v/auvio.svg)
+
+### install
+`pip3 install auvio`
+
+### How to use
+1. `from auvio import *`
+2. `auvio('YOUR_ZUVIO_EMAIL@PUT.HERE', 'YOUR_ZUVIO_PASSWORD', 'CLASS_NUMBER', Latitude, longitude)`
+
+
+Argument           | Type  
+--------------|-------
+email    | str 
+password    | str
+class number  | str
+Latitude | float
+longitude | float
+
+
+3. logout will happen when user puts Ctrl + c or other KeyboardInterrupt commands
---
+++ packages/auvio/auvio-1.0.1.tar.gz/auvio-1.0.1/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="auvio",
+    version="1.0.1",
+    author="NPC OwO",
+    author_email="support@ntut.club",
+    description="Let your zuvio roll the call automatically, "
+                "don’t get up for roll call anymore, give yourself a good morning!",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Xanonymous-GitHub/auvio",
+    packages=['auvio'],
+    platforms=["all"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.8',
+)
