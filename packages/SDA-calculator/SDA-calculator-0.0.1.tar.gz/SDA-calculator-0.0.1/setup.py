97718cc0b2099951622cf82d28b64bb783cde3e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:44 +0000

    Add package SDA-calculator/0.0.1/SDA-calculator-0.0.1.tar.gz

---
+++ packages/SDA-calculator/SDA-calculator-0.0.1.tar.gz/SDA-calculator-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+name='SDA-calculator',
+version='0.0.1',
+describtion='a short summery about my package ',
+url='',
+author_Email='sadra85aba@gmail.com',
+authorname='sadra ',
+License='MIT',
+keywords='',
+pakages=find_packages(),
+install_requiers=['']
+)
