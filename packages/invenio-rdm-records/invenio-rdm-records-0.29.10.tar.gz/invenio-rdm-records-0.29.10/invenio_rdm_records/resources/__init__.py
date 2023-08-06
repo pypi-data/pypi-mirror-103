598a15564394a0baaf3f46d7ef13bb4a20b7327b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:50 +0000

    Add package invenio-rdm-records/0.29.10/invenio-rdm-records-0.29.10.tar.gz

---
+++ packages/invenio-rdm-records/invenio-rdm-records-0.29.10.tar.gz/invenio-rdm-records-0.29.10/invenio_rdm_records/resources/__init__.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2020 CERN.
+#
+# Invenio-RDM-Records is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+
+"""Invenio RDM module to create REST APIs."""
+
+from .config import PIDResolverResourceConfig, RDMDraftFilesResourceConfig, \
+    RDMManagedPIDProviderResourceConfig, RDMParentRecordLinksResourceConfig, \
+    RDMRecordFilesResourceConfig, RDMRecordResourceConfig
+from .resources import PIDResolverResource, RDMManagedPIDProviderResource, \
+    RDMParentRecordLinksResource, RDMRecordResource
+
+__all__ = (
+    "PIDResolverResource",
+    "PIDResolverResourceConfig",
+    "RDMDraftFilesResourceConfig",
+    "RDMManagedPIDProviderResource",
+    "RDMManagedPIDProviderResourceConfig",
+    "RDMParentRecordLinksResource",
+    "RDMParentRecordLinksResourceConfig",
+    "RDMRecordFilesResourceConfig",
+    "RDMRecordResource",
+    "RDMRecordResourceConfig",
+)
