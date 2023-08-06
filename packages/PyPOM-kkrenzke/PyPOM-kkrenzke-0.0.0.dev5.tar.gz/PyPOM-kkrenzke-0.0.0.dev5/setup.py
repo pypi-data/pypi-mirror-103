57c2d5cadb9d29826fc0f6d6789e36d9d4f806e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:06:17 +0000

    Add package PyPOM-kkrenzke/0.0.0.dev5/PyPOM-kkrenzke-0.0.0.dev5.tar.gz

---
+++ packages/PyPOM-kkrenzke/PyPOM-kkrenzke-0.0.0.dev5.tar.gz/PyPOM-kkrenzke-0.0.0.dev5/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="PyPOM-kkrenzke",
+    version="0.0.0dev5",
+    author="Kyler Krenzke",
+    author_email="kkrenzke@gmail.com",
+    description="A page object model (POM) framework for Python",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/kylerkrenzke/pypom",
+    project_urls={
+        "Bug Tracker": "https://github.com/kylerkrenzke/pypom/issues",
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    py_modules=['pypom'],
+    install_requires=[
+        'selenium',
+    ]
+)
