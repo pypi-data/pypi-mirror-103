a9f1da32e17ab5071550879ffb21198228704639
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:57 +0000

    Add package hbshare/1.4.1/hbshare-1.4.1.tar.gz

---
+++ packages/hbshare/hbshare-1.4.1.tar.gz/hbshare-1.4.1/hbshare/asset_allocation/daily_update_job.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+#coding:utf-8
+from hbshare.asset_allocation.marco import TradingCRNCalculator
+
+
+def update_job():
+    # today = datetime.datetime.today().strftime('%Y%m%d')
+    today = '20210415'
+    # 更新数据
+    TradingCRNCalculator(today, today).get_construct_result()
+
+
+if __name__ == '__main__':
+    update_job()
