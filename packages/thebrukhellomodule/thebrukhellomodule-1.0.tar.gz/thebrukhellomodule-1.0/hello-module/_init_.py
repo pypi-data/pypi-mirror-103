5bb857d4efbe6ce5fdd5c21a23cd29b2151cbd54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:51:53 +0000

    Add package thebrukhellomodule/1.0/thebrukhellomodule-1.0.tar.gz

---
+++ packages/thebrukhellomodule/thebrukhellomodule-1.0.tar.gz/thebrukhellomodule-1.0/hello-module/_init_.py
@@ -0,0 +1,13 @@
+def main():
+    def hello():
+        print()
+        print("|￣￣￣￣￣￣|")
+        print("| Привет!  |")
+        print("| От Егора |")
+        print("|＿＿＿＿＿__|")
+        print("(\__/) ||    ")
+        print("(•ㅅ•) ||    ")
+        print("/ 　 づ      ")
+
+if __name__ == '__main__':
+    main()
---
+++ packages/thebrukhellomodule/thebrukhellomodule-1.0.tar.gz/thebrukhellomodule-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='thebrukhellomodule',
+      version='1.0',
+      description='Hello!',
+      packages=['hello-module'],
+      author_email='thebrukprojects@gmail.com',
+      zip_safe=False)
