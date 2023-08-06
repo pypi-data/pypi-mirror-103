d0b46cda41ca3c8169d7e59427e90bbc1686182b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:14 +0000

    Add package wakdbe/0.4.1/wakdbe-0.4.1.tar.gz

---
+++ packages/wakdbe/wakdbe-0.4.1.tar.gz/wakdbe-0.4.1/wakdbe/bin/linux_dependencies.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+echo "Installing dependencies... Hold on."
+
+UNAME=$(uname)
+confirm=""
+
+if [ "$UNAME" == "Linux" ] ; then
+	echo "Linux detected."
+    packages="adb curl tar openjdk-11-jdk p7zip-full scrcpy"
+    confirm="-y"
+    pkg_man="apt-get"
+else [ "$UNAME" == "Darwin" ]
+	echo "Darwin detected"
+    packages="android-platform-tools curl gnu-tar openjdk@11 p7zip scrcpy"
+    pkg_man="brew"
+fi
+
+echo "Updating system."
+
+$pkg_man update
+
+echo "Installing packages."
+
+for package in $packages
+do
+
+$pkg_man install $confirm $package
+
+done
+
+echo "Done installing packages. Cheers."
+
+# Do not edit this with any other OS than linux. It will edit the line endings and mess with interpreter and won't work.
