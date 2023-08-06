afcb27068f15e6ad81d89a54bfaf24950543ee6a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:55 +0000

    Add package db-mimic/0.0.6/db_mimic-0.0.6.tar.gz

---
+++ packages/db-mimic/db_mimic-0.0.6.tar.gz/db_mimic-0.0.6/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.6"
+DESCRIPTION = "package to emulate simple db functions"
+LONG_DESCRIPTION = "package to emulate simple db functions: create_table, select_from_table, show_table, filter_table"
+
+# Setting up
+setup(
+    name="db_mimic",
+    version=VERSION,
+    author="Alex Kuhn",
+    author_email="<alex.kuhn1123@gmail.com>",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=["python", "db_mimic"],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
