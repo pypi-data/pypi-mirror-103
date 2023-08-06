84429500e0e59df463c7db13e4e95c3620563471
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:38 +0000

    Add package response-differ/0.1.2/response_differ-0.1.2-py3-none-any.whl

---
+++ packages/response-differ/response_differ-0.1.2-py3-none-any.whl/response_differ/cheks.py
@@ -0,0 +1,37 @@
+import copy
+
+from deepdiff import DeepDiff
+from nested_lookup import nested_delete
+
+from response_differ import play, get_grouped_exception
+
+
+def check_status_code(replayed):
+    if not replayed.response.ok:
+        message = f"URL:{replayed.response.url}\n " \
+                  f"STATUS CODE: {replayed.response.status_code}"
+        play.Replayed.errors.append(AssertionError(message))
+
+
+def check_diff_responses():
+    for i in play.Replayed.responses:
+        if i['uri'] in play.Replayed.config:
+            old = i['old']
+            new = i['new']
+            if play.Replayed.config[i['uri']]:
+                old = _filter(old, filters=play.Replayed.config[i['uri']])
+                new = _filter(new, filters=play.Replayed.config[i['uri']])
+            result = DeepDiff(old, new, ignore_order=True)
+            print(result)
+            if result.get('values_changed'):
+                message = f"ERROR {result}"
+                play.Replayed.errors.append(AssertionError(message))
+                exception_cls = get_grouped_exception(*play.Replayed.errors)
+                raise exception_cls(play.Replayed.errors)
+
+
+def _filter(resp, filters):
+    filtered_resp = copy.deepcopy(resp)
+    for f in filters:
+        filtered_resp = _filter(nested_delete(filtered_resp, f), filters=filters[1::])
+    return filtered_resp
