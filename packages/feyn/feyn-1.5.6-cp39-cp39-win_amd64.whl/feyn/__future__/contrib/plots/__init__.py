f8fd0088c65003f68f06bda092d17b9ed2af2260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:24 +0000

    Add package feyn/1.5.6/feyn-1.5.6-cp39-cp39-win_amd64.whl

---
+++ packages/feyn/feyn-1.5.6-cp39-cp39-win_amd64.whl/feyn/__future__/contrib/plots/__init__.py
@@ -0,0 +1,7 @@
+from feyn.__future__.contrib.plots._barcode_plot import BarcodePlot
+from feyn.__future__.contrib.plots._feat_freq_mat import feat_freq_heatmap, feat_freq_matrix
+
+__all__ = ['BarcodePlot',
+           'feat_freq_heatmap',
+           'feat_freq_matrix'
+           ]
