498d502488157d02c7c9b920fc1f4d8af2034441
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:35:43 +0000

    Add package wxc/6.0.1/wxc-6.0.1.tar.gz

---
+++ packages/wxc/wxc-6.0.1.tar.gz/wxc-6.0.1/wxc/cli.py
@@ -0,0 +1,51 @@
+import sys
+from argparse import ArgumentParser
+from typing import List, Optional
+
+from wxc.api import get_full_data
+
+
+def main(argv: Optional[List[str]] = None) -> int:
+    parser = ArgumentParser()
+    parser.add_argument(
+        "name",
+        help="target Python scope (package.module.submodule.class.method)",
+    )
+    command_group = parser.add_mutually_exclusive_group()
+    command_group.add_argument(
+        "-v", "--version", action="store_true", help="print module version"
+    )
+    command_group.add_argument(
+        "-f",
+        "--full",
+        action="store_true",
+        help="print a full report",
+    )
+    args = parser.parse_args(argv)
+
+    data = get_full_data(args.name)
+
+    if not any(list(data.values())):
+        print(
+            f"Error: did not resolve any data for '{args.name}'",
+            file=sys.stderr,
+        )
+        return 1
+
+    if args.full:
+        data["name"] = args.name
+        print("\n".join([f"{k} = {v}" for k, v in data.items()]))
+        return 0
+
+    if args.version:
+        if data["version"] is None:
+            print(
+                f"Error: did not find version metadata for '{args.name}'",
+                file=sys.stderr,
+            )
+            return 1
+        print(data["version"])
+        return 0
+
+    print(data["source"])
+    return 0
