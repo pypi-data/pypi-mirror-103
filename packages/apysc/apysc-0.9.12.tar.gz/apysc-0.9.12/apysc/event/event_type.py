f31eab3d60310418a4d8a53864279935e4d0d967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:09 +0000

    Add package apysc/0.9.12/apysc-0.9.12.tar.gz

---
+++ packages/apysc/apysc-0.9.12.tar.gz/apysc-0.9.12/apysc/event/event_type.py
@@ -0,0 +1,13 @@
+"""Definitions of event type.
+"""
+
+from enum import Enum
+
+
+class EventType(Enum):
+
+    CLICK = 'click'
+    DBLCLICK = 'dblclick'
+    MOUSEDOWN = 'mousedown'
+    MOUSEUP = 'mouseup'
+    MOUSEOVER = 'mouseover'
