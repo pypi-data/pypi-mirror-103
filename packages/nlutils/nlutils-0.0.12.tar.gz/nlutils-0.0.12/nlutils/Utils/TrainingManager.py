0d4216fe49f6e08a5b87be3851164af738caab6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:24 +0000

    Add package nlutils/0.0.12/nlutils-0.0.12.tar.gz

---
+++ packages/nlutils/nlutils-0.0.12.tar.gz/nlutils-0.0.12/nlutils/Utils/TrainingManager.py
@@ -0,0 +1,27 @@
+import os
+
+from multiprocessing import Process, Queue, Lock, Pool
+from .Log import Logger
+
+class TrainingManager(object):
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def shell_training(cmd_str):
+        Logger.get_logger().info(f"Process {os.getpid()} is running")
+        os.system(cmd_str)
+
+    def start_shell_training(self, cmd_strs):
+        self.training_pool = Pool(len(cmd_strs))
+        self.training_pool.map(TrainingManager.shell_training, cmd_strs)
+        self.training_pool.close()
+        self.training_pool.join()
+    
+    def start_api_training(self, entry, arg_list):
+        self.training_pool = Pool(arg_list)
+        self.training_pool.map(entry, arg_list)
+        self.training_pool.close()
+        self.training_pool.join()
+
