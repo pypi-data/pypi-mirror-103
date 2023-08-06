1e39aba0c04f504632a53b3a6ea8ec7ced2006a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:55 +0000

    Add package jsoneditor/0.7.0/jsoneditor-0.7.0.tar.gz

---
+++ packages/jsoneditor/jsoneditor-0.7.0.tar.gz/jsoneditor-0.7.0/README.md
@@ -0,0 +1,23 @@
+# Installation
+
+```bash
+pip3 install jsoneditor
+```
+
+# Usage
+
+In python:
+```python
+import requests
+import jsoneditor
+
+data = requests.get('your endpiont here').json()
+jsoneditor.editjson(data)
+```
+From the terminal:
+
+You got a cuple of options.
+
+1. `python -m jsoneditor '{"Hey": "Hi"}'`
+2. `curl https://jsonplaceholder.typicode.com/comments | jsoneditor`
+3. `jsoneditor '{"Hey": "Hi"}'`
---
+++ packages/jsoneditor/jsoneditor-0.7.0.tar.gz/jsoneditor-0.7.0/jsoneditor/jsoneditor.py
@@ -0,0 +1,58 @@
+from typing import Union
+from flask import Flask, request, render_template
+import json
+from multiprocessing import Process, set_start_method
+import time
+import webbrowser
+import random
+import os
+import sys
+import platform
+
+
+
+if platform.system() == 'Darwin':
+    set_start_method("fork")
+
+# get installation dir
+install_dir = os.path.dirname(os.path.realpath(__file__))
+
+# disable flask logs
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+os.environ['WERKZEUG_RUN_MAIN'] = 'true'
+
+
+def editjson(data: Union[dict, str], finnish_callback: callable = None, options: dict = None):
+    if type(data) is str:
+        data = json.loads(data)
+
+    app = Flask('jsoneditor', template_folder= install_dir + '/templates')
+    port = random.randint(1023, 65353)
+
+    @app.route('/')
+    def jsoneditor_route():
+        return render_template('index.html', data=data, send_back_json= bool(finnish_callback), options= options)
+
+    if finnish_callback:
+        @app.route('/post', methods=['POST'])
+        def callback_route():
+            data = request.json['data']
+            finnish_callback(data)
+            return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+    
+    server = Process(target=app.run, args=('localhost', port))
+    server.start()
+    webbrowser.open(f'http://localhost:{port}/')
+
+def main():
+    if not os.isatty(0):
+        data = ''.join(x for x in sys.stdin)
+    else:
+        if len(sys.argv) == 2:
+            data = sys.argv[1]
+        else:
+            raise Exception('Got invalid number of arguments')
+
+    editjson(data)
