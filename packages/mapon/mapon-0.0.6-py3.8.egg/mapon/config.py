1b850c1b301eb6222ffbed7e725ee61d4b9594d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:26 +0000

    Add package mapon/0.0.6/mapon-0.0.6-py3.8.egg

---
+++ packages/mapon/mapon-0.0.6-py3.8.egg/mapon/config.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from importlib import resources
+
+import psutil
+
+_locale = 'English_United States.1252' if os.name == 'nt' else 'en_US.UTF-8'
+
+_CONFIG = {
+    'ws': '',
+    'max_workers': psutil.cpu_count(logical=False),
+    'locale': _locale,
+    'msg': True,
+    'refresh': None,
+    'export': None,
+}
+
+_filename, _ = os.path.splitext(os.path.basename(sys.argv[0]))
+_DBNAME = _filename + '.db'
+_GRAPH_NAME = _filename + '.gv'
+_JOBS = {}
+# folder name (in workspace) for temporary databases for parallel work
+_TEMP = "_temp"
+_CONN = [None]
+
+# sqlite3 keywords
+with resources.open_text('mapon', 'sqlite_keywords.txt') as f:
+    _RESERVED_KEYWORDS = set(x.strip() for x in f.readlines())
+
