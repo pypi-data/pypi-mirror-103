fedc0b06559e1ef47879d73d20c2d2c243c0665f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:15:21 +0000

    Add package elaina/0.10.17/elaina-0.10.17-py3-none-any.whl

---
+++ packages/elaina/elaina-0.10.17-py3-none-any.whl/elaina/__init__.py
@@ -0,0 +1,91 @@
+_str='''
+
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,ttf:::;;::.............:::::....::::::::::::
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;jD:::ii::.............:::::..::::::::::::::
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,ijG.::ii:::..........:.:::::::::.:::::::::::
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,ijG..:ii:,:::.......::.:::::::::::::::::::::
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,tj;:..;i,,::::..:....:::::::::::::::::.:::::
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,tf,:..;i;:::::::GEE#.:.:::::::::::::::::::::
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,DjD,:..,i;::,::.EDDKEEEEE::::,:::::::::::::::
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;jG,:..:i;::,:.DDDEEEEEEWE,,,,,::::EEKKKEEi::
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,;jG,:...;;:j  ;:;j;;iEEE#KE,;,,,,WKEEEEEEEKKt
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,ijL,:..:;iif#EEEEEEjG;fDKKWEEDEWWEEEEEEEEEEEK
+,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,:,GK#fi  .;;GLEEEDEEEKKW#GLGWKKKKKELEEEEEEEEEEE
+,,,,,,,,,,,,,,,,,,,,,,,:::#LDDDWLLGGGGGGGGLLDWDEED#KKKKKKKWWDGDKEEKEDEEEEEEEEEEE
+,:,,,,,,,:::,:,::::,::#GDGfLLLLLLLGGGGGGGGGGGGGLLLLDEEK;DWKKWtGLWEEEEEEEEEKKKKKE
+:::::::::::::::::::WGGfttttttjjjjjjjjjGGGGLLLLLLLLLLLLLfEE#GGGEjfWEEEDDKEEEKKKKK
+:::::::::;:::::::fGLtttttttjjjjjjjjjjjjjjGGLLLLLLLLLLLLLLLLGEDGDDEDKDtELKEEKKKEK
+:::::::::L:::::jGttttttttttttjjjjjjjjjjjjjjLGLLLLLLLLLLLLLLLLLL#GKDGGtDLEKKKKKEE
+:::::::::L:::jGiiiitttttjjjfffffffjjjjjjjjjjLGGLLLLLLLLLLLLLLLLLLLW#EEEKKKKKKKKK
+:::::::::j::GEtiiiittjjjjjjjjffffffjjjjjjjjfGGGGGfEj,;;,j;;jft#LLLLGLEWKKKKKKKEK
+:::::::::f:LLGtttttjjtjjjjjjjjfffffjjjjjjLLLLLi,;;;f;i,;,fi;;tiittfLLGGLKKKKKKEE
+:::::::::f:LLGDttttttjjjjjjjjjjjjjjjjjLLffffij,,;t;;.,;:i;Lii;;;ittjfLKGGGGKKEKK
+::::::::ff:::tGDtttttttjjjjjjjjjjjjjLLffffLj,G:,f;,i;.;;f:,iii,;;DttiitLGGGGGGGG
+::::::::jfi::::: GLjittjjjjjjjjtjtjfffffft,.,j.;,,i;;:;;:::i::i,;;ttiijitGGGGGGD
+:...:.::jfD::...::..:..:. .ttGEjttffffff,G..:K,.:.;:;::j:,;::::;;j;iiiiiGiLLGGGL
+:::.:::.jfD:....:ffjtijttGftjtttttjfffff.:..:;,L.:,::;:E;LKWt::;:i;it,iijLLLGGGG
+........jfG:.......ti;;;;;;;ittttttjfff.:.,..,: t,.:,,f.EKKj,::L:;LjtiiiiLLLLLLL
+...L....jfG:...........Li;;;;;;itttjjjj ..:.:;:: ,::.i,,.  .j,:;:;G;iitiiiLLLLLL
+...f...fjfG...:.............fi;;;;iiit j:..L:fE,Gj...:i:::,:.,,;:D,;jiji;iiLLLLL
+...f::D.DDiLL::G::::::::::::..::WtD.jLLff.:;..;.tL:..:::::::.,j:;,j;jjji;;iffLiL
+..:f:::t;jjj:::f:::::::::::.::::;tG:...i:.:i:,.::::..:::::::,,:,Efjjj,:;;;iifL;f
+:::f:.::fLL:::Lf::::::::::::::::;j.::.:::,,i;GD.:::.:.i,::::,::tWDif,::;;iifff;f
+:::f;::.jj::::;t::::::::::::::::tj.::.::t::ii:t::::.;,,::::::;;LffLi::..;;tffj;j
+ .tfG:::ti,::::;::::::::::::::..tD.::.,::fLii;jjE:.:::::::,;i;Djttfiii.,L;fj.tfj
+.:ffG,;;ijfLf:it:::::::.::::.:,,jG::,..:;:,iiiitiff.:::::iiE,,iELiiii;,:;Kfjjjjj
+,,jfG;fLffLfff,t::::::::,.,.E.jfGGGK:,,.::,;;;iitjKfjiiiii:t,;i;iii;;iii;ffjjjjj
+..LfL::i;ijL:::j:::::...;.;,EEifLGLi;;i.::,i;;itGffj:itE;;;i,;iiLiiii;iWEEfjjjjj
+.:..f:ijL.LLfj;,::::::::;:;,DEK:GDE:,:,..;,;iitt;jL;,,;;:,;,E,;fE#WW##EKKKfjjjtt
+ii..L,ifLLLGLtt::::::::,;;;DDDEK#L:::,,...;j,tjf;D;.,;i;:..;,,:tWWWEEEEEEKKjjjtt
+;i:.G,,ii;GiLtt,,:,:t,,:ffDGDKEEfG;;;;,.; ;LijfDii,L,i:;:i;,KEW,;iiEWWWWKKfjjjjt
+ii:,Gi,;;,ifjfL ...,....LfLLDEEEKt;Liiij:t:t;jEEKKtiGi::L;tEEKWD::i,W;WWWfLjjjjt
+GffffDt;,,if..........f.fGGDKKKEKfELti. f;:tGjEDKGtiL;,:itWEEEEEt:;;iKKWWWLfjjjj
+LfffGGDfLGG.........::::fLDEEEEEKfDji;;:,:i;#fKEKEfE;;GjiEDDDGDE;::K::WKKWLfjjjj
+GffLGLGLG,..j.....::::::::KEEEEEEtGLti;,f;:EKEDEKjjtjKDf;D:tjL;K;:;K:fKKWWLfjjjj
+GfLLGGG.;::.......:::::::::W,EEEKDGDG:f:::tEEDEWjttLtWGiD:,tGjii;;WWfGWWWWffffjj
+GGLLLLL.j:::...:::::::,::::::.:E,LtW:tj:.;EDGDEGiii;jtfLWtf.i;;;tt;WWKKKKWffffjj
+LLLLLLLLj:::..::::::::i:::::::jiLLLi,ij,DE;,DE;fDWjitttE;:DWfDDEKKKEEKKKKWfffjjj
+LLGGGGDEG,,::.:::::::::,:::::::LEEEEjfGGKEDKEf;ttijL:jt:EEKEEEE#KKKKKKKKKWfffjfj
+LGGGGDDDL::,::.::::::::::::::::::EEEEGWEEEEEGfiitij::D;KWE:DEEKKKKEKKKEKKWffffjj
+LDEGGGLGGG:::::..::::::::::::::::,KEEKKEKKKKffD,ii:;,:tKKKKKKKKWKKKKKEEEWWffffff
+LLDEKGGGGGE.:::..::::::::::::::::::jEKKWWK#Lfff;ti;,:jLjKKKKKKKKKKKEEEEEWWffffjf
+LLGEEEGGGGEi.:::..:::::::::.::::::::,EKKWKWLjffitG;;:,EtEKKKKKKKKKEEEEEWKWfffjff
+LLfEEEKGGGEE.::::..:::::::::;:::::::::#WWWWjjjf;i;;;,,ttGKKKKKKKKEEEEKKKEWffffff
+LLtfEEEGGDEEG:::::.:::::::::::::::::::::KWWWjjff,::;;;tj;EKKKKKKKEEKKKKKEEKfffff
+f;jjfDDDGDEED.::::..:::::::::,:::::::::: iKWtjfG;D,;Gif,;EKKKKKKEEEKKKKEEWWjffff
+G;ttEEDDGGDDGD:::::.:::::::::i,:::::::::::::Wjf;;::,,DG,;EEKKK#EEEEEEKKEEKfjffff
+GDfLEEEDGGDGGG :::::.:::::::::,:::::::::::,:,KKij,:,::;:DEEEKKKKKEEEEEEEEEEEKfff
+GDDDEEEGGGDGGLG.::::G.:::::::,;,:::::::::::,,:KD,;::,:::;KEKKKKKEEEEEEEEEEEEEEEK
+GDDGEGLEfEGLfff;:::::.::::::::,;:::::::::::,,;EE,;::,G;;;WKKKKKEEEEEEEEEEEEEEEEE
+;;;G;jfLLGLfffft.:::::.:::::::,;;:::::::::::,:EDK,,:;:,;,;KKKKKEEEEEEEEEEEEEEEEE
+;;;itiEDDGLLLf;if.::::,.::::::,;;,::::::::::,:;KK#j,,jijitEEKK#EEEEEEEEEKKEEEEEE
+DDDEDDDGGLLLf,;jff::::,,::::::,,;;,::::::::::;LKKE#;LEitjtLWEKEEEEEEEEKKKEEEEEEE
+DGDDDDDGLLLffffEGLD:::,,,:::::,;;;;,::::::::;;KKKKKDDLLi,,;LEKEEEEEEKKKEEEEEEWGD
+DGDDDDGGLffffffffLGD::,,,:::::,;;;;;,,,,,::,i;KEKKKKLGLi;ijLGWEEEEKKKKKEEEKDEEDL
+DDDDDDGLfffffjtjjjLt.,,,,,::::,;;G,;;;,,,,,,;;iEEKKKKLLj;;DLGEEEEKKKKEEEEGEKELff
+DDDDDGGfjjjjjtittjGtf.,,,,L,,,,;;DL;;;;;,,,,;;;EEKKKKKL;GtGtGKEEEKKKKKK;tLELLLff
+jjfLLLftiitj.::,tLGGff,,,,GE,,,;;;LLi;;;;;;;it;EEEEKKKj;G;EjGEEEEEEEE,;,;ffGELLL
+tttjffj:::::::::,#Kfj.j;jjLL:,,;;LLGi;;;;;;;;;GEEEEKKi;GG;DELffDDWGj,i;ifEDfGGEE
+iittjj;;;.::,,,,,,,,,,tDD,GLLD,;;jGGt;;;;;;,;;;EEEKKK;GGGGGLLLtLj;;;LjjjjDtEE#G,
+iiiiiG,;;;;t:::,,,,,,,;tWD;jjWEfjjjLf;;;;;;;;tLEEEEEi;GGGGLLLGjtj,t;;jGiGjWLGG,;
+iiiiii,,,;;;;i ::,,,,,,.iEKfE,Ki,jLLK;;;;;;;;;;;#EEWfDG;GfLLLL,ijiti;;ftKWG;GG;;
+i;;;;,,,,,,,;;;ii:::,,;,jDDKKKWK;DGLKK;;;;;;;;;;iiEtiDDLDffGGjGjGLLiitKKGGGLGG,;
+;,,,,,;t,,,,,;;iiiit::,DDDEEK#KGK;LKKKK;;;;;;;Li;;tKijDDifffGtGGttGi#DEGGGG,GG;;
+,,,,,,,;;G,,,,,;;;iiiiGDDDEEEKEiWEfKEKEK;;;;;;;iG;tKKKtiffffiDGjiiiLGGGGGGG;GG;,
+,,,,,,,,,jLL,,,,,Gf;iiEDKEEEEKWKDEEEEKEK:;;;;;i;iEKKKKKEEELiDDEi;DDGEDGGGGGGGGGj
+:::,,,,,,;;ifL;,,iLiiGDDDEDWELK#EEEEKEEEGG:;;;L;EKKKGKKKKtDDDLEGLLfEGDDDDGGGtDDj
+:,,,,,,,,;;ijELLiiiDEEDDEEEK#WKEEEEEKEELDDDDD;,EKEK,WGE#fDDfLGGGGGGGGfLGGDDDjDDj
+,,,,,,;;,;iitGiifttLEEDEjEEEEEEKEEEEKGLLDDDDDt;;ttjij;tDDiGGGGGGGGGGGGfjEGDDiDDt
+,,,,,,;;;;;;jLijjjfffEEjjKLDKEEEEEKKLGLDDDffjG;,E,:DEttDtGDGGLLLLLGLGELfjjGtDGDj
+,,,;;;;;;;,,LDjjjfjfffjjjKKKKEEEEEKKELDffffffffjffffGtDGGDDELLLLLLLLDDDDDjjGDGij
+,,;;;;;,:,,,,LGDfjjfEGKLjEEEEEEEEKGfDDLfLjffffffLfffttGDEiiELLLLLLDDDDDDDGDfEtiW
+,::,,,,:::,,,;;DKKjjELjEffEDEEEELLDDDDfffffLjfffffffiEDDGGGGGGLLKDDDDDDLfjjjfDLL
+
+
+この魔女の証であるブローチを付け、灰色の髪をなびかせて、その美しさと才能の輝きに、太陽さえも思わず目を細めてしまうほどの美女は、誰でしょうか？
+
+そう、私です
+
+
+'''
+print(_str)
