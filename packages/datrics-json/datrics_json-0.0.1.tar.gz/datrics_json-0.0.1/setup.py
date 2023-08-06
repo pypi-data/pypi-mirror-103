d842a5ecbef16bc446598af4bd1ee433d7f9c1fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:06:27 +0000

    Add package datrics-json/0.0.1/datrics_json-0.0.1.tar.gz

---
+++ packages/datrics-json/datrics_json-0.0.1.tar.gz/datrics_json-0.0.1/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+from os import path
+import pathlib
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+# The text of the README file
+long_description = (HERE / "README.md").read_text()
+
+# automatically captured required modules for install_requires in requirements.txt and as well as configure dependency links
+with open(path.join(HERE, 'requirements.txt'), encoding='utf-8') as f:
+    all_reqs = f.read().split('\n')
+install_requires = [x.strip() for x in all_reqs if ('git+' not in x) and (
+    not x.startswith('#')) and (not x.startswith('-'))]
+dependency_links = [x.strip().replace('git+', '') for x in all_reqs \
+                    if 'git+' not in x]
+
+setup(name='datrics_json',
+      version='0.0.1',
+      description='Open source library for the Datrics models deserialization',
+      packages = find_packages(),
+      install_requires = install_requires,
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      license='MIT',
+      url='https://github.com/datrics-ai/datrics-json',
+      dependency_links=dependency_links,
+      author_email='th@datrics.ai',
+      classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: MIT License",
+            "Operating System :: OS Independent",
+      ],
+      python_requires='>=3.5')
