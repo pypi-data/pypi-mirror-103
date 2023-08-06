efe9c30565f70047c2152f3b4f5ec1d4df578f45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:10 +0000

    Add package tricli/2.1.2/tricli-2.1.2.tar.gz

---
+++ packages/tricli/tricli-2.1.2.tar.gz/tricli-2.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+import sys
+
+version_name = sys.argv[1].replace("refs/tags/", "")
+del sys.argv[1]
+
+setup(
+    name = 'tricli',
+    version=version_name, 
+    author = "Anish Lakkapragada", 
+    author_email="anish.lakkapragada@gmail.com",
+    description = "tricli manages a trie data structure that can be globally accessed.", 
+    keywords=["REST", "Data Structures", "Trie"], 
+    packages = ['tricli'],
+    license = "MIT", 
+    entry_points = {
+        'console_scripts': [
+            'tricli = tricli.__main__:main'
+        ]
+    })
