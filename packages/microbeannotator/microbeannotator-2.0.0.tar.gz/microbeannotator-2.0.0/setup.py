e9959cf4fe359ccd7e06ebb004f83511a94c570b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:48 +0000

    Add package microbeannotator/2.0.0/microbeannotator-2.0.0.tar.gz

---
+++ packages/microbeannotator/microbeannotator-2.0.0.tar.gz/microbeannotator-2.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+from microbeannotator import version
+setup(
+    name='microbeannotator',
+    version=version,
+    description='A user friendly microbe genome annotation tool',
+    url='https://github.com/cruizperez/MicrobeAnnotator',
+    author='Carlos A. Ruiz Perez',
+    author_email='cruizperez3@gatech.edu',
+    keywords = ['genome annotation', 'protein', 'comparative genomics'],
+    packages=find_packages(),
+    include_package_data=True,
+    use_scm_version=False,
+    setup_requires=[
+        'setuptools_scm'],
+    python_requires='>=3.6, <=3.7.5',
+    scripts=['bin/microbeannotator',
+             'bin/microbeannotator_db_builder'] 
+)
