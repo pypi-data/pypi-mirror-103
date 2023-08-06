4a81eea94f1c8bcd320cd3680759f2f24382953a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:16 +0000

    Add package db-fg/0.0.1/db_fg-0.0.1.tar.gz

---
+++ packages/db-fg/db_fg-0.0.1.tar.gz/db_fg-0.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+    name="db_fg",
+    version="0.0.1",
+    author="lovic",
+    author_email="",
+    description="database connection",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
