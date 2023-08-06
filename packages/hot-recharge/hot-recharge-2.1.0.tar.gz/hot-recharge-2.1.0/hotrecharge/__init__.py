452b2a2bad5254e031ec6d2c797ce7bc8db5162e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:35:55 +0000

    Add package hot-recharge/2.1.0/hot-recharge-2.1.0.tar.gz

---
+++ packages/hot-recharge/hot-recharge-2.1.0.tar.gz/hot-recharge-2.1.0/hotrecharge/__init__.py
@@ -0,0 +1,7 @@
+#  __init__.py
+from hotrecharge.HotRecharge import HotRecharge
+from hotrecharge.HRConfig import HRAuthConfig
+
+__author__  = "Donald Chinhuru"
+__version__ = "2.1.0"
+__name__    = "hot-recharge"
