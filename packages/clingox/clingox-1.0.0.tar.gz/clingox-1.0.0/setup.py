fdd31956e1842d25e3b1bfc72e7234fda738d6e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:49 +0000

    Add package clingox/1.0.0/clingox-1.0.0.tar.gz

---
+++ packages/clingox/clingox-1.0.0.tar.gz/clingox-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from textwrap import dedent
+from setuptools import setup
+
+setup(
+    version = '1.0.0',
+    name='clingox',
+    description='Support library for clingo.',
+    long_description=dedent('''\
+        Preliminary documentation can be found here:
+
+        - https://www.cs.uni-potsdam.de/~kaminski/pyclingo-cffi/
+        '''),
+    long_description_content_type='text/markdown',
+    author='Roland Kaminski',
+    author_email='kaminski@cs.uni-potsdam.de',
+    license='MIT',
+    url='https://github.com/potassco/python-clingox',
+    install_requires=['clingo'],
+    packages=['clingox'],
+    package_data={'clingox': ['py.typed']},
+    zip_safe=False,
+    python_requires=">=3.6"
+)
