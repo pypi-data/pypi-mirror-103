4951c4d49d8961c99e342befc1cddc07ccfb4927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:10:33 +0000

    Add package apkg/0.0.2/apkg-0.0.2-py3-none-any.whl

---
+++ packages/apkg/apkg-0.0.2-py3-none-any.whl/apkg/commands/build_dep.py
@@ -0,0 +1,43 @@
+"""
+install build dependencies
+
+Usage: apkg build-dep [-u] [-s | -a] [<file> | -F <file-list>]...
+                      [-d <distro>] [-y]
+
+Arguments:
+  <file>                  specify input <file>s (when using -s or -a)
+  -F, --file-list <fl>    specify text file listing one input file per line
+                          use '-' to read from stdin
+
+Options:
+  -u, --upstream          use upstream archive / apkg get-archive
+                          default: dev archive / apkg make-archive
+  -s, --srcpkg            use source package <file>s
+  -a, --archive           use archive <files>s
+                          default: use dev archvie / apkg make-source
+  -d, --distro <distro>   set target distro
+                          default: current distro
+  -y, --yes               non-interactive mode
+                          default: interactive (distro tool defualts)
+""" # noqa
+
+from docopt import docopt
+
+from apkg.lib import build
+
+
+def run_command(cargs):
+    args = docopt(__doc__, argv=cargs)
+    # XXX: this will be refactored in following patch, disabling for now
+    # https://gitlab.nic.cz/packaging/apkg/-/issues/34
+    # pylint: disable=unreachable
+    raise NotImplementedError
+    return build.install_build_deps(
+        srcpkg=args['--srcpkg'],
+        archive=args['--archive'],
+        upstream=args['--upstream'],
+        version=args['--version'],
+        release=args['--release'],
+        distro=args['--distro'],
+        interactive=not args['--yes'],
+        )
---
+++ packages/apkg/apkg-0.0.2-py3-none-any.whl/docs/__init__.py
@@ -0,0 +1,58 @@
+"""
+generate docs from apkg code/docstrings using mkdocs-macros-plugin
+"""
+import inspect
+
+from apkg import ex
+from apkg import pkgstyle
+from apkg.cli import cmd2mod
+from pathlib import Path
+
+
+BASE_PATH = Path(__file__).parent.parent
+BASE_CODE_URL = "https://gitlab.nic.cz/packaging/apkg/-/blob/master/"
+APKG_NEW_ISSUE_URL = "https://gitlab.nic.cz/packaging/apkg/-/issues/new"
+
+
+def define_env(env):
+    """
+    this is available in docs using jinja2 templates
+    """
+    env.variables.pkgstyles = pkgstyle.PKGSTYLES
+    env.variables.new_issue_url = APKG_NEW_ISSUE_URL
+    env.variables.exceptions = get_exceptions()
+
+    @env.filter
+    def relpath(path):
+        return Path(path).relative_to(BASE_PATH)
+
+    @env.filter
+    def file_link(path):
+        return "[{fn}]({url}{fn})".format(
+            fn=path,
+            url=BASE_CODE_URL)
+
+    @env.filter
+    def file_text(path):
+        text = Path(path).open('r').read().strip()
+        return "``` text\n%s\n```" % text
+
+    @env.filter
+    def mod_doc(modname):
+        mod = __import__(modname, fromlist=[''])
+        return mod.__doc__.strip()
+
+    @env.filter
+    def cmd_help(cmd):
+        modname = 'apkg.commands.%s' % cmd2mod(cmd)
+        return "``` text\n$> apkg %s --help\n\n%s\n```" % (
+            cmd, mod_doc(modname))
+
+
+def get_exceptions():
+    """
+    return all apkg exceptions sorted by exit_code
+    """
+    exs = [e for _, e in inspect.getmembers(ex, inspect.isclass)]
+    exs.sort(key=lambda x: x.exit_code)
+    return exs
