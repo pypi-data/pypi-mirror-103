d452422825ac6c2a68315d7bbfbc99f6ad8e3948
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:18 +0000

    Add package baguette/0.1.3/baguette-0.1.3.tar.gz

---
+++ packages/baguette/baguette-0.1.3.tar.gz/baguette-0.1.3/baguette/types.py
@@ -0,0 +1,30 @@
+import typing
+
+from .headers import Headers as Headers_
+from .request import Request
+from .responses import Response
+
+Scope = typing.MutableMapping[str, typing.Any]
+Message = typing.MutableMapping[str, typing.Any]
+
+Receive = typing.Callable[[], typing.Awaitable[Message]]
+Send = typing.Callable[[Message], typing.Awaitable[None]]
+
+ASGIApp = typing.Callable[[Scope, Receive, Send], typing.Awaitable[None]]
+
+Headers = typing.Union[
+    Headers_,
+    typing.Mapping[str, str],
+    typing.Sequence[typing.Tuple[str, str]],
+]
+
+Result = typing.Union[
+    Response,
+    typing.Tuple[
+        typing.Any,
+        typing.Optional[int],
+        typing.Optional[Headers],
+    ],
+]
+
+Handler = typing.Callable[[Request], typing.Awaitable[Result]]
