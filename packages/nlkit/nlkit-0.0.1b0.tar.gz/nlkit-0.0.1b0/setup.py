99120cfd489f06b30f70acd232a39fad5b9b4166
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:17 +0000

    Add package nlkit/0.0.1b0/nlkit-0.0.1b0.tar.gz

---
+++ packages/nlkit/nlkit-0.0.1b0.tar.gz/nlkit-0.0.1b0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("./README.md") as frd:
+    long_description = frd.read()
+
+setup(
+    name='nlkit',
+    version='0.0.1b0',
+    description='easy to use nlp tools',
+    long_description=long_description,
+    license='MIT',
+    url='https://shihanmax.github.io',
+    author='shihanmax',
+    author_email='shihanmax@foxmail.com',
+    install_requires=[
+        "numpy>=1.17.0",
+        "scipy>=1.5.0",
+        "matplotlib>=3.0.0",
+        "torch>=1.6.0",
+        "gensim>=3.7.0",
+    ],
+    packages=find_packages()
+)
