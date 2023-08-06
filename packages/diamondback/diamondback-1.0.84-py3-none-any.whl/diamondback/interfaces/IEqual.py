a1662ddc76d9227e5e7d3b380a2c93daa8dcecbd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:15:08 +0000

    Add package diamondback/1.0.84/diamondback-1.0.84-py3-none-any.whl

---
+++ packages/diamondback/diamondback-1.0.84-py3-none-any.whl/diamondback/interfaces/IEqual.py
@@ -0,0 +1,72 @@
+""" **Description**
+
+        Equal interface.
+
+    **Example**
+
+        ::
+
+            from diamondback import IEqual, IPhase
+
+
+            class Test( IEqual, IPhase ) :
+
+                def __eq__( self, other : any ) -> bool :
+
+                    return ( ( super( ).__eq__( other ) ) and ( numpy.isclose( self.phase, other.phase ) ) )
+
+    **License**
+
+        `BSD-3C. <https://github.com/larryturner/diamondback/blob/master/license>`_
+
+        © 2018 - 2021 Larry Turner, Schneider Electric Industries SAS. All rights reserved.
+
+    **Author**
+
+        Larry Turner, Schneider Electric, Analytics & AI, 2018-01-23.
+
+    **Definition**
+
+"""
+
+class IEqual( object ) :
+
+    """ Equal interface.
+    """
+
+    def __eq__( self, other : any ) -> bool :
+
+        """ Equal.
+
+            Arguments :
+
+                other : any.
+
+            Returns :
+
+                equal : bool.
+        """
+
+        return ( ( isinstance( other, self.__class__ ) ) and ( ( id( self ) == id( other ) ) or ( super( ).__eq__( other ) ) ) )
+
+    def __init__( self ) -> None :
+
+        """ Initialize.
+        """
+
+        pass
+
+    def __ne__( self, other : any ) -> bool :
+
+        """ Not equal.
+
+            Arguments :
+
+                other : any.
+
+            Returns :
+
+                notequal : bool.
+        """
+
+        return not ( self == other )
