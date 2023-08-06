44373018e821d47595a9730607de0c4a76843365
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:17 +0000

    Add package dexp/2021.4.22.1109/dexp-2021.4.22.1109-py2.py3-none-any.whl

---
+++ packages/dexp/dexp-2021.4.22.1109-py2.py3-none-any.whl/dexp/datasets/clearcontrol_dataset.py
@@ -0,0 +1,220 @@
+import re
+from fnmatch import fnmatch
+from os import listdir, cpu_count
+from os.path import join
+from typing import Sequence, Tuple, Any
+
+import dask
+import numcodecs
+import numpy
+from arbol.arbol import aprint
+from cachey import Cache
+from dask import array, delayed
+from numpy import uint16, frombuffer
+
+from dexp.datasets.base_dataset import BaseDataset
+
+numcodecs.blosc.use_threads = True
+numcodecs.blosc.set_nthreads(cpu_count() // 2)
+
+
+class CCDataset(BaseDataset):
+
+    def __init__(self, path, cache_size=8e9):
+
+        super().__init__(dask_backed=False)
+
+        self.folder = path
+        self._channels = []
+        self._index_files = {}
+
+        all_files = list(listdir(path))
+        # print(all_files)
+
+        for file in all_files:
+            if fnmatch(file, '*.index.txt'):
+                if not file.startswith('._'):
+                    channel = file.replace('.index.txt', '')
+                    self._channels.append(channel)
+                    self._index_files[channel] = join(path, file)
+
+        # print(self._channels)
+        # print(self._index_files)
+
+        self._nb_time_points = {}
+        self._times_sec = {}
+        self._shapes = {}
+        self._time_points = {}
+
+        for channel in self._channels:
+            self._parse_channel(channel)
+
+        self.cache = Cache(cache_size)  # Leverage two gigabytes of memory
+
+    def _parse_channel(self, channel):
+
+        index_file = self._index_files[channel]
+
+        with open(index_file) as f:
+            lines = f.readlines()
+
+        lines = [line.strip() for line in lines]
+
+        lines = [re.split(r'\t+', line) for line in lines]
+
+        self._time_points[channel] = []
+
+        for line in lines:
+            time_point = int(line[0])
+            time_sec = float(line[1])
+            shape = eval('(' + line[2] + ')')[::-1]
+
+            self._times_sec[(channel, time_point)] = time_sec
+            self._shapes[(channel, time_point)] = shape
+
+            self._time_points[channel].append(time_point)
+
+        self._nb_time_points[channel] = len(self._time_points[channel])
+
+    def _get_stack_file_name(self, channel, time_point):
+
+        return join(self.folder, 'stacks', channel, str(time_point).zfill(6) + '.raw')
+
+    def _get_array_for_stack_file(self, file_name, shape=None):
+
+        try:
+            with open(file_name, 'rb') as my_file:
+                aprint(f"Accessing file: {file_name}")
+                buffer = my_file.read()
+
+                dt = numpy.dtype(uint16)
+                dt = dt.newbyteorder('L')
+
+                array = frombuffer(buffer, dtype=dt)
+
+            if not shape is None:
+                array = array.reshape(shape)
+
+            return array
+
+        except FileNotFoundError:
+            aprint(f"Could not find file: {file_name} for array of shape: {shape}")
+            return numpy.zeros(shape, dtype=uint16)
+
+    def _get_slice_array_for_stack_file_and_z(self, file_name, shape, z):
+
+        try:
+            with open(file_name, 'rb') as file:
+                aprint(f"Accessing file: {file_name} at z={z}")
+
+                length = shape[1] * shape[2] * numpy.dtype(uint16).itemsize
+                offset = z * length
+
+                file.seek(offset)
+
+                buffer = file.read(length)
+                array = frombuffer(buffer, dtype=uint16)
+
+                array = array.reshape(shape[1:])
+                return array
+
+        except FileNotFoundError:
+            aprint(f"Could  not find file: {file_name} for array of shape: {shape} at z={z}")
+            return numpy.zeros(shape[1:], dtype=uint16)
+
+    def close(self):
+        # Nothing to do...
+        pass
+
+    def channels(self):
+        return list(self._channels)
+
+    def shape(self, channel: str, time_point: int = 0) -> Sequence[int]:
+        if (channel, time_point) in self._shapes:
+            return (self._nb_time_points[channel],) + self._shapes[(channel, time_point)]
+        else:
+            return ()
+
+    def dtype(self, channel: str):
+        return numpy.uint16
+
+    def info(self, channel: str = None) -> str:
+        if channel:
+            info_str = f"Channel: '{channel}', nb time points: {self.shape(channel)[0]}, shape: {self.shape(channel)[1:]} "
+            return info_str
+        else:
+            return self.tree()
+
+    def get_metadata(self):
+        # TODO: implement this!
+        return {}
+
+    def get_array(self, channel: str, per_z_slice: bool = True, wrap_with_dask: bool = False):
+
+        if False:
+            # For some reason this is slower, should have been faster!:
+            # Construct a small Dask array for every lazy value:
+            arrays = [self.get_stack(channel, time_point, per_z_slice) for time_point in self._time_points[channel]]
+            stacked_array = array.stack(arrays, axis=0)  # Stack all small Dask arrays into one
+            return stacked_array
+
+        else:
+            # Lazy and memorized version of get_stack:
+            lazy_get_stack = delayed(self.cache.memoize(self.get_stack), pure=True)
+
+            # Lazily load each stack for each time point:
+            lazy_stacks = [lazy_get_stack(channel, time_point, per_z_slice) for time_point in self._time_points[channel]]
+
+            # Construct a small Dask array for every lazy value:
+            arrays = [array.from_delayed(lazy_stack,
+                                         dtype=uint16,
+                                         shape=self._shapes[(channel, 0)])
+                      for lazy_stack in lazy_stacks]
+
+            stacked_array = array.stack(arrays, axis=0)  # Stack all small Dask arrays into one
+
+            return stacked_array
+
+    def get_stack(self, channel, time_point, per_z_slice=True):
+
+        file_name = self._get_stack_file_name(channel, time_point)
+        shape = self._shapes[(channel, time_point)]
+
+        if per_z_slice:
+
+            lazy_get_slice_array_for_stack_file_and_z = delayed(self.cache.memoize(self._get_slice_array_for_stack_file_and_z), pure=True)
+
+            # Lazily load each stack for each time point:
+            lazy_stacks = [lazy_get_slice_array_for_stack_file_and_z(file_name, shape, z) for z in range(0, shape[0])]
+
+            arrays = [array.from_delayed(lazy_stack,
+                                         dtype=uint16,
+                                         shape=shape[1:])
+                      for lazy_stack in lazy_stacks]
+
+            stack = array.stack(arrays, axis=0)
+
+        else:
+            stack = self._get_array_for_stack_file(file_name, shape=shape)
+
+        return stack
+
+    def add_channel(self, name: str, shape: Tuple[int, ...], dtype, enable_projections: bool = True, **kwargs) -> Any:
+        raise NotImplementedError('Not implemented!')
+
+    def get_projection_array(self, channel: str, axis: int, wrap_with_dask: bool = True) -> Any:
+        array = self.get_array(channel=channel,
+                               per_z_slice=False,
+                               wrap_with_dask=True)
+        projection = dask.array.max(array, axis=axis + 1)
+        return projection
+
+    def write_array(self, channel: str, array: numpy.ndarray):
+        raise NotImplementedError('Not implemented!')
+
+    def write_stack(self, channel: str, time_point: int, stack: numpy.ndarray):
+        raise NotImplementedError('Not implemented!')
+
+    def check_integrity(self, channels: Sequence[str]) -> bool:
+        # TODO: actually implement!
+        return True
---
+++ packages/dexp/dexp-2021.4.22.1109-py2.py3-none-any.whl/dexp/datasets/test/test_zarr_dataset.py
@@ -0,0 +1,175 @@
+import tempfile
+from os.path import join
+
+import numpy
+from skimage.data import binary_blobs
+from skimage.filters import gaussian
+
+from dexp.datasets.zarr_dataset import ZDataset
+from dexp.processing.backends.numpy_backend import NumpyBackend
+
+
+def test_zarr_dataset_livecycle():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        print('created temporary directory', tmpdir)
+
+        # self, path:str, mode:str ='r', store:str ='dir'
+        zdataset = ZDataset(path=join(tmpdir, 'test.zarr'),
+                            mode='w',
+                            store='dir')
+
+        array1 = zdataset.add_channel(name='first',
+                                      shape=(10, 100, 100, 100),
+                                      chunks=(1, 50, 50, 50),
+                                      dtype='f4',
+                                      codec='zstd',
+                                      clevel=3)
+
+        array2 = zdataset.add_channel(name='second',
+                                      shape=(17, 10, 20, 30),
+                                      chunks=(1, 5, 1, 6),
+                                      dtype='f4',
+                                      codec='zstd',
+                                      clevel=3)
+
+        assert array1 is not None
+        assert array2 is not None
+
+        assert len(zdataset.channels()) == 2
+        assert 'first' in zdataset.channels()
+        assert 'second' in zdataset.channels()
+
+        assert zdataset.shape('first') == (10, 100, 100, 100)
+        assert zdataset.chunks('first') == (1, 50, 50, 50)
+        assert zdataset.dtype('first') == 'f4'
+
+        assert zdataset.shape('second') == (17, 10, 20, 30)
+        assert zdataset.chunks('second') == (1, 5, 1, 6)
+        assert zdataset.dtype('second') == 'f4'
+
+        assert not zdataset.check_integrity()
+
+        with NumpyBackend() as backend:
+            xp = backend.get_xp_module()
+            blobs = binary_blobs(length=100, n_dim=3, blob_size_fraction=0.1).astype('f4')
+            for i in range(0, 10):
+
+                blobs = gaussian(blobs, sigma=1 + 0.1 * i)
+                zdataset.write_stack('first', i, blobs)
+
+                # import napari
+                # with napari.gui_qt():
+                #     from napari import Viewer
+                #     viewer = Viewer()
+                #     viewer.add_image(blobs, name='blobs')
+                #     viewer.add_image(zdataset.get_stack('first', i), name='(zdataset.get_stack(first, i)')
+
+                assert xp.all((zdataset.get_stack('first', i) == blobs))
+
+                for axis in range(3):
+                    assert xp.all((zdataset.get_projection_array('first', axis=axis)[i] == xp.max(blobs, axis=axis)))
+
+            blobs = binary_blobs(length=30, n_dim=3, blob_size_fraction=0.03).astype('f4')
+            for i in range(0, 17):
+                blobs = gaussian(blobs, sigma=1 + 0.1 * i)
+                blobs = blobs[0:10, 0:20, 0:30]
+                zdataset.write_stack('second', i, blobs)
+
+                assert xp.all((zdataset.get_stack('second', i) == blobs))
+
+                for axis in range(3):
+                    assert xp.all((zdataset.get_projection_array('second', axis=axis)[i] == xp.max(blobs, axis=axis)))
+
+        assert zdataset.check_integrity()
+
+        # with napari.gui_qt():
+        #     viewer = Viewer()
+        #     viewer.add_image(array1, name='image')
+
+
+def test_zarr_integrity():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        print('created temporary directory', tmpdir)
+
+        # self, path:str, mode:str ='r', store:str ='dir'
+        zdataset = ZDataset(path=join(tmpdir, 'test.zarr'),
+                            mode='w',
+                            store='dir')
+
+        array = zdataset.add_channel(name='first',
+                                     shape=(10, 100, 100, 100),
+                                     chunks=(1, 50, 50, 50),
+                                     dtype='f4',
+                                     codec='zstd',
+                                     clevel=3)
+
+        # we initialise almost everything:
+        array[0:9] = 0
+
+        # the dataset integrity must be False!
+        assert not zdataset.check_integrity()
+
+        # we initialise everything:
+        array[...] = 1
+        # the dataset integrity must be True!
+        assert zdataset.check_integrity()
+
+
+def test_add_channels_to():
+    with tempfile.TemporaryDirectory() as tmpdir:
+        print('created temporary directory', tmpdir)
+
+        # self, path:str, mode:str ='r', store:str ='dir'
+        dataset1_path = join(tmpdir, 'test1.zarr')
+        zdataset1 = ZDataset(path=dataset1_path,
+                             mode='w',
+                             store='dir')
+
+        array1 = zdataset1.add_channel(name='first',
+                                       shape=(10, 11, 12, 13),
+                                       chunks=None,
+                                       dtype='f4',
+                                       codec='zstd',
+                                       clevel=3)
+
+        array1[...] = 1
+
+        dataset2_path = join(tmpdir, 'test2.zarr')
+        zdataset2 = ZDataset(path=dataset2_path,
+                             mode='w',
+                             store='dir')
+
+        array2 = zdataset2.add_channel(name='second',
+                                       shape=(17, 10, 20, 30),
+                                       chunks=None,
+                                       dtype='f4',
+                                       codec='zstd',
+                                       clevel=3)
+
+        array2[...] = 1
+
+        zdataset2.add_channels_to(dataset1_path,
+                                  channels=('second',),
+                                  rename=('second-',),
+                                  )
+
+        zdataset1_reloaded = ZDataset(path=dataset1_path,
+                                      mode='r',
+                                      store='dir')
+
+        assert len(zdataset1_reloaded.channels()) == 2
+        assert 'first' in zdataset1_reloaded.channels()
+        assert 'second-' in zdataset1_reloaded.channels()
+
+        assert zdataset1_reloaded.shape('first') == zdataset1.shape('first')
+        assert zdataset1_reloaded.shape('second-') == zdataset2.shape('second')
+        assert zdataset1_reloaded.dtype('first') == zdataset1.dtype('first')
+        assert zdataset1_reloaded.dtype('second-') == zdataset2.dtype('second')
+
+        a = zdataset1_reloaded.get_array('second-', wrap_with_dask=True).compute()
+        b = zdataset2.get_array('second', wrap_with_dask=True).compute()
+        assert numpy.all(a == b)
+
+        zdataset1.close()
+        zdataset2.close()
+        zdataset1_reloaded.close()
