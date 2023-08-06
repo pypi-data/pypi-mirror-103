65fda83806a51aa6b35618f20d20417addc56ac1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:35 +0000

    Add package streamstate-utils/0.4.2/streamstate-utils-0.4.2.tar.gz

---
+++ packages/streamstate-utils/streamstate-utils-0.4.2.tar.gz/streamstate-utils-0.4.2/src/streamstate_utils/gcs_create_folder.py
@@ -0,0 +1,32 @@
+from google.cloud import storage
+from streamstate_utils.utils import get_folder_location
+from streamstate_utils.structs import InputStruct
+from typing import List
+import google.auth
+
+
+def create_gcs_folders(
+    bucket_name: str,
+    app_name: str,
+    inputs: List[InputStruct],
+):
+    credentials, project = google.auth.default()
+    gcs_client = storage.Client(project=project, credentials=credentials)
+    bucket = gcs_client.get_bucket(bucket_name)
+    for input in inputs:
+        folder_name = get_folder_location(app_name, input.topic)
+        folder_name_with_delimiter = f"{folder_name}/"
+        result = 0
+        for blob in gcs_client.list_blobs(
+            bucket_name, prefix=folder_name, delimiter="/"
+        ):
+            result = 1
+
+        if result == 0:
+            blob = bucket.blob(folder_name_with_delimiter)
+            blob.upload_from_string(
+                "", content_type="application/x-www-form-urlencoded;charset=UTF-8"
+            )
+            print("Folder does not exist, creating")
+        else:
+            print("Folder already exists, skipping")
