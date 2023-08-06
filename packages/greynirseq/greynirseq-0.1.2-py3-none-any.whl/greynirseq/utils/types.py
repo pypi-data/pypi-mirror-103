a9c1fd5497bd535fae66eb138840a9b06090f6b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:39 +0000

    Add package greynirseq/0.1.2/greynirseq-0.1.2-py3-none-any.whl

---
+++ packages/greynirseq/greynirseq-0.1.2-py3-none-any.whl/greynirseq/utils/types.py
@@ -0,0 +1,9 @@
+# Copyright (C) Miðeind ehf.
+# This file is part of GreynirSeq <https://github.com/mideind/GreynirSeq>.
+# See the LICENSE file in the root of the project for terms of use.
+
+from typing import Union
+
+from torch import LongTensor, Tensor
+
+Numeric = Union[int, float, Tensor]
