750e52d1739a3d7de4a4796ad3f443698ad2520f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:20 +0000

    Add package db-ud/0.0.1/db_ud-0.0.1.tar.gz

---
+++ packages/db-ud/db_ud-0.0.1.tar.gz/db_ud-0.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="db_ud",
+    version="0.0.1",
+    author="lovic",
+    author_email="",
+    description="database connect",
+    long_description="",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
