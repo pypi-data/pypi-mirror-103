30ffc44c252e7619ce09b53afe8235051564132a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:36 +0000

    Add package pydeflate/0.1.4/pydeflate-0.1.4-py2.py3-none-any.whl

---
+++ packages/pydeflate/pydeflate-0.1.4-py2.py3-none-any.whl/pydeflate/deflate/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for pydeflate."""
+
+__author__ = """Jorge Rivera"""
+__email__ = 'jorge.rivera@one.org'
+__version__ = '0.1.2'
+
+
+from pydeflate.deflate.deflate import deflate_data
