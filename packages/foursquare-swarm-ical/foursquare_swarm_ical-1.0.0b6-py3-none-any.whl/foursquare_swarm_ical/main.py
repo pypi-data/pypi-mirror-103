9a3f208985c192564a2c5c625bde3ba372048a25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:30 +0000

    Add package foursquare-swarm-ical/1.0.0b6/foursquare_swarm_ical-1.0.0b6-py3-none-any.whl

---
+++ packages/foursquare-swarm-ical/foursquare_swarm_ical-1.0.0b6-py3-none-any.whl/foursquare_swarm_ical/main.py
@@ -0,0 +1,43 @@
+import os
+
+import appdirs  # type: ignore [import]
+import click
+
+from . import config_file
+from . import db
+from . import ical
+from .emoji import Emojis
+
+
+@click.command(context_settings={'max_content_width': 120})
+@click.option(
+    '-v', '--verbose', count=True,
+    help="Be more verbose")
+@click.option(
+    '--sync/--no-sync', default=True, show_default=True,
+    help="Sync again or just use local database?")
+@click.option(
+    '--access-token', type=str, envvar='FOURSQUARE_TOKEN', show_envvar=True,
+    help="Foursquare oauth2 access token")
+@click.option(
+    '--database', type=click.Path(writable=True),
+    default=os.path.join(appdirs.user_data_dir(appname=__package__), 'checkins.sqlite'), show_default=True,
+    help="SQLite database file")
+@click.option(
+    '-e', '--emoji/--no-emoji', default=False, show_default=True,
+    help="Prefix summary with venue category as emoji")
+@click.option(
+    '-o', '--output', type=click.File('wb'), default='-',
+    help="Output file")
+@config_file.yaml_config_option()
+@config_file.yaml_config_sample_option()
+def main(verbose: bool, sync: bool, access_token: str, database: str, emoji: bool, output) -> None:
+    """Sync Foursquare Swarm check-ins to local sqlite DB and generate iCalendar"""
+    with db.database(database) as db_conn:
+        if sync:
+            if not access_token:
+                raise RuntimeError("--access-token or FOURSQUARE_TOKEN required")
+
+            db.sync(db=db_conn, access_token=access_token, verbose=verbose)
+
+        output.write(ical.ical(db=db_conn, emojis=(Emojis() if emoji else None)))
