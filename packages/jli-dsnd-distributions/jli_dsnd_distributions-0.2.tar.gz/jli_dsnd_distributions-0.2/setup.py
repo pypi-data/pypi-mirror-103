355c938aac03925ce98c0d739e8167889b7afdd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:19:53 +0000

    Add package jli-dsnd-distributions/0.2/jli_dsnd_distributions-0.2.tar.gz

---
+++ packages/jli-dsnd-distributions/jli_dsnd_distributions-0.2.tar.gz/jli_dsnd_distributions-0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jli_dsnd_distributions',
+      version='0.2',
+      description='Gaussian and Binomial distributions',
+      packages=['jli_dsnd_distributions'],
+      author = 'Haotong Li',
+      author_email = 'jamesli0512@gmail.com',
+      zip_safe=False)
