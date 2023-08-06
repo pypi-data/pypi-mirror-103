eed437d5bef332a20b29390efbe3cfb88ff8a51a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:31 +0000

    Add package kaldiprep/0.5.1/kaldiprep-0.5.1-py3-none-any.whl

---
+++ packages/kaldiprep/kaldiprep-0.5.1-py3-none-any.whl/kaldiprep/dictionary.py
@@ -0,0 +1,30 @@
+import os
+
+from kaldiprep.utterances import _write_file
+
+
+def write_lexicon(entries, path, encoding=None):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+    if isinstance(entries, dict):
+        entries = entries.items()
+
+    lexicon_lines = ["{} {}".format(word, pronunciation) for word, pronunciation in _iterate_entries(entries)]
+    _write_file("lexicon.txt", lexicon_lines, path, encoding=encoding)
+
+
+def _iterate_entries(entries):
+    for word, pronunciation in sorted(entries):
+        if isinstance(pronunciation, str):
+            yield word, pronunciation
+            continue
+
+        try:
+            for n, p in enumerate(pronunciation):
+                if n > 0:
+                    yield f'{word}#({n})', p
+                else:
+                    yield word, p
+        except:
+            raise TypeError('Couldn\'t handle lexicon entry {} of type {}'.format(pronunciation, type(pronunciation)))
