176517108373c78f38bd09b8f4bf0d3f4f65d234
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:20 +0000

    Add package sqlglot/0.15.0/sqlglot-0.15.0.tar.gz

---
+++ packages/sqlglot/sqlglot-0.15.0.tar.gz/sqlglot-0.15.0/sqlglot/__init__.py
@@ -0,0 +1,20 @@
+from sqlglot.dialects import Dialect
+from sqlglot.expressions import Expression
+from sqlglot.generator import Generator
+from sqlglot.tokens import Tokenizer, TokenType
+from sqlglot.parser import Parser
+
+
+__version__ = '0.15.0'
+
+
+def parse(code, read=None):
+    dialect = Dialect.get(read, Dialect)()
+    return dialect.parse(code)
+
+
+def transpile(code, read=None, write=None, **opts):
+    return [
+        Dialect.get(write or read, Dialect)().generate(expression, **opts)
+        for expression in parse(code, read)
+    ]
