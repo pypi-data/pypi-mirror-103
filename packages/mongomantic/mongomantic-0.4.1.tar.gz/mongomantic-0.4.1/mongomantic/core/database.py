26b4894b1a7a68d6d19f86281d2f3ce4c7370afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:27 +0000

    Add package mongomantic/0.4.1/mongomantic-0.4.1.tar.gz

---
+++ packages/mongomantic/mongomantic-0.4.1.tar.gz/mongomantic-0.4.1/mongomantic/core/database.py
@@ -0,0 +1,27 @@
+# # Package # #
+from pymongo import MongoClient
+from pymongo.database import Database
+
+__all__ = ["MongomanticClient"]
+
+
+class MongomanticClient:
+    client: MongoClient = None
+    db: Database = None
+
+
+def connect(uri: str, database: str, mock: bool = False) -> None:
+    if mock:
+        try:
+            import mongomock
+        except ImportError:
+            raise RuntimeError("Mongomock needs to be installed for mocking a connection")
+        MongomanticClient.client = mongomock.MongoClient(uri)
+    else:
+        MongomanticClient.client = MongoClient(uri)
+
+    MongomanticClient.db = MongomanticClient.client.__getattr__(database)
+
+
+def disconnect() -> None:
+    MongomanticClient.client.close()
