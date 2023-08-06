d969a41ead722b2e4c8d74e3d599824a8f9a57f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:44 +0000

    Add package wikipeople/0.1/wikipeople-0.1.tar.gz

---
+++ packages/wikipeople/wikipeople-0.1.tar.gz/wikipeople-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name='wikipeople',
+      version='0.1',
+      description='Get information about people using wikidata',
+      url='http://github.com/svanstroud/wikipeople',
+      author='Sam VS',
+      author_email='sam.van.stround@cern.com',
+      license='MIT',
+      packages=['wikipeople'],
+      install_requires=[
+          'requests',
+      ],
+      python_requires='>=3.6',
+      zip_safe=False)
