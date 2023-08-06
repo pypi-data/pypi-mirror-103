f8362f036ec3c8da7e9c72fbbff288d35ce9da0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:37:45 +0000

    Add package whacc/0.0.23/whacc-0.0.23.tar.gz

---
+++ packages/whacc/whacc-0.0.23.tar.gz/whacc-0.0.23/whacc/examples/gui_example.py
@@ -0,0 +1,12 @@
+H5_file_name = '/Users/phil/Downloads/untitled folder 2/AH1157X22012021xS408_subset.h5'
+label_key = 'new_labels'
+
+
+
+
+from whacc.touch_curation_GUI import touch_gui
+
+touch_gui(H5_file_name, label_key, 'labels')
+'''test
+H5_file_name
+to see if it has new labels '''
