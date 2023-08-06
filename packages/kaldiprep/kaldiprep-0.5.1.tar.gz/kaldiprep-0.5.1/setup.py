3556781665f983d844d194a5a74c51d1c08c3712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 01:52:31 +0000

    Add package kaldiprep/0.5.1/kaldiprep-0.5.1.tar.gz

---
+++ packages/kaldiprep/kaldiprep-0.5.1.tar.gz/kaldiprep-0.5.1/setup.py
@@ -0,0 +1,14 @@
+import setuptools
+
+setuptools.setup(
+    name='kaldiprep',
+    version='0.5.1',
+    author='Robert Gale',
+    author_email='galer@ohsu.edu',
+    packages=['kaldiprep'],
+    url='https://repo.cslu.ohsu.edu/galer/kaldiprep',
+    install_requires=[
+        'sortedcontainers'
+    ],
+)
+
