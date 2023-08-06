ec61e7cd9d376151bb5961b06233598aec6b4d0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:40 +0000

    Add package hedron/0.0.1/hedron-0.0.1.tar.gz

---
+++ packages/hedron/hedron-0.0.1.tar.gz/hedron-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='hedron',
+      version='0.0.1',
+      description='A python package project for doing analysis on coordinates and clustering them.',
+      author='Odos Matthews',
+      author_email='odosmatthews@gmail.com',
+      packages=['hedron'],
+      intall_requires=['pandas'])
