a55f3be84028d6c2f0280a75e030a1a56bf99d49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:54 +0000

    Add package binomial-gaussian-distributions-ldz/1.0/binomial_gaussian_distributions_ldz-1.0.tar.gz

---
+++ packages/binomial-gaussian-distributions-ldz/binomial_gaussian_distributions_ldz-1.0.tar.gz/binomial_gaussian_distributions_ldz-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='binomial_gaussian_distributions_ldz',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['binomial_gaussian_distributions_ldz'],
+      author_email = 'ldzarzec@syr.edu',
+      zip_safe=False)
