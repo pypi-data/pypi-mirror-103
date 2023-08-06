79bf07e8ff7bb936ab3393dde2dca81bbd2bc66f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:08:44 +0000

    Add package MotionUI/0.1.1/MotionUI-0.1.1.tar.gz

---
+++ packages/MotionUI/MotionUI-0.1.1.tar.gz/MotionUI-0.1.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name="MotionUI",
+    version='0.1.1',
+    description='ハンドジェスチャーによるUI操作をカメラ入力のリアルタイム解析により実現する汎用ソフトウェアライブラリ',
+    author='Taisuke Umetani',
+    author_email='nazogle@gmail.com',
+    url='https://github.com/TaisukeUmetani/MotionUI',
+)
