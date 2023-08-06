88c39ba95cd122e1585807445fb181d752b5b926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:56:31 +0000

    Add package decentriq-platform/0.1.0/decentriq_platform-0.1.0-py3-none-any.whl

---
+++ packages/decentriq-platform/decentriq_platform-0.1.0-py3-none-any.whl/decentriq_platform/config.py
@@ -0,0 +1,5 @@
+import os
+
+DECENTRIQ_HOST = os.getenv("DECENTRIQ_HOST", "api.decentriq.ch")
+DECENTRIQ_PORT = int(os.getenv("DECENTRIQ_PORT", "443"))
+DECENTRIQ_USE_TLS = True if os.getenv("DECENTRIQ_USE_TLS", "true") == "true" else False
