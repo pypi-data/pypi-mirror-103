c7ba56011efe6a296c85e86386498efc9510b9d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:38 +0000

    Add package qhub/0.3.3/qhub-0.3.3.tar.gz

---
+++ packages/qhub/qhub-0.3.3.tar.gz/qhub-0.3.3/qhub/destroy.py
@@ -0,0 +1,38 @@
+import logging
+
+from qhub.utils import (
+    timer,
+    check_cloud_credentials,
+    verify_configuration_file_exists,
+)
+from qhub.provider import terraform
+
+
+logger = logging.getLogger(__name__)
+
+
+def destroy_configuration(config):
+    logger.info(
+        """Removing all infrastructure, your local files will still remain, \n
+    you can use 'qhub deploy' to re - install infrastructure using same config file"""
+    )
+
+    with timer(logger, "destroying QHub"):
+        # 01 Verify configuration file exists
+        verify_configuration_file_exists()
+
+        # 02 Check Environment Variables
+        check_cloud_credentials(config)
+
+        # 03 Remove all infrastructure
+        terraform.init(directory="infrastructure")
+        terraform.destroy(directory="infrastructure")
+
+        # 06 Remove terraform backend remote state bucket
+        # backwards compatible with `qhub-config.yaml` which
+        # don't have `terraform_state` key
+        if (config.get("terraform_state", {}).get("type") == "remote") and (
+            config.get("provider") != "local"
+        ):
+            terraform.init(directory="terraform-state")
+            terraform.destroy(directory="terraform-state")
