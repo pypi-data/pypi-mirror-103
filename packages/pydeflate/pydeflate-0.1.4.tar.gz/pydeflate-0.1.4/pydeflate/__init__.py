13cf8ad4c858372e263c87751e11cbac47473fa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:32:36 +0000

    Add package pydeflate/0.1.4/pydeflate-0.1.4.tar.gz

---
+++ packages/pydeflate/pydeflate-0.1.4.tar.gz/pydeflate-0.1.4/pydeflate/__init__.py
@@ -0,0 +1,10 @@
+"""Top-level package for pydeflate."""
+
+__author__ = """Jorge Rivera"""
+__email__ = 'jorge.rivera@one.org'
+__version__ = '0.1.4'
+
+
+from pydeflate.deflate.deflate import deflate_data
+
+from pydeflate.utils import update_data
