5bbb9e8d5e17e715d4c57aef2a7c3c9831878706
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:58:10 +0000

    Add package cortx-jupyter/0.1.17/cortx_jupyter-0.1.17-py2-none-any.whl

---
+++ packages/cortx-jupyter/cortx_jupyter-0.1.17-py2-none-any.whl/cortx_jupyter/utils.py
@@ -0,0 +1,39 @@
+
+
+import time
+
+class MultiPartUploadHelper:
+
+    def __init__(self, seconds):
+        self._seconds = seconds
+        self._store = {}
+
+    def _remove_old_keys(self, now):
+        self._store = {
+            key: (expires, value)
+            for key, (expires, value) in self._store.items()
+            if expires > now
+        }
+
+    def __getitem__(self, key):
+        now = int(time.time())
+        self._remove_old_keys(now)
+        return self._store[key][1]
+
+    def __setitem__(self, key, value):
+        now = int(time.time())
+        self._remove_old_keys(now)
+        self._store[key] = (now + self._seconds, value)
+
+    def __delitem__(self, key):
+        now = int(time.time())
+        self._remove_old_keys(now)
+        del self._store[key]
+
+
+def get_key(config, path):
+    return config.prefix + path.lstrip('/')
+
+
+def get_path(config, key):
+    return '/' + key[len(config.prefix):]
