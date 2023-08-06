e42d9798f3c740848f19c13a36f3f6ab9602d6e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:41 +0000

    Add package tinmanlol/1.1.0/tinmanlol-1.1.0.tar.gz

---
+++ packages/tinmanlol/tinmanlol-1.1.0.tar.gz/tinmanlol-1.1.0/setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='tinmanlol',
+    version='1.1.0',
+    py_modules=['tinmanlol'],
+    author='Tinman',
+    author_email='dingding2627@sohu.com',
+    url='https://user.qzone.qq.com/339526812/infocenter',
+    description='A simple printer of nested lists',
+    )
