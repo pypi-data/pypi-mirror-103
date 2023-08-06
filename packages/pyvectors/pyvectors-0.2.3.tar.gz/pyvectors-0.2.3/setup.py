31e9a92cd635b151f7bb69f05aea600b2e8d0570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:17:07 +0000

    Add package pyvectors/0.2.3/pyvectors-0.2.3.tar.gz

---
+++ packages/pyvectors/pyvectors-0.2.3.tar.gz/pyvectors-0.2.3/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_desc = fh.read()
+
+setuptools.setup(
+    name="pyvectors",
+    version="0.2.3",
+    author="Divyajeet Singh",
+    author_email="knightt1821@gmail.com",
+    description="provides 3D position Vectors as Python objects",
+    long_description=long_desc,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
