bba42b0ef62c9a59879561d2373e7764d5fb87b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:34:00 +0000

    Add package AlgoOmid/1.0.15/AlgoOmid-1.0.15.tar.gz

---
+++ packages/AlgoOmid/AlgoOmid-1.0.15.tar.gz/AlgoOmid-1.0.15/AlgoOmid/__init__.py
@@ -0,0 +1,4 @@
+from AlgoOmid.AlgorithmTrading_Module import Create_isin_object
+from AlgoOmid.AlgorithmTrading_Module import TSE_Close_Daily
+from AlgoOmid.AlgorithmTrading_Module import dataengine_Data
+from AlgoOmid.AlgorithmTrading_Module import Get_SW_Data
