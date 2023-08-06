8b3ccae87481f92550c81c78961539f71a8f8672
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:28:14 +0000

    Add package binalyzer-data-provider/1.0.0/binalyzer_data_provider-1.0.0-py3-none-any.whl

---
+++ packages/binalyzer-data-provider/binalyzer_data_provider-1.0.0-py3-none-any.whl/binalyzer_data_provider/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    binalyzer_data_provider
+    ~~~~~~~~~~~~~~~~~~~~~~~
+
+    A library supporting the analysis of binary data.
+
+    :copyright: 2020 Denis Vasilík
+    :license: MIT, see LICENSE for details.
+"""
+
+name = "binalyzer_data_provider"
+
+__tag__ = "v1.0.0"
+__build__ = 25
+__version__ = "{}".format(__tag__)
+__commit__ = "48ae84c"
