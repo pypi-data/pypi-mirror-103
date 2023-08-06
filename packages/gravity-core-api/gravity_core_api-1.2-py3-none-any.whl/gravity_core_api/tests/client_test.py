c617c14a0b6a27d583e9ae57fa26a8dc135dcf38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:17:24 +0000

    Add package gravity-core-api/1.2/gravity_core_api-1.2-py3-none-any.whl

---
+++ packages/gravity-core-api/gravity_core_api-1.2-py3-none-any.whl/gravity_core_api/tests/client_test.py
@@ -0,0 +1,15 @@
+from socket import socket
+from gravity_core_api.tests import test_settings as s
+import pickle
+
+
+test_data = {'trash_type': {'name': 'TEST123', 'wserver_id': 99, 'category': 12 ,'active': True}}
+command = {}
+command['wserver_insert_command'] = test_data
+command = pickle.dumps(command)
+sock = socket()
+sock.connect((s.api_ip, s.api_port))
+sock.send(command)
+response = sock.recv(1024)
+response = pickle.loads(response)
+print(response)
