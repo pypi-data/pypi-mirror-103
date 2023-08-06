6ad168a784e745898fb563307583e4e809feb873
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:58 +0000

    Add package EncryptEnv/0.0.2/EncryptEnv-0.0.2.tar.gz

---
+++ packages/EncryptEnv/EncryptEnv-0.0.2.tar.gz/EncryptEnv-0.0.2/setup.py
@@ -0,0 +1,34 @@
+import os
+import setuptools
+
+base_dir = os.path.dirname(os.path.realpath(__file__))
+with open(os.path.join(base_dir, "README.md"), "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="EncryptEnv",
+    version="0.0.2",
+    author="Thinktron",
+    author_email="jeremywang@thinktronltd.com",
+    description="Encrypt the passwords in the environment variables.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://rd.thinktronltd.com/jeremywang/EncryptEnv",
+    packages=setuptools.find_packages(),
+    # package_data={'TronGisPy': ['data/*', 'data/*/*']},
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
+      install_requires=[
+          'cryptography',
+      ]
+)
+
+# v2 Excrypt key as string type
+
+
+# python setup.py sdist bdist_wheel
+# twine upload dist/EncryptEnv-0.0.1*
+# scp Z:\R014_Jeremy\Projects\EncryptEnv\dist\EncryptEnv-0.0.1-py3-none-any.whl  jeremy@rd.thinktronltd.com:/home/ttl/pypi/EncryptEnv-0.0.1-py3-none-any.whl
