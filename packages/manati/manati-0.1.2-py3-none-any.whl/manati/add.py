289f2938462b9ff62bc35b16003792dd499cb8fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:21:24 +0000

    Add package manati/0.1.2/manati-0.1.2-py3-none-any.whl

---
+++ packages/manati/manati-0.1.2-py3-none-any.whl/manati/add.py
@@ -0,0 +1,58 @@
+import pathlib
+import os
+import shutil
+
+import click
+
+from manati.utils import task, substitute
+
+
+@task('Create package...', ' OK')
+def add_package(name):
+    """ Add a package structure in the current working directory
+
+    Parameters
+    ----------
+    name: str
+        Name of the package including parent packages.
+    """
+    components = name.split('.')
+
+    path = pathlib.Path('.')
+
+    for comp in components:
+        path = path / comp
+        if not os.path.exists(path):
+            os.mkdir(path)
+        if not os.path.exists(path / '__init__.py'):
+            with open(path / '__init__.py', 'w') as f:
+                f.write('')
+
+
+def add_license(path, license):
+
+    if isinstance(path, str):
+        if path == '.' or path == './':
+            path = pathlib.Path.cwd()
+        else:
+            path = pathlib.Path(path)
+
+    if os.path.exists(path / 'LICENSE'):
+        raise click.BadParameter('LICENSE file already existing.')
+
+    templates_dir = pathlib.Path(__file__).parent / 'templates' / 'licenses'
+
+    files = {'MIT': templates_dir / 'MIT',
+             'GPLv3': templates_dir / 'GPLv3',
+             'Apache': templates_dir / 'Apache'
+             }
+
+    shutil.copyfile(files[license], path / 'LICENSE')
+
+    setup_py = path / 'setup.py'
+    if not os.path.exists(setup_py):
+        click.echo('Warning: cannot find setup.py to set license type.')
+        return
+    substitute(setup_py, "license='None'", "license='%s'" % license)
+
+
