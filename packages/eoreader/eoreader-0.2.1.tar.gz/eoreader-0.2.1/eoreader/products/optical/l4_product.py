1b715e8a7d8b3883f112bdf39a7745f5f514ec50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:36 +0000

    Add package eoreader/0.2.1/eoreader-0.2.1.tar.gz

---
+++ packages/eoreader/eoreader-0.2.1.tar.gz/eoreader-0.2.1/eoreader/products/optical/l4_product.py
@@ -0,0 +1,27 @@
+""" Landsat-4 products """
+from eoreader.exceptions import InvalidProductError
+from eoreader.products.optical.landsat_product import LandsatProduct, LandsatProductType
+
+
+class L4Product(LandsatProduct):
+    """Class of Landsat-4 Products"""
+
+    def _set_resolution(self) -> float:
+        """
+        Set product default resolution (in meters)
+        """
+        if self.product_type == LandsatProductType.L1_TM:
+            def_res = 60.0
+        else:
+            # DO NOT TAKE INTO ACCOUNT TIRS RES
+            def_res = 30.0
+        return def_res
+
+    def _set_product_type(self) -> None:
+        """Get products type"""
+        if "LT04" in self.name:
+            self._set_tm_product_type()
+        elif "LM04" in self.name:
+            self._set_mss_product_type(version=4)
+        else:
+            raise InvalidProductError(f"Invalid Landsat-4 name: {self.name}")
---
+++ packages/eoreader/eoreader-0.2.1.tar.gz/eoreader-0.2.1/eoreader/utils.py
@@ -0,0 +1,60 @@
+""" Utils: mostly getting directories relative to the project """
+import logging
+import os
+
+from sertit.ci import get_db2_path
+
+EOREADER_NAME = "eoreader"
+DATETIME_FMT = "%Y%m%dT%H%M%S"
+LOGGER = logging.getLogger(EOREADER_NAME)
+
+
+def get_src_dir() -> str:
+    """
+    Get src directory.
+
+    Returns:
+        str: Root directory
+    """
+    return os.path.abspath(os.path.dirname(__file__))
+
+
+def get_root_dir() -> str:
+    """
+    Get root directory.
+
+    Returns:
+        str: Root directory
+    """
+    return os.path.abspath(os.path.join(get_src_dir(), ".."))
+
+
+def get_data_dir() -> str:
+    """
+    Get data directory.
+
+    Returns:
+        str: Data directory
+    """
+    return os.path.abspath(os.path.join(get_src_dir(), "data"))
+
+
+def get_db_dir() -> str:
+    """
+    Get database directory in the DS2
+
+    Returns:
+        str: Database directory
+    """
+    db_dir = os.path.join(r"\\ds2", "database02", "BASES_DE_DONNEES")
+
+    if not os.path.isdir(db_dir):
+        try:
+            db_dir = os.path.join(get_db2_path(), "BASES_DE_DONNEES")
+        except NotADirectoryError:
+            db_dir = os.path.join("/home", "ds2_db2", "BASES_DE_DONNEES")
+
+    if not os.path.isdir(db_dir):
+        raise NotADirectoryError("Impossible to open database directory !")
+
+    return db_dir
