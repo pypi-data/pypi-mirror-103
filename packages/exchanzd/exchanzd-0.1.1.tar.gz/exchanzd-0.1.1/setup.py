eee764f0f72b6912c592d9c8997268baf7da8e3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 05:05:59 +0000

    Add package exchanzd/0.1.1/exchanzd-0.1.1.tar.gz

---
+++ packages/exchanzd/exchanzd-0.1.1.tar.gz/exchanzd-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['exchanzd']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'exchanzd',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'Hirokazu Hirono',
+    'author_email': 'hirono.hirokazu@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
