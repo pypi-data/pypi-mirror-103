94ab6e1fe848698fd1a01aed3f9abd8e3bff054a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:33:19 +0000

    Add package felipe-teste/0.0.2/felipe_teste-0.0.2.tar.gz

---
+++ packages/felipe-teste/felipe_teste-0.0.2.tar.gz/felipe_teste-0.0.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+requirements = ["ray", "NanoFilt>=2.8.0"]
+
+# teste
+setup(
+    name="felipe_teste",
+    version="0.0.2",
+    author="Felipe Marcelo",
+    author_email="felipemarcelo@usp.br",
+    description="A teste package",
+    long_description=readme,
+    url="https://github.com/sfmpds/felipe_teste/",
+    packages=find_packages(),
+    install_requires=requirements,
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
